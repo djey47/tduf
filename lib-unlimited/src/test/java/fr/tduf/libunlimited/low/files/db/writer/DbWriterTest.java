ccb70768d2a43ca94a165ae297745c490e4aa542
djey <j.canler@gmail.com>
2014-11-23 20:35:36 +0100

    feature(database): add writer test - WIP.

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/db/writer/DbWriterTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/db/writer/DbWriterTest.java
@@ -1,10 +1,17 @@
 package fr.tduf.libunlimited.low.files.db.writer;
 
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DbWriterTest {
 
@@ -18,5 +25,23 @@ public class DbWriterTest {
 
         //THEN
         assertThat(dbWriter).isNotNull();
+    }
+
+    @Ignore
+    public void writeAll_whenRealContents_shouldCreateFiles() throws IOException {
+        //GIVEN
+        // TODO reinject ref and topic into structure instead of root db
+        InputStream resourceAsStream = getClass().getResourceAsStream("/db/TDU_Achievements.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        DbDto dbDto = objectMapper.readValue(resourceAsStream, DbDto.class);
+        Path tempDirectory = Files.createTempDirectory("libUnlimited-tests");
+
+        //WHEN
+        DbWriter.load(dbDto).writeAll(tempDirectory.toString());
+
+        //THEN
+        String outputContentsFile = tempDirectory + "//TDU_Achievements.db";
+        assertThat(new File(outputContentsFile).exists()).isTrue();
+//        String outputResourceFile = tempDirectory + "";
     }
 }
