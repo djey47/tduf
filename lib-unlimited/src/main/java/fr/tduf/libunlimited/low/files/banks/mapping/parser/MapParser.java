0130963b5de89f758901a04b859f87800927c16d
djey <j.canler@gmail.com>
2014-12-07 21:05:01 +0100

    feature(files) : add feature to read Bnk1.map contents.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/banks/mapping/parser/MapParser.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/banks/mapping/parser/MapParser.java
@@ -1,31 +1,68 @@
 package fr.tduf.libunlimited.low.files.banks.mapping.parser;
 
+import fr.tduf.libunlimited.low.files.banks.mapping.domain.BankMap;
+import fr.tduf.libunlimited.low.files.research.dto.FileStructureDto;
+import fr.tduf.libunlimited.low.files.research.parser.GenericParser;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.ObjectMapper;
+
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static java.util.Objects.requireNonNull;
 
 /**
  * Helper class to fetch entry list contained in Bnk1.map file.
  */
+// TODO Generify
 public class MapParser {
 
-    private final ByteArrayInputStream mapInputStream;
+    private final GenericParser parser;
 
-    private MapParser(ByteArrayInputStream inputStream) {
-        this.mapInputStream = inputStream;
+    private MapParser(ByteArrayInputStream inputStream) throws IOException {
+        InputStream structureAsStream = getClass().getResourceAsStream("/files/structures/MAP4-map.json");
+        FileStructureDto fileStructure = new ObjectMapper().readValue(structureAsStream, FileStructureDto.class);
+
+        parser = GenericParser.load(inputStream, fileStructure);
     }
 
     /**
      * Single entry point for this parser.
      * @return a {@link MapParser} instance.
      */
-    public static MapParser load(ByteArrayInputStream inputStream) {
+    public static MapParser load(ByteArrayInputStream inputStream) throws IOException {
         requireNonNull(inputStream, "A stream containing map contents is required");
 
         return new MapParser(inputStream);
     }
 
-    ByteArrayInputStream getMapInputStream() {
-        return mapInputStream;
+    /**
+     *
+     * @return
+     */
+    public BankMap parse() {
+        parser.parse();
+        List<Map<String, String>> repeatedValues = parser.getRepeatedValuesOf("entry_list");
+
+        BankMap bankMap = new BankMap();
+
+        for (Map<String, String> values : repeatedValues) {
+
+            long checksum = Long.valueOf(values.get("file_name_hash"));
+            long size1 = Long.valueOf(values.get("size_bytes_1"));
+            long size2 = Long.valueOf(values.get("size_bytes_2"));
+
+            bankMap.addEntry(checksum, size1, size2);
+        }
+
+        return bankMap;
+    }
+
+    GenericParser getParser() {
+        return parser;
     }
 }
--- lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/banks/mapping/parser/MapParserTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/banks/mapping/parser/MapParserTest.java
@@ -1,12 +1,21 @@
 package fr.tduf.libunlimited.low.files.banks.mapping.parser;
 
+import fr.tduf.libunlimited.low.files.banks.mapping.domain.BankMap;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MapParserTest {
+
+    private static Class<MapParserTest> thisClass = MapParserTest.class;
 
     @Test
     public void load_whenProvidedContents_shouldReturnParserInstance() throws Exception {
@@ -18,6 +27,24 @@ public class MapParserTest {
 
         //THEN
         assertThat(mapParser).isNotNull();
-        assertThat(mapParser.getMapInputStream()).isSameAs(mapInputStream);
+        assertThat(mapParser.getParser()).isNotNull();
+    }
+
+    @Test
+    public void parse_whenRealFiles_shouldReadAllEntries() throws IOException, URISyntaxException {
+        // GIVEN
+        URI uri = thisClass.getResource("/banks/Bnk1.map").toURI();
+        byte[] mapContents = Files.readAllBytes(Paths.get(uri));
+        ByteArrayInputStream mapInputStream = new ByteArrayInputStream(mapContents);
+
+        // WHEN
+        BankMap actualBankMap = MapParser.load(mapInputStream).parse();
+
+        // THEN
+        assertThat(actualBankMap).isNotNull();
+        assertThat(actualBankMap.getEntries()).hasSize(4);
+        assertThat(actualBankMap.getEntries()).extracting("hash").containsAll(asList(858241L, 1507153L, 1521845L, 1572722L));
+        assertThat(actualBankMap.getEntries()).extracting("size1").containsAll(asList(0L, 0L, 0L, 0L));
+        assertThat(actualBankMap.getEntries()).extracting("size2").containsAll(asList(0L, 0L, 0L, 0L));
     }
 }
