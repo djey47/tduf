eb7786236102793b0cd3f7493381cefeb86a6bef
djey <j.canler@gmail.com>
2014-12-12 23:28:52 +0100

    feature(cli) : add startup scripts.

---
+++ cli/src/main/shell/TDUF-intro.cmd
@@ -0,0 +1,8 @@
+@ECHO OFF
+ECHO Welcome to TDUF: Command Line Interface.
+ECHO.
+ECHO Available modules :
+ECHO - DatabaseTool
+ECHO - MappingTool
+ECHO.
+ECHO Enjoy!
---
+++ cli/src/main/shell/TDUF.cmd
@@ -0,0 +1,2 @@
+@ECHO OFF
+START "TDUF Command Line Interface" TDUF-intro.cmd
