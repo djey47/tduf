f63dede59ad3106679e6e91c7ed85f9f73d2bed5
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-01-20 15:56:55 +0100

    refactor(cli) : externalize common features to GenericTool

--- cli/src/main/java/fr/tduf/cli/tools/DatabaseTool.java
+++ cli/src/main/java/fr/tduf/cli/tools/DatabaseTool.java
@@ -3,7 +3,6 @@ package fr.tduf.cli.tools;
 import fr.tduf.cli.common.CommandHelper;
 import fr.tduf.libunlimited.low.files.db.DatabaseReadWriteHelper;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
-import org.kohsuke.args4j.Argument;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
@@ -11,25 +10,21 @@ import org.kohsuke.args4j.Option;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import static fr.tduf.cli.tools.DatabaseTool.Command.DUMP;
+import static java.util.Arrays.asList;
 
 /**
  * Command line interface for handling TDU database.
  */
-//TODO extends GenericTool
-public class DatabaseTool {
+public class DatabaseTool extends GenericTool {
 
     @Option(name = "-d", aliases = "--databaseDir", usage = "UNPACKED TDU database directory, defaults to current directory.")
     private String databaseDirectory;
 
     @Option(name = "-o", aliases = "--outputDir", usage = "Target directory for generated JSON files, defaults to current directory\\tdu-database-dump.")
     private String outputDirectory;
-
-    @Argument
-    private List<String> arguments = new ArrayList<>();
 
     private Command command;
 
@@ -70,80 +65,57 @@ public class DatabaseTool {
         new DatabaseTool().doMain(args);
     }
 
-    private void doMain(String[] args) throws FileNotFoundException {
-        if (!checkArgumentsAndOptions(args)) {
-            System.exit(1);
-        }
-
+    @Override
+    protected boolean commandDispatch() throws IOException {
         switch (command) {
             case DUMP:
                 dump();
                 break;
             default:
-                System.err.println("Error: command is not implemented, yet.");
-                System.exit(1);
-                break;
-        }
-    }
-
-    private boolean checkArgumentsAndOptions(String[] args) {
-        try {
-            CmdLineParser parser = new CmdLineParser(this);
-            parser.parseArgument(args);
-
-            checkCommand(parser);
-
-            // Database directory: defaulted to current
-            if (databaseDirectory == null) {
-                databaseDirectory = ".";
-            }
-
-            // Map file: defaulted to current directory\tdu-database-dump
-            if (outputDirectory == null) {
-                outputDirectory = "." + File.separator + "tdu-database-dump";
-            }
-        } catch (CmdLineException e) {
-            String displayedName = this.getClass().getSimpleName();
-
-            System.err.println(e.getMessage());
-            System.err.println("Syntax: " + displayedName + " command [-options]");
-            System.err.println();
-
-            System.err.println("  Commands:");
-            CommandHelper.getValuesAsMap(DUMP)
-                    .forEach((label, description) -> System.err.println(label + " : " + description));
-            System.err.println();
-
-            System.err.println("  Options:");
-            e.getParser().printUsage(System.err);
-            System.err.println();
-
-            System.err.println("  Example:");
-            System.err.println(displayedName + " " + DUMP.label + " --databaseDir \"C:\\Program Files (x86)\\Test Drive Unlimited\\Euro\\Bnk\\Database\"");
-            return false;
+                return false;
         }
         return true;
     }
 
-    private void checkCommand(CmdLineParser parser) throws CmdLineException {
-
-        if (arguments.isEmpty()) {
-            throw new CmdLineException(parser, "Error: No command is given.", null);
-        }
-
-        String commandArgument = arguments.get(0);
-
+    @Override
+    protected boolean checkAndAssignCommand(String commandArgument) throws CmdLineException {
         if (!CommandHelper.getLabels(DUMP).contains(commandArgument)) {
-            throw new CmdLineException(parser, "Error: An unsupported command is given.", null);
+            return false;
         }
 
         this.command = (Command) CommandHelper.fromLabel(DUMP, commandArgument);
+        return true;
+    }
+
+    @Override
+    protected void checkAndAssignDefaultParameters(CmdLineParser parser) throws CmdLineException {
+        // Database directory: defaulted to current
+        if (databaseDirectory == null) {
+            databaseDirectory = ".";
+        }
+
+        // Map file: defaulted to current directory\tdu-database-dump
+        if (outputDirectory == null) {
+            outputDirectory = "." + File.separator + "tdu-database-dump";
+        }
+    }
+
+    @Override
+    protected CommandHelper.CommandEnum getCommand() {
+        return Command.DUMP;
+    }
+
+    @Override
+    protected List<String> getExamples() {
+        return asList(
+                DUMP.label + " --databaseDir \"C:\\Program Files (x86)\\Test Drive Unlimited\\Euro\\Bnk\\Database\""
+        );
     }
 
     private void dump() throws FileNotFoundException {
         File outputDirectory = new File(this.outputDirectory);
         if (!outputDirectory.exists()) {
-            outputDirectory.mkdirs();
+            assert outputDirectory.mkdirs();
         }
 
         System.out.println("Dumping TDU database to JSON, please wait...");
