0ed3a8f011c873aaf12bec1d0175d7b48be2ef6d
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-02-17 15:48:37 +0100

    test(cli) : add integ test for MappingTool.

---
+++ cli/src/integTest/java/fr/tduf/cli/tools/MappingToolIntegTest.java
@@ -0,0 +1,69 @@
+package fr.tduf.cli.tools;
+
+import fr.tduf.libunlimited.low.files.banks.mapping.domain.BankMap;
+import fr.tduf.libunlimited.low.files.banks.mapping.writer.MapWriter;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class MappingToolIntegTest {
+
+    private final String mappingDirectory = "integ-tests/mapping";
+    private final String contentsDirectory1 = "integ-tests/mapping/cnt1";
+    private final String contentsDirectory2 = "integ-tests/mapping/cnt1/cnt2";
+
+    @Before
+    public void setUp() {
+        new File(mappingDirectory).mkdirs();
+        new File(contentsDirectory2).mkdirs();
+    }
+
+    @Test
+    public void info_ListMissing_FixMissing_List_whenEmptyInitialMap_andAddingNewFiles_shouldNotThrowError() throws IOException {
+        //GIVEN: empty map file
+        String emptyMapFilePath = createEmptyMapFile(mappingDirectory);
+
+        //WHEN: info
+        System.out.println("-> Info!");
+        MappingTool.main(new String[] { "info", "-b", mappingDirectory });
+
+
+        //GIVEN: 2 new files to bank directory
+        new File(contentsDirectory1, "c1").createNewFile();
+        new File(contentsDirectory2, "c2").createNewFile();
+
+        //WHEN: list-missing
+        System.out.println("-> List-missing!");
+        MappingTool.main(new String[]{"list-missing", "-b", mappingDirectory, "-m", emptyMapFilePath});
+
+
+        //WHEN: fix-missing
+        System.out.println("-> Fix-missing!");
+        MappingTool.main(new String[]{"fix-missing", "-b", mappingDirectory});
+
+
+        //WHEN: list
+        System.out.println("-> List!");
+        MappingTool.main(new String[]{"list", "-b", mappingDirectory});
+    }
+
+    private static String createEmptyMapFile(String mappingDirectory) throws IOException {
+        BankMap bankMap = new BankMap();
+        bankMap.setEntrySeparator(new byte[] { (byte)0xFE, 0x12, 0x00, 0x00});
+        bankMap.setTag("MAP4");
+        bankMap.addMagicEntry(0);
+
+        ByteArrayOutputStream mapOutputStream = MapWriter.load(bankMap).write();
+
+        File mapFileName = new File(mappingDirectory, "Bnk1.map");
+        String mapAbsolutePath = mapFileName.getAbsolutePath();
+        Files.write(Paths.get(mapAbsolutePath), mapOutputStream.toByteArray());
+
+        return mapAbsolutePath;
+    }
+}
