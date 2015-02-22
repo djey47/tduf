15d695a93268b7a8bb04dde063c0d00f447ee05d
djey <j.canler@gmail.com>
2015-02-22 01:29:32 +0100

    feature(database) : fix resource reference error.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/integrity/DatabaseIntegrityFixer.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/integrity/DatabaseIntegrityFixer.java
@@ -2,9 +2,11 @@ package fr.tduf.libunlimited.high.files.db.integrity;
 
 import fr.tduf.libunlimited.low.files.db.domain.IntegrityError;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
 
 import java.util.*;
 
+import static fr.tduf.libunlimited.low.files.db.domain.IntegrityError.ErrorInfoEnum.*;
 import static fr.tduf.libunlimited.low.files.db.domain.IntegrityError.ErrorTypeEnum.*;
 import static java.util.Arrays.asList;
 import static java.util.Objects.requireNonNull;
@@ -16,20 +18,19 @@ import static java.util.stream.Collectors.toMap;
  */
 public class DatabaseIntegrityFixer {
 
+    private static final String RESOURCE_REF_DEFAULT = "-FIXED BY TDUF-";
     private final List<DbDto> dbDtos;
-    private List<DbDto> fixedDbDtos = null;
 
     private final List<IntegrityError> integrityErrors;
 
     // Following errors are auto-handled: CONTENT_ITEMS_COUNT_MISMATCH, STRUCTURE_FIELDS_COUNT_MISMATCH
-    // Following errors are not handled yet: CONTENTS_FIELDS_COUNT_MISMATCH, RESOURCE_NOT_FOUND, RESOURCE_REFERENCE_NOT_FOUND, RESOURCE_ITEMS_COUNT_MISMATCH
-    private static final Set<IntegrityError.ErrorTypeEnum> FIXABLE_ERRORS = new HashSet<>(asList(CONTENTS_REFERENCE_NOT_FOUND));
+    // Following errors are not handled yet: CONTENTS_FIELDS_COUNT_MISMATCH, RESOURCE_NOT_FOUND, CONTENTS_REFERENCE_NOT_FOUND, RESOURCE_ITEMS_COUNT_MISMATCH
+    private static final Set<IntegrityError.ErrorTypeEnum> FIXABLE_ERRORS = new HashSet<>(asList(RESOURCE_REFERENCE_NOT_FOUND));
     private static final Set<IntegrityError.ErrorTypeEnum> UNFIXABLE_ERRORS = new HashSet<>(asList(CONTENTS_NOT_FOUND, CONTENTS_ENCRYPTION_NOT_SUPPORTED));
 
 
     private DatabaseIntegrityFixer(List<DbDto> dbDtos, List<IntegrityError> integrityErrors) {
         this.dbDtos = dbDtos;
-        this.fixedDbDtos = dbDtos;
         this.integrityErrors = integrityErrors;
     }
 
@@ -95,7 +96,48 @@ public class DatabaseIntegrityFixer {
     }
 
     private boolean fixIntegrityError(IntegrityError integrityError) {
-        return false;
+
+        try {
+            Map<IntegrityError.ErrorInfoEnum, Object> information = integrityError.getInformation();
+
+            switch(integrityError.getErrorTypeEnum()) {
+                case RESOURCE_REFERENCE_NOT_FOUND:
+                    DbDto.Topic remoteTopic = (DbDto.Topic) information.get(REMOTE_TOPIC);
+                    DbResourceDto.Locale locale = (DbResourceDto.Locale) information.get(LOCALE);
+                    String reference = (String) information.get(REFERENCE);
+
+                    fixResourceReference(reference, remoteTopic, locale);
+                    break;
+                default:
+                    throw new IllegalArgumentException("Integrity error type not handled yet: " + integrityError.getErrorTypeEnum());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void fixResourceReference(String reference, DbDto.Topic topic, DbResourceDto.Locale locale) {
+
+        DbResourceDto resourceDto = this.dbDtos.stream()
+
+                .filter((databaseObject) -> databaseObject.getStructure().getTopic() == topic)
+
+                .findFirst().get().getResources().stream()
+
+                    .filter((resourceObject) -> resourceObject.getLocale() == locale)
+
+                    .findFirst().get();
+
+        DbResourceDto.Entry newEntry = DbResourceDto.Entry.builder()
+                .forReference(reference)
+                .withValue(RESOURCE_REF_DEFAULT)
+                .build();
+
+        //TODO check if adding at the end is enough
+        resourceDto.getEntries().add(newEntry);
     }
 
     private static void checkRequirements(List<DbDto> dbDtos, List<IntegrityError> integrityErrors) {
@@ -104,7 +146,7 @@ public class DatabaseIntegrityFixer {
     }
 
     public List<DbDto> getFixedDbDtos() {
-        return fixedDbDtos;
+        return dbDtos;
     }
 
     List<DbDto> getDbDtos() {
--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/integrity/DatabaseIntegrityFixerTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/integrity/DatabaseIntegrityFixerTest.java
@@ -3,14 +3,18 @@ package fr.tduf.libunlimited.high.files.db.integrity;
 import fr.tduf.libunlimited.low.files.db.domain.IntegrityError;
 import fr.tduf.libunlimited.low.files.db.dto.DbDataDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic;
+import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto.Locale;
+import fr.tduf.libunlimited.low.files.db.dto.DbStructureDto;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import static fr.tduf.libunlimited.low.files.db.domain.IntegrityError.ErrorTypeEnum.CONTENTS_NOT_FOUND;
-import static fr.tduf.libunlimited.low.files.db.domain.IntegrityError.ErrorTypeEnum.CONTENT_ITEMS_COUNT_MISMATCH;
+import static fr.tduf.libunlimited.low.files.db.domain.IntegrityError.ErrorInfoEnum.*;
+import static fr.tduf.libunlimited.low.files.db.domain.IntegrityError.ErrorTypeEnum.*;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -101,10 +105,58 @@ public class DatabaseIntegrityFixerTest {
         assertThat(actualRemainingErrors).isEqualTo(integrityErrors);
     }
 
+    @Test
+    public void fixAllContentsObjects_whenOneError_asLocalResourceReferenceNotFound_shouldInsertMissingResource() {
+        // GIVEN
+        List<DbDto> dbDtos = createDefaultDatabaseObjects();
+
+        HashMap<IntegrityError.ErrorInfoEnum, Object> info = new HashMap<>();
+        info.put(SOURCE_TOPIC, Topic.ACHIEVEMENTS);
+        info.put(REMOTE_TOPIC, Topic.ACHIEVEMENTS);
+        info.put(LOCALE, Locale.FRANCE);
+        info.put(REFERENCE, "123456");
+        List<IntegrityError> integrityErrors = asList(IntegrityError.builder().ofType(RESOURCE_REFERENCE_NOT_FOUND).addInformations(info).build());
+
+
+        // WHEN
+        DatabaseIntegrityFixer integrityFixer = DatabaseIntegrityFixer.load(dbDtos, integrityErrors);
+        List<IntegrityError> actualRemainingErrors = integrityFixer.fixAllContentsObjects();
+        List<DbDto> fixedDatabaseObjects = integrityFixer.getFixedDbDtos();
+
+
+        // THEN
+        assertThat(actualRemainingErrors).isEmpty();
+
+        assertThat(fixedDatabaseObjects).isNotEmpty();
+
+        DbResourceDto.Entry createdEntry = fixedDatabaseObjects.stream()
+
+                .filter((databaseObject) -> databaseObject.getStructure().getTopic() == Topic.ACHIEVEMENTS)
+
+                .findFirst().get().getResources().stream()
+
+                    .filter((resourceObject) -> resourceObject.getLocale() == Locale.FRANCE)
+
+                    .findFirst().get().getEntries().stream()
+
+                            .filter((entry) -> entry.getReference().equals("123456"))
+
+                            .findFirst().orElse(null);
+
+        assertThat(createdEntry).isNotNull();
+        assertThat(createdEntry.getValue()).isEqualTo("-FIXED BY TDUF-");
+    }
+
     private List<DbDto> createDefaultDatabaseObjects() {
         List<DbDto> dbDtos = new ArrayList<>();
         dbDtos.add(DbDto.builder()
+                .withStructure(DbStructureDto.builder()
+                        .forTopic(Topic.ACHIEVEMENTS)
+                        .build())
                 .withData(DbDataDto.builder()
+                        .build())
+                .addResource(DbResourceDto.builder()
+                        .withLocale(Locale.FRANCE)
                         .build())
                 .build());
         return dbDtos;
