d81b29f9b21198e0aaf1c657739d205a3ecfe458
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-03-04 10:52:04 +0100

    test(cli) : add tests for new operations.

--- cli/src/test/java/fr/tduf/cli/tools/AllToolsTest.java
+++ cli/src/test/java/fr/tduf/cli/tools/AllToolsTest.java
@@ -86,6 +86,22 @@ public class AllToolsTest {
     }
 
     @Test
+    public void fileTool_unpack() throws NoSuchFieldException, IOException {
+        // GIVEN-WHEN-THEN
+        exitRule.expectSystemExitWithStatus(1);
+
+        testToolCommand(new FileTool(), UNPACK);
+    }
+
+    @Test
+    public void fileTool_repack() throws NoSuchFieldException, IOException {
+        // GIVEN-WHEN-THEN
+        exitRule.expectSystemExitWithStatus(1);
+
+        testToolCommand(new FileTool(), REPACK);
+    }
+
+    @Test
     public void mappingTool_info() throws NoSuchFieldException, IOException {
         // GIVEN-WHEN-THEN
         exitRule.expectSystemExitWithStatus(1);
