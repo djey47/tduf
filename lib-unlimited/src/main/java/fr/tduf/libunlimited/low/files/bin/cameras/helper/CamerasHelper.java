39045265276e608b586edee0e6fe38d984a828db
djey <j.canler@gmail.com>
2015-03-19 01:17:28 +0100

    feature(cameras) : implement set duplication - WIP.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/bin/cameras/helper/CamerasHelper.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/bin/cameras/helper/CamerasHelper.java
@@ -1,4 +1,33 @@
 package fr.tduf.libunlimited.low.files.bin.cameras.helper;
 
+import fr.tduf.libunlimited.low.files.bin.cameras.rw.CamerasParser;
+
+import static java.util.Objects.requireNonNull;
+
 public class CamerasHelper {
+
+    /**
+     *
+     * @param sourceCameraId
+     * @param targetCameraId
+     * @param parser
+     */
+    public static void duplicateCameraSet(long sourceCameraId, long targetCameraId, CamerasParser parser) {
+        requireNonNull(parser, "Parser with cameras contents is required.");
+
+        int currentIndexEntryCount = parser.getCameraIndex().size();
+        short viewCount = parser.getCameraIndex().get(sourceCameraId);
+        parser.getDataStore().addRepeatedIntegerValue("index", "cameraId", currentIndexEntryCount, targetCameraId);
+        parser.getDataStore().addRepeatedIntegerValue("index", "viewCount", currentIndexEntryCount, viewCount);
+
+        int currentViewsCount = parser.getCameraViews().values().size();
+
+        // TODO copy all sets
+//        parser.getCameraViews().get(sourceCameraId).stream()
+//
+//                .forEach( (viewStore) -> {
+//                            parser.getDataStore().
+//                        }
+//                );
+    }
 }
---
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/bin/cameras/helper/CamerasHelperTest.java
@@ -0,0 +1,53 @@
+package fr.tduf.libunlimited.low.files.bin.cameras.helper;
+
+import fr.tduf.libunlimited.low.files.bin.cameras.rw.CamerasParser;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class CamerasHelperTest {
+
+    private static final Class<CamerasHelperTest> thisClass = CamerasHelperTest.class;
+
+    private CamerasParser parser;
+
+    @Before
+    public void setUp() throws URISyntaxException, IOException {
+        URI uri = thisClass.getResource("/bin/Cameras.bin").toURI();
+        byte[] camContents = Files.readAllBytes(Paths.get(uri));
+        ByteArrayInputStream camInputStream = new ByteArrayInputStream(camContents);
+
+        parser = CamerasParser.load(camInputStream);
+        parser.parse();
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void duplicateCameraSet_whenNullParser_shouldThrowNullPointerException() throws Exception {
+        // GIVEN-WHEN
+        CamerasHelper.duplicateCameraSet(1, 1001, null);
+
+        // THEN: NPE
+
+    }
+
+    @Test
+    @Ignore
+    public void duplicateCameraSet_whenSourceExists_shouldAddSet() throws Exception {
+        // GIVEN-WHEN
+        CamerasHelper.duplicateCameraSet(1, 1001, parser);
+
+        // THEN
+        assertThat(parser.getCameraIndex()).hasSize(152);
+        assertThat(parser.getCameraViews()).hasSize(150);
+    }
+}
