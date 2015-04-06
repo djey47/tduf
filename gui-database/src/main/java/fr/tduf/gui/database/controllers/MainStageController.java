bdc280d00f16128e4e0639ab3c50cf263361c960
djey <j.canler@gmail.com>
2015-04-07 00:59:30 +0200

    feature(gui/database) : set up MVC mapping.

---
+++ gui-database/src/main/java/fr/tduf/gui/database/controllers/MainStageController.java
@@ -0,0 +1,66 @@
+package fr.tduf.gui.database.controllers;
+
+import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import fr.tduf.libunlimited.low.files.db.rw.helper.DatabaseReadWriteHelper;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+
+/**
+ * Makes it a possible to intercept all GUI events.
+ */
+public class MainStageController {
+
+    private Stage mainStage;
+
+    @FXML
+    private TitledPane settingsPane;
+
+    @FXML
+    private ChoiceBox localesChoiceBox;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private TextField databaseLocationTextField;
+
+
+    private List<DbDto> databaseObjects;
+
+    @FXML
+    public void handleLoadButtonMouseClick(ActionEvent actionEvent) {
+        System.out.println("handleLoadButtonMouseClick");
+
+        String databaseLocation = getDatabaseLocationTextField().getText();
+        if (StringUtils.isNotEmpty(databaseLocation)) {
+            this.databaseObjects = DatabaseReadWriteHelper.readFullDatabaseFromJson(databaseLocation);
+        }
+    }
+
+    public TitledPane getSettingsPane() {
+        return this.settingsPane;
+    }
+
+    public TextField getDatabaseLocationTextField() {
+        return this.databaseLocationTextField;
+    }
+
+    public ChoiceBox getLocalesChoiceBox() {
+        return this.localesChoiceBox;
+    }
+
+    public void setMainStage(Stage stage) {
+        this.mainStage = stage;
+    }
+}
--- gui-database/src/main/java/fr/tduf/gui/database/stages/MainStageDesigner.java
+++ gui-database/src/main/java/fr/tduf/gui/database/stages/MainStageDesigner.java
@@ -1,12 +1,16 @@
 package fr.tduf.gui.database.stages;
 
+import fr.tduf.gui.database.controllers.MainStageController;
+import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
+import static java.util.Arrays.asList;
 
 /**
  * Loads graphical interface for main window.
@@ -20,16 +24,40 @@ public class MainStageDesigner {
      * @param primaryStage
      */
     public static void init(Stage primaryStage) throws IOException {
+        FXMLLoader mainLoader = new FXMLLoader(thisClass.getResource("/designer/MainDesigner.fxml"));
+        Parent mainRoot = mainLoader.load();
+        MainStageController mainController = mainLoader.<MainStageController>getController();
+        mainController.setMainStage(primaryStage);
 
+        initMainWindow(primaryStage, mainRoot);
+
+        initSettingsPane(mainController);
+
+        // DEBUG
+        mainController.getDatabaseLocationTextField().setText("/media/DevStore/GIT/tduf/cli/integ-tests/db-json/");
+    }
+
+    private static void initMainWindow(Stage primaryStage, Parent mainRoot) {
         String styledToolBarCss = thisClass.getResource("/css/ToolBars.css").toExternalForm();
-        Parent parent = FXMLLoader.load(thisClass.getResource("/designer/MainDesigner.fxml"));
-        parent.getStylesheets().add(styledToolBarCss);
+        mainRoot.getStylesheets().add(styledToolBarCss);
 
-        primaryStage.setScene(new Scene(parent, 1280, 768));
+        primaryStage.setScene(new Scene(mainRoot, 1280, 768));
         primaryStage.setTitle("TDUF Database Editor");
         primaryStage.setResizable(false);
+    }
 
-        TitledPane settingsPane = (TitledPane) primaryStage.getScene().lookup("#settingsPane");
-        settingsPane.setExpanded(false);
+    private static void initSettingsPane(MainStageController controller) {
+        fillLocales(controller);
+
+        controller.getSettingsPane().setExpanded(false);
+    }
+
+    private static void fillLocales(MainStageController controller) {
+        ChoiceBox localesChoiceBox = controller.getLocalesChoiceBox();
+
+        asList(DbResourceDto.Locale.values())
+                .forEach((locale) -> localesChoiceBox.getItems().add(locale));
+
+        localesChoiceBox.setValue(DbResourceDto.Locale.UNITED_STATES);
     }
 }
