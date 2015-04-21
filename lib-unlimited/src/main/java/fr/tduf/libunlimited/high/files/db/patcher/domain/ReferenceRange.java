4ceb969a4c85dea9e96784e4fd06e77a1b8f0fd2
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-21 18:15:15 +0200

    feature(lib/database) : initialize Range object - WIP.

---
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/patcher/domain/ReferenceRange.java
@@ -0,0 +1,54 @@
+package fr.tduf.libunlimited.high.files.db.patcher.domain;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Represents a range of entry references for patch generation.
+ */
+public class ReferenceRange {
+
+    private final Optional<String> minRef;
+    private final Optional<String> maxRef;
+    private final List<String> refs;
+
+    /**
+     *
+     * @param rangeOptionValue
+     * @return
+     */
+    public static ReferenceRange fromCliOption(Optional<String> rangeOptionValue) {
+        if (!rangeOptionValue.isPresent()) {
+            return new ReferenceRange(Optional.<String>empty(), Optional.<String>empty());
+        }
+
+        return null; //TODO
+    }
+
+    /**
+     *
+     * @param ref
+     * @return
+     */
+    public boolean isAccepted(String ref) {
+        return false; //TODO
+    }
+
+    ReferenceRange(List<String> refs) {
+        this.refs = refs;
+        this.minRef = Optional.empty();
+        this.maxRef = Optional.empty();
+    }
+
+    ReferenceRange(Optional<String> minRef, Optional<String> maxRef) {
+        this.refs = null;
+        this.minRef = minRef;
+        this.maxRef = maxRef;
+    }
+
+    boolean isGlobal() {
+        return !this.minRef.isPresent()
+                && !this.maxRef.isPresent()
+                && this.refs == null;
+    }
+}
