bdd7fe5357fd5e664eec4d2760a7fce116bee8c2
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-23 13:59:54 +0200

    feature(lib/database) : implement reference range.

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/domain/ReferenceRangeTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/domain/ReferenceRangeTest.java
@@ -13,7 +13,7 @@ public class ReferenceRangeTest {
     @Test
     public void isGlobal_whenBothMinAndMaxEmpty_shouldReturnTrue() {
         // GIVEN-WHEN
-        ReferenceRange actualRange = new ReferenceRange(Optional.<String>empty(), Optional.<String>empty());
+        ReferenceRange actualRange = new ReferenceRange(Optional.<Long>empty(), Optional.<Long>empty());
 
         //THEN
         assertThat(actualRange.isGlobal()).isTrue();
@@ -22,7 +22,7 @@ public class ReferenceRangeTest {
     @Test
     public void isGlobal_whenMinEmpty_shouldReturnFalse() {
         // GIVEN-WHEN
-        ReferenceRange actualRange = new ReferenceRange(Optional.<String>empty(), Optional.of("1000000"));
+        ReferenceRange actualRange = new ReferenceRange(Optional.<Long>empty(), Optional.of(1000000L));
 
         //THEN
         assertThat(actualRange.isGlobal()).isFalse();
@@ -55,18 +55,42 @@ public class ReferenceRangeTest {
         // THEN: IAE
     }
 
-//    @Test(expected = IllegalArgumentException.class)
-//    public void fromCliOption_whenValueWithIllegalBounds_shouldThrowException() {
-//        // GIVEN-WHEN
-//        ReferenceRange.fromCliOption(Optional.of("1..0"));
-//
-//        // THEN: IAE
-//    }
+    @Test(expected = IllegalArgumentException.class)
+    public void fromCliOption_whenValueWithIllegalBounds_shouldThrowException() {
+        // GIVEN-WHEN
+        ReferenceRange.fromCliOption(Optional.of("1..0"));
+
+        // THEN: IAE
+    }
+
+    @Test
+    public void fromCliOption_whenValueWithValidBounds_shouldReturnRange() {
+        // GIVEN-WHEN
+        ReferenceRange actualRange = ReferenceRange.fromCliOption(Optional.of("1..10"));
+
+        // THEN
+        assertThat(actualRange).isNotNull();
+        assertThat(actualRange.getMinRef().get()).isEqualTo(1L);
+        assertThat(actualRange.getMaxRef().get()).isEqualTo(10L);
+        assertThat(actualRange.getRefs()).isNull();
+    }
+
+    @Test
+    public void fromCliOption_whenValueWithEnumeration_shouldReturnRange() {
+        // GIVEN-WHEN
+        ReferenceRange actualRange = ReferenceRange.fromCliOption(Optional.of("1,2,3,4"));
+
+        // THEN
+        assertThat(actualRange).isNotNull();
+        assertThat(actualRange.getMinRef()).isEmpty();
+        assertThat(actualRange.getMaxRef()).isEmpty();
+        assertThat(actualRange.getRefs()).containsExactly("1", "2", "3", "4");
+    }
 
     @Test
     public void accepts_whenGlobalRange_shouldReturnTrue(){
-        // GIVEN
-        ReferenceRange actualRange = new ReferenceRange(Optional.<String>empty(), Optional.<String>empty());
+        // Long
+        ReferenceRange actualRange = new ReferenceRange(Optional.<Long>empty(), Optional.<Long>empty());
 
         // WHEN-THEN
         assertThat(actualRange.accepts("12345678")).isTrue();
@@ -82,7 +106,7 @@ public class ReferenceRangeTest {
     }
 
     @Test
-    public void accepts_whenRefNotInList_shouldReturnTrue(){
+    public void accepts_whenRefNotInList_shouldReturnFalse(){
         // GIVEN
         ReferenceRange actualRange = new ReferenceRange(new ArrayList<>());
 
@@ -90,4 +114,39 @@ public class ReferenceRangeTest {
         assertThat(actualRange.accepts("12345678")).isFalse();
     }
 
+    @Test
+    public void accepts_whenRefInRange_shouldReturnTrue(){
+        // GIVEN
+        ReferenceRange actualRange = new ReferenceRange(Optional.of(1L), Optional.of(1000L));
+
+        // WHEN-THEN
+        assertThat(actualRange.accepts("500")).isTrue();
+    }
+
+    @Test
+    public void accepts_whenLowerUnbounded_andRefInRange_shouldReturnTrue(){
+        // GIVEN
+        ReferenceRange actualRange = new ReferenceRange(Optional.empty(), Optional.of(1000L));
+
+        // WHEN-THEN
+        assertThat(actualRange.accepts("500")).isTrue();
+    }
+
+    @Test
+    public void accepts_whenUpperUnbounded_andRefInRange_shouldReturnTrue(){
+        // GIVEN
+        ReferenceRange actualRange = new ReferenceRange(Optional.of(1L), Optional.empty());
+
+        // WHEN-THEN
+        assertThat(actualRange.accepts("12345678")).isTrue();
+    }
+
+    @Test
+    public void accepts_whenRefNotInRange_shouldReturnFalse(){
+        // GIVEN
+        ReferenceRange actualRange = new ReferenceRange(Optional.of(1L), Optional.of(1000L));
+
+        // WHEN-THEN
+        assertThat(actualRange.accepts("12345678")).isFalse();
+    }
 }
