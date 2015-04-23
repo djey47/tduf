4dba7d8ea5cfae993cdd3c42ccdf3cda0bf33c52
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-23 16:21:01 +0200

    feature(lib/database) : add method to retrieve ref field from a field list.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/db/rw/helper/DatabaseStructureQueryHelper.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/db/rw/helper/DatabaseStructureQueryHelper.java
@@ -3,7 +3,10 @@ package fr.tduf.libunlimited.low.files.db.rw.helper;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbStructureDto;
 
+import java.util.List;
 import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Class providing methods to query database structure.
@@ -16,11 +19,23 @@ public class DatabaseStructureQueryHelper {
      * @return searched field, or empty if it does not exist.
      */
     public static Optional<DbStructureDto.Field> getIdentifierField(DbDto topicObject) {
+        // TODO throw exception (requirenonnull)
         if (topicObject == null) {
             return null;
         }
 
-        return topicObject.getStructure().getFields().stream()
+        return getIdentifierField(topicObject.getStructure().getFields());
+    }
+
+    /**
+     * Finds structure field used for entry identification.
+     * @param structureFields   : database topic to search for such a field
+     * @return searched field, or empty if it does not exist.
+     */
+    public static Optional<DbStructureDto.Field> getIdentifierField(List<DbStructureDto.Field> structureFields) {
+        requireNonNull(structureFields, "A list of fields is required.");
+
+        return structureFields.stream()
 
                 .filter((field) -> field.getFieldType() == DbStructureDto.FieldType.UID)
 
