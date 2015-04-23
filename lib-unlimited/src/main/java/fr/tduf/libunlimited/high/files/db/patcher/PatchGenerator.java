8f40e38fa9129d9da5530c3f7d1d0f9f3a204644
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-23 14:04:14 +0200

    feature(lib/database) : implement patch generator - WIP.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/patcher/PatchGenerator.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/patcher/PatchGenerator.java
@@ -5,18 +5,46 @@ import fr.tduf.libunlimited.high.files.db.patcher.domain.ReferenceRange;
 import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
+import static java.util.Objects.requireNonNull;
+
 /**
  * Used to generate patches from an existing database.
  */
 public class PatchGenerator extends AbstractDatabaseHolder {
 
+    private DbDto topicObject;
+
     /**
-     *
-     * @param topic
-     * @param range
-     * @return
+     * Generates a patch based on current database objects.
+     * @param topic : database topic for which the patch should apply
+     * @param range : range of reference values for concerned entries.
+     * @return a patch object with all necessary instructions.
      */
     public DbPatchDto makePatch(DbDto.Topic topic, ReferenceRange range) {
-        return null;
+        requireNonNull(topic, "A database topic is required.");
+        requireNonNull(range, "A reference range is required.");
+
+        this.topicObject = checkTopic(topic);
+
+        return DbPatchDto.builder()
+                .addChanges(new ArrayList<>())
+                .build();
+    }
+
+    private DbDto checkTopic(DbDto.Topic topic) {
+        Optional<DbDto> potentielTopicObject = databaseMiner.getDatabaseTopic(topic);
+
+        if (!potentielTopicObject.isPresent()) {
+            throw new IllegalArgumentException("Topic not found in provided database: " + topic);
+        }
+
+        return potentielTopicObject.get();
+    }
+
+    DbDto getTopicObject() {
+        return topicObject;
     }
 }
