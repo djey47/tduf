765fabf116325b846271977f803391bb59e4c0bd
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-23 14:04:14 +0200

    feature(lib/database) : implement patch generator - WIP.

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/PatchGeneratorTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/PatchGeneratorTest.java
@@ -1,5 +1,68 @@
 package fr.tduf.libunlimited.high.files.db.patcher;
 
+import fr.tduf.libunlimited.high.files.db.common.AbstractDatabaseHolder;
+import fr.tduf.libunlimited.high.files.db.patcher.domain.ReferenceRange;
+import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbStructureDto;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Collections.singletonList;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PatchGeneratorTest {
 
+    @Test(expected = NullPointerException.class)
+    public void makePatch_whenNullArguments_shouldThrowException() throws ReflectiveOperationException {
+        // GIVEN
+        PatchGenerator generator = createPatchGenerator(new ArrayList<>());
+
+        // WHEN
+        generator.makePatch(null, null);
+
+        // THEN: NPE
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void makePatch_whenTopicNotFound_shouldThrowException() throws ReflectiveOperationException {
+        // GIVEN
+        PatchGenerator generator = createPatchGenerator(new ArrayList<>());
+
+        // WHEN
+        generator.makePatch(DbDto.Topic.ACHIEVEMENTS, ReferenceRange.fromCliOption(Optional.empty()));
+
+        // THEN: IAE
+    }
+
+    @Test
+    public void makePatch_whenTopicFound_shouldSetTopicObject_andReturnPatchObject() throws ReflectiveOperationException {
+        // GIVEN
+        List<DbDto> databaseObjects = createDatabaseObjectsWithOneTopic();
+        PatchGenerator generator = createPatchGenerator(databaseObjects);
+
+        // WHEN
+        DbPatchDto actualPatchObject = generator.makePatch(DbDto.Topic.ACHIEVEMENTS, ReferenceRange.fromCliOption(Optional.empty()));
+
+        // THEN
+        assertThat(generator.getTopicObject()).isNotNull();
+        assertThat(actualPatchObject).isNotNull();
+    }
+
+    private static PatchGenerator createPatchGenerator(List<DbDto> databaseObjects) throws ReflectiveOperationException {
+        return AbstractDatabaseHolder.prepare(PatchGenerator.class, databaseObjects);
+    }
+
+    private static List<DbDto> createDatabaseObjectsWithOneTopic() {
+        DbDto topicObject = DbDto.builder()
+                .withStructure(DbStructureDto.builder()
+                    .forTopic(DbDto.Topic.ACHIEVEMENTS)
+                    .build())
+                .build();
+
+        return singletonList(topicObject);
+    }
 }
