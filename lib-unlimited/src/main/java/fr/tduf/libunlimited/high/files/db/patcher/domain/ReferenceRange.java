4cb7eaffa3574179850fc048d054e657979a9055
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-23 13:59:54 +0200

    feature(lib/database) : implement reference range.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/patcher/domain/ReferenceRange.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/patcher/domain/ReferenceRange.java
@@ -4,36 +4,55 @@ import java.util.List;
 import java.util.Optional;
 import java.util.regex.Pattern;
 
+import static java.util.Arrays.asList;
+
 /**
  * Represents a range of entry references for patch generation.
  */
 public class ReferenceRange {
 
-    private static final String REGEX_RANGE = "\\d(\\d,)*|\\d*\\.\\.\\d*";
+    private static final String REGEX_RANGE = "\\d(,\\d)*|\\d*\\.\\.\\d*";
 
-    private final Optional<String> minRef;
-    private final Optional<String> maxRef;
+    private final Optional<Long> minRef;
+    private final Optional<Long> maxRef;
     private final List<String> refs;
 
     /**
      * Creates a range from a command-line option.
      * e.g: 1,2,3 or 1..3
      */
-    public static ReferenceRange fromCliOption(Optional<String> rangeOptionValue) {
-        if (!rangeOptionValue.isPresent()) {
-            return new ReferenceRange(Optional.<String>empty(), Optional.<String>empty());
+    public static ReferenceRange fromCliOption(Optional<String> potentialRangeOptionValue) {
+        if (!potentialRangeOptionValue.isPresent()) {
+            return new ReferenceRange(Optional.<Long>empty(), Optional.<Long>empty());
         }
 
-        checkValueFormat(rangeOptionValue.get());
+        String rangeOptionValue = potentialRangeOptionValue.get();
+        checkValueFormat(rangeOptionValue);
 
-        return null; //TODO
+        if (rangeOptionValue.contains("..")) {
+            return fromBounds(rangeOptionValue);
+        }
+
+        return fromEnumerated(rangeOptionValue);
     }
 
     /**
-     * @return true if provided ref enters current range.
+     * @return true if provided ref enters current range, false otherwise.
      */
     public boolean accepts(String ref) {
-        return isGlobal() || refs.contains(ref);
+
+        if (isGlobal()) {
+            return true;
+        }
+
+        if (refs != null) {
+            return  refs.contains(ref);
+        }
+
+        long reference = Long.parseLong(ref);
+        return (!minRef.isPresent() && reference <= maxRef.get())
+                || (!maxRef.isPresent() && reference >= minRef.get())
+                || (reference >= minRef.get() && reference <= maxRef.get());
     }
 
     ReferenceRange(List<String> refs) {
@@ -42,7 +61,7 @@ public class ReferenceRange {
         this.maxRef = Optional.empty();
     }
 
-    ReferenceRange(Optional<String> minRef, Optional<String> maxRef) {
+    ReferenceRange(Optional<Long> minRef, Optional<Long> maxRef) {
         this.refs = null;
         this.minRef = minRef;
         this.maxRef = maxRef;
@@ -60,5 +79,34 @@ public class ReferenceRange {
         if (!rangePattern.matcher(rangeOptionValue).matches()) {
             throw new IllegalArgumentException("Unrecognized range value: " + rangeOptionValue);
         }
+    }
+
+    private static ReferenceRange fromEnumerated(String enumeratedRange) {
+        String[] items = enumeratedRange.split(",");
+        return new ReferenceRange(asList(items));
+    }
+
+    private static ReferenceRange fromBounds(String boundedRange) {
+        String[] bounds = boundedRange.split("\\.\\.");
+        long lowerBound = Integer.valueOf(bounds[0]);
+        long upperBound = Integer.valueOf(bounds[1]);
+
+        if (upperBound < lowerBound) {
+            throw new IllegalArgumentException("Invalid range: " + lowerBound + ".." + upperBound);
+        }
+
+        return new ReferenceRange(Optional.of(lowerBound), Optional.of(upperBound));
+    }
+
+    Optional<Long> getMinRef() {
+        return minRef;
+    }
+
+    Optional<Long> getMaxRef() {
+        return maxRef;
+    }
+
+    List<String> getRefs() {
+        return refs;
     }
 }
