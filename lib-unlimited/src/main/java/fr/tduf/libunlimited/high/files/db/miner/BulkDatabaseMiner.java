6d81863b99db4b650d4442629c2b147b7c09dc2e
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-27 11:29:34 +0200

    refactor(lib/database) : add TODO.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/miner/BulkDatabaseMiner.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/miner/BulkDatabaseMiner.java
@@ -14,6 +14,7 @@ import static java.util.Objects.requireNonNull;
 /**
  * Class providing utility methods to request data from database objects.
  */
+// TODO use debug logs to get performance info
 public class BulkDatabaseMiner {
 
     private final List<DbDto> topicObjects;
