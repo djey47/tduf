1c456de23f848a5064d78e41a8b4159938b1e32b
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-27 14:35:40 +0200

    test(cli) : add method to assert json contents between 2 files.

--- cli/src/test/java/fr/tduf/cli/common/helper/AssertionsHelper.java
+++ cli/src/test/java/fr/tduf/cli/common/helper/AssertionsHelper.java
@@ -1,7 +1,10 @@
 package fr.tduf.cli.common.helper;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 
 import static net.sf.json.test.JSONAssert.assertJsonEquals;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  *
  */
-// TODO move to lib-unlimited module
+// TODO Merge with AssertionsHelper from lib-unlimited module
 public class AssertionsHelper {
 
     /**
@@ -43,6 +46,34 @@ public class AssertionsHelper {
     public static void assertOutputStreamContainsSequence(OutputStream outputStream, String... expectedItems) throws IOException {
         finalizeOutputStream(outputStream);
         assertThat(outputStream.toString()).containsSequence(expectedItems);
+    }
+
+    /**
+     * Checks if specified JSON files contents are the same.
+     * @param fileName1 : path and file name
+     * @param fileName2 : path and file name
+     */
+    public static void assertJsonFilesMatch(String fileName1, String fileName2) throws IOException {
+        String json1 = assertAndReadJsonFileContents(fileName1);
+        String json2 = assertAndReadJsonFileContents(fileName2);
+
+        assertJsonEquals("Files " + fileName1 + " and " + fileName2 + " must match: ", json1, json2);
+    }
+
+    /**
+     * Checks if specified file exists or not and return {@link java.io.File} instance if OK.
+     * @param fileName  : path and file name
+     */
+    public static File assertFileExistAndGet(String fileName) {
+        File actualContentsFile = new File(fileName);
+        assertThat(actualContentsFile.exists()).describedAs("File must exist: " + actualContentsFile.getPath()).isTrue();
+        return actualContentsFile;
+    }
+
+    private static String assertAndReadJsonFileContents(String fileName1) throws IOException {
+        File contentsFile1 = assertFileExistAndGet(fileName1);
+        byte[] encoded1 = Files.readAllBytes(contentsFile1.toPath());
+        return new String(encoded1, Charset.forName("UTF-8"));
     }
 
     private static void finalizeOutputStream(OutputStream outputStream) throws IOException {
