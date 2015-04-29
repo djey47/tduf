eef7e98842e947526ce1d9a8adcc1aa17bf4d86f
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-04-29 16:27:04 +0200

    feature(lib/db/interop) : implement tduf=>tdu conversion for resources updates.

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/interop/PatchConverterTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/interop/PatchConverterTest.java
@@ -3,7 +3,6 @@ package fr.tduf.libunlimited.high.files.db.interop;
 
 import fr.tduf.libunlimited.common.helper.FilesHelper;
 import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -66,7 +65,7 @@ public class PatchConverterTest {
         // THEN
         assertThat(actualDocument).isNotNull();
 
-        NodeList instructions = assertStructureAndReturnInstructions(actualDocument);
+        NodeList instructions = assertStructureAndReturnInstructions(actualDocument, 2);
 
         Element instruction1 = (Element) instructions.item(0);
         assertUpdateDatabaseInstruction(instruction1, "CarPhysicsData", "1221657049|1221657049\t864426\t56338407\t");
@@ -75,19 +74,57 @@ public class PatchConverterTest {
         assertUpdateDatabaseInstruction(instruction2, "CarPhysicsData", "606298799|606298799\t864426\t56338407\t");
     }
 
-    private static NodeList assertStructureAndReturnInstructions(Document actualDocument) {
+    @Test
+    public void jsonToPch_whenRealPatchObject_ForResources_shouldReturnPatch() throws IOException, URISyntaxException, ParserConfigurationException, SAXException {
+        // GIVEN
+        DbPatchDto patchObject = FilesHelper.readObjectFromJsonResourceFile(DbPatchDto.class, "/db/patch/updateResources.mini.json");
+
+
+        // WHEN
+        Document actualDocument = PatchConverter.jsonToPch(patchObject);
+
+
+        // THEN
+        assertThat(actualDocument).isNotNull();
+
+        NodeList instructions = assertStructureAndReturnInstructions(actualDocument, 2);
+
+        Element instruction1 = (Element) instructions.item(0);
+        assertUpdateResourceInstruction(instruction1, "Bots", "54367256|Brian Molko");
+
+        Element instruction2 = (Element) instructions.item(1);
+        assertUpdateResourceInstruction(instruction2, "Bots", "33333333|Cindy");
+    }
+
+    private static NodeList assertStructureAndReturnInstructions(Document actualDocument, int instructionsCount) {
         NodeList instructionsNodes = actualDocument.getDocumentElement().getElementsByTagName("instructions");
         assertThat(instructionsNodes.getLength()).isEqualTo(1);
 
         Element instructionsNode = (Element) instructionsNodes.item(0);
-        return instructionsNode.getElementsByTagName("instruction");
+        NodeList instructions = instructionsNode.getElementsByTagName("instruction");
+
+        assertThat(instructions.getLength()).isEqualTo(instructionsCount);
+
+        return instructions;
     }
 
     private static void assertUpdateDatabaseInstruction(Element instruction, String resourceName, String beginningOfResourceValue) {
+        assertThat(instruction.getAttribute("type")).isEqualTo("updateDatabase");
+
         Element resourceNameParameter = (Element) instruction.getElementsByTagName("parameter").item(0);
         Element resourceValueParameter = (Element) instruction.getElementsByTagName("parameter").item(1);
 
         assertThat(resourceNameParameter.getAttribute("value")).isEqualTo(resourceName);
         assertThat(resourceValueParameter.getAttribute("value")).startsWith(beginningOfResourceValue);
+    }
+
+    private static void assertUpdateResourceInstruction(Element instruction, String resourceName, String resourceValue) {
+        assertThat(instruction.getAttribute("type")).isEqualTo("updateResource");
+
+        Element resourceNameParameter = (Element) instruction.getElementsByTagName("parameter").item(0);
+        Element resourceValueParameter = (Element) instruction.getElementsByTagName("parameter").item(1);
+
+        assertThat(resourceNameParameter.getAttribute("value")).isEqualTo(resourceName);
+        assertThat(resourceValueParameter.getAttribute("value")).isEqualTo(resourceValue);
     }
 }
