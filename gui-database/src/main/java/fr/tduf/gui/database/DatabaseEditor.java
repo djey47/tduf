1684d9d2d34b4ba34132031dfbe510ca838f672e
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-05-26 13:02:53 +0200

    refactor(gui/database) : rename Main class.

--- gui-database/src/main/java/fr/tduf/gui/database/DatabaseEditor.java
+++ gui-database/src/main/java/fr/tduf/gui/database/DatabaseEditor.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
 /**
  * Application entry point.
  */
-public class Main extends Application {
+public class DatabaseEditor extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
