ae36b6e004dd9e29aeba45b5e1d31b061958762e
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-05-27 09:41:23 +0200

    clean(gui/database) : remove unused imports.

--- gui-database/src/main/java/fr/tduf/gui/database/DatabaseEditor.java
+++ gui-database/src/main/java/fr/tduf/gui/database/DatabaseEditor.java
@@ -2,14 +2,6 @@ package fr.tduf.gui.database;
 
 import fr.tduf.gui.database.stages.MainStageDesigner;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
-import javafx.scene.control.ToolBar;
-import javafx.scene.control.ToolBarBuilder;
-import javafx.scene.layout.VBoxBuilder;
 import javafx.stage.Stage;
 
 /**
