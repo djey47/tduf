d8b38996d9202e727ec877f9e9c51010eefc308a
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-06-02 17:56:38 +0200

    refactor(lib/database) : add generation methods to common db helper. (cherry picked from commit fe837e8)

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/common/helper/DatabaseHelper.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/common/helper/DatabaseHelper.java
@@ -1,21 +1,153 @@
 package fr.tduf.libunlimited.high.files.db.common.helper;
 
+import fr.tduf.libunlimited.high.files.db.miner.BulkDatabaseMiner;
+import fr.tduf.libunlimited.low.files.db.dto.DbDataDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbStructureDto;
 import fr.tduf.libunlimited.low.files.db.rw.helper.DatabaseStructureQueryHelper;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.toList;
 import static java.util.stream.Collectors.toSet;
 
 /**
- * Class providing methods to manipulate databases.
+ * Class providing methods to generate database contents and resources.
  */
+// TODO rename to DatabaseGenHelper
+// TODO unit tests
+// TODO see to use this class from DatabaseIntegrityFixer
 public class DatabaseHelper {
 
     public static final String RESOURCE_VALUE_DEFAULT = "??";
+
+    private static final String BITFIELD_VALUE_DEFAULT = "00000000";
+
+    private final BulkDatabaseMiner databaseMiner;
+
+    public DatabaseHelper(BulkDatabaseMiner databaseMiner) {
+        this.databaseMiner = databaseMiner;
+    }
+
+    /**
+     *
+     * @param reference
+     * @param topicObject
+     * @return
+     */
+    public List<DbDataDto.Item> buildDefaultContentItems(Optional<String> reference, DbDto topicObject) {
+
+        return topicObject.getStructure().getFields().stream()
+
+                .map((structureField) -> buildDefaultContentItem(reference, structureField, topicObject))
+
+                .collect(toList());
+    }
+
+    /**
+     *
+     * @param entryReference
+     * @param field
+     * @param topicObject
+     * @return
+     */
+    public DbDataDto.Item buildDefaultContentItem(Optional<String> entryReference, DbStructureDto.Field field, DbDto topicObject) {
+        String rawValue;
+        DbDto remoteTopicObject = databaseMiner.getDatabaseTopicFromReference(field.getTargetRef());
+
+        DbStructureDto.FieldType fieldType = field.getFieldType();
+        switch (fieldType) {
+            case UID:
+                if (entryReference.isPresent()) {
+                    rawValue = entryReference.get();
+                } else {
+                    rawValue = DatabaseHelper.generateUniqueContentsEntryIdentifier(topicObject);
+                }
+                break;
+            case BITFIELD:
+                rawValue = BITFIELD_VALUE_DEFAULT;
+                break;
+            case FLOAT:
+                rawValue = "0.0";
+                break;
+            case INTEGER:
+                rawValue = "0";
+                break;
+            case PERCENT:
+                rawValue = "1";
+                break;
+            case REFERENCE:
+                rawValue = DatabaseHelper.generateUniqueContentsEntryIdentifier(remoteTopicObject);
+                addContentsEntryWithDefaultItems(Optional.of(rawValue), remoteTopicObject.getTopic());
+                break;
+            case RESOURCE_CURRENT_GLOBALIZED:
+            case RESOURCE_CURRENT_LOCALIZED:
+                rawValue = DatabaseHelper.generateUniqueResourceEntryIdentifier(topicObject);
+                addDefaultResourceReferenceForAllLocales(rawValue, topicObject);
+                break;
+            case RESOURCE_REMOTE:
+                rawValue = DatabaseHelper.generateUniqueResourceEntryIdentifier(remoteTopicObject);
+                addDefaultResourceReferenceForAllLocales(rawValue, remoteTopicObject);
+                break;
+            default:
+                throw new IllegalArgumentException("Unhandled field type: " + fieldType);
+        }
+
+        return DbDataDto.Item.builder()
+                .fromStructureField(field)
+                .withRawValue(rawValue)
+                .build();
+    }
+
+    /**
+     *
+     * @param reference
+     * @param topic
+     */
+    public void addContentsEntryWithDefaultItems(Optional<String> reference, DbDto.Topic topic) {
+
+        DbDto topicObject = databaseMiner.getDatabaseTopic(topic).get();
+
+        DbDataDto dataDto = topicObject.getData();
+
+        DbDataDto.Entry newEntry = DbDataDto.Entry.builder()
+                .forId(dataDto.getEntries().size())
+                .addItems(buildDefaultContentItems(reference, topicObject))
+                .build();
+
+        dataDto.getEntries().add(newEntry);
+    }
+
+    /**
+     *
+     * @param resourceReference
+     * @param topicObject
+     */
+    public void addDefaultResourceReferenceForAllLocales(String resourceReference, DbDto topicObject) {
+        Stream.of(DbResourceDto.Locale.values())
+
+                .forEach((locale) -> addDefaultResourceEntry(resourceReference, topicObject.getTopic(), locale));
+    }
+
+    /**
+     *
+     * @param reference
+     * @param topic
+     * @param locale
+     */
+    public void addDefaultResourceEntry(String reference, DbDto.Topic topic, DbResourceDto.Locale locale) {
+        DbResourceDto.Entry newEntry = DbResourceDto.Entry.builder()
+                .forReference(reference)
+                .withValue(RESOURCE_VALUE_DEFAULT)
+                .build();
+
+        DbResourceDto resourceDto = databaseMiner.getResourceFromTopicAndLocale(topic, locale).get();
+        resourceDto.getEntries().add(newEntry);
+    }
 
     /**
      * Produces a random, unique identifier of content entry.
