1a801edac5af1c83a92422e30467e53b0bdf0d9f
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-06-05 13:35:16 +0200

    feature(gui/database) : add factory for entry cell.

---
+++ gui-database/src/main/java/fr/tduf/gui/database/factory/EntryCellFactory.java
@@ -0,0 +1,25 @@
+package fr.tduf.gui.database.factory;
+
+import fr.tduf.gui.database.domain.RemoteResource;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.util.Callback;
+
+public class EntryCellFactory implements Callback<ListView<RemoteResource>, ListCell<RemoteResource>> {
+    @Override
+    public ListCell<RemoteResource> call(ListView<RemoteResource> param) {
+        return new ListCell<RemoteResource>() {
+            @Override
+            protected void updateItem(RemoteResource item, boolean empty) {
+
+                if (item == null) {
+                    setText(null);
+                } else {
+                    setText(item.valueProperty().get());
+                }
+
+                super.updateItem(item, empty);
+            }
+        };
+    }
+}
