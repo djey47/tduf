87429cb6b2756b32755d4904b144c4e564c95f47
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-06-17 15:54:19 +0200

    feature(gui/database) : add structure to transmit localized resource.

---
+++ gui-database/src/main/java/fr/tduf/gui/database/domain/LocalizedResource.java
@@ -0,0 +1,29 @@
+package fr.tduf.gui.database.domain;
+
+import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
+import javafx.util.Pair;
+
+import java.util.Optional;
+
+/**
+ * Represents a selected resource with optional localization information.
+ */
+public class LocalizedResource {
+
+    private Pair<String, String> referenceValuePair;
+
+    private Optional<DbResourceDto.Locale> locale;
+
+    public LocalizedResource(Pair<String, String> referenceValuePair, Optional<DbResourceDto.Locale> locale) {
+        this.referenceValuePair = referenceValuePair;
+        this.locale = locale;
+    }
+
+    public Pair<String, String> getReferenceValuePair() {
+        return referenceValuePair;
+    }
+
+    public Optional<DbResourceDto.Locale> getLocale() {
+        return locale;
+    }
+}
