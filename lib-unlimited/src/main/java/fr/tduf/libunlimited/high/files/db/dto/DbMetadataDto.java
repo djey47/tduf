dffa941e49a2415e17a787b7347d88f6484118c0
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-07-09 14:04:44 +0200

    feature (lib/database) : init database metadata.

---
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/dto/DbMetadataDto.java
@@ -0,0 +1,70 @@
+package fr.tduf.libunlimited.high.files.db.dto;
+
+import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonTypeName;
+
+import java.util.List;
+
+/**
+ * Contains all metadata written in databaseMetadata.json file.
+ */
+@JsonTypeName("databaseMetadata")
+public class DbMetadataDto {
+
+    @JsonProperty("topics")
+    private List<TopicMetadataDto> topics;
+
+    public DbMetadataDto() {}
+
+    public List<TopicMetadataDto> getTopics() {
+        return topics;
+    }
+
+    @JsonTypeName("topicMetadata")
+    public static class TopicMetadataDto {
+
+        @JsonProperty("topicName")
+        private DbDto.Topic topic;
+
+        @JsonProperty("bitfieldReference")
+        private List<BitfieldMetadataDto> bitfields;
+
+        public TopicMetadataDto() {}
+
+        public DbDto.Topic getTopic() {
+            return topic;
+        }
+
+        public List<BitfieldMetadataDto> getBitfields() {
+            return bitfields;
+        }
+
+        @JsonTypeName("bitfieldMetadata")
+        public static class BitfieldMetadataDto {
+
+            @JsonProperty("index")
+            private int index;
+
+            @JsonProperty("label")
+            private String label;
+
+            @JsonProperty("comment")
+            private String comment;
+
+            public BitfieldMetadataDto() {}
+
+            public int getIndex() {
+                return index;
+            }
+
+            public String getLabel() {
+                return label;
+            }
+
+            public String getComment() {
+                return comment;
+            }
+        }
+    }
+}
