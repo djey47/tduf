55d48d5c653102baa59c1fbd7c7e57c0b554b3c7
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-07-15 10:49:11 +0200

    feature(gui/database) : redirect all output to a .log file.

--- cli/src/main/shell/Alpha-DatabaseEditor.cmd
+++ cli/src/main/shell/Alpha-DatabaseEditor.cmd
@@ -2,7 +2,7 @@
 IF [%1]==[] (
 	ECHO Missing parameter: database directory.
 ) ELSE (
-	CALL DatabaseTool unpack-all -d %1 -j %1\tduf-json
-	CALL DatabaseEditor %1\tduf-json
-	CALL DatabaseTool repack-all -o %1 -j %1\tduf-json
+	CALL DatabaseTool unpack-all -d %1 -j %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
+	CALL DatabaseEditor %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
+	CALL DatabaseTool repack-all -o %1 -j %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
 )
