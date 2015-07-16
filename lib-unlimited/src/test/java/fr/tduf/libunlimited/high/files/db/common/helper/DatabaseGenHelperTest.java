8caed062a4303157a1d0dcec7ce77341b4caaa1a
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-07-16 11:04:11 +0200

    test(lib/database) : add unit tests and javadoc. (cherry picked from commit bed4981)

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/common/helper/DatabaseGenHelperTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/common/helper/DatabaseGenHelperTest.java
@@ -1,15 +1,35 @@
 package fr.tduf.libunlimited.high.files.db.common.helper;
 
+import fr.tduf.libunlimited.high.files.db.miner.BulkDatabaseMiner;
 import fr.tduf.libunlimited.low.files.db.dto.DbDataDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbStructureDto;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@RunWith(MockitoJUnitRunner.class)
 public class DatabaseGenHelperTest {
+
+    private static final String ENTRY_REFERENCE = "11111111";
+    private static final String RESOURCE_REFERENCE = "11111111";
+
+    @Mock
+    BulkDatabaseMiner minerMock;
+
+    @Mock
+    DatabaseChangeHelper changeHelperMock;
+
+    @InjectMocks
+    DatabaseGenHelper genHelper;
 
     @Test
     public void generateUniqueResourceEntryIdentifier_whenNullTopicObject_shouldReturnNull() throws Exception {
@@ -24,13 +44,13 @@ public class DatabaseGenHelperTest {
                 .addResource(DbResourceDto.builder()
                         .withLocale(DbResourceDto.Locale.FRANCE)
                         .addEntry(DbResourceDto.Entry.builder()
-                            .forReference("11111111")
+                            .forReference(RESOURCE_REFERENCE)
                             .build())
                         .build())
                 .addResource(DbResourceDto.builder()
                         .withLocale(DbResourceDto.Locale.ITALY)
                         .addEntry(DbResourceDto.Entry.builder()
-                            .forReference("11111111")
+                            .forReference(RESOURCE_REFERENCE)
                             .build())
                         .build())
                 .build();
@@ -54,14 +74,7 @@ public class DatabaseGenHelperTest {
     @Test(expected = IllegalArgumentException.class)
     public void generateUniqueContentsEntryIdentifier_whenTopicObjectWithoutIdentifierField_shouldThrowIllegalArgumentException() throws Exception {
         // GIVEN-
-        DbDto topicObject = DbDto.builder()
-                .withStructure(DbStructureDto.builder()
-                        .addItem(DbStructureDto.Field.builder()
-                                .fromType(DbStructureDto.FieldType.INTEGER)
-                                .ofRank(1)
-                                .build())
-                        .build())
-                .build();
+        DbDto topicObject = createTopicObjectOneIntegerField();
 
         // WHEN
         DatabaseGenHelper.generateUniqueContentsEntryIdentifier(topicObject);
@@ -71,23 +84,8 @@ public class DatabaseGenHelperTest {
 
     @Test
     public void generateUniqueContentsEntryIdentifier_whenTopicObjectWithIdentifierField_shouldReturnCorrectIdentifier() throws Exception {
-        // GIVEN-
-        DbDto topicObject = DbDto.builder()
-                .withStructure(DbStructureDto.builder()
-                        .addItem(DbStructureDto.Field.builder()
-                                .fromType(DbStructureDto.FieldType.UID)
-                                .ofRank(1)
-                                .build())
-                        .build())
-                .withData(DbDataDto.builder()
-                        .addEntry(DbDataDto.Entry.builder()
-                                .addItem(DbDataDto.Item.builder()
-                                        .ofFieldRank(1)
-                                        .withRawValue("11111111")
-                                        .build())
-                                .build())
-                        .build())
-                .build();
+        // GIVEN
+        DbDto topicObject = createTopicObjectOneUIDField();
 
         // WHEN
         String actualEntryIdentifier = DatabaseGenHelper.generateUniqueContentsEntryIdentifier(topicObject);
@@ -100,7 +98,58 @@ public class DatabaseGenHelperTest {
     }
 
     @Test
-    public void generateUniqueResourceEntryIdentifier() throws Exception {
+    public void buildDefaultContentItems_whenOneField_andReference_shouldCreateOneItem() {
+        // GIVEN
+        DbDto topicObject = createTopicObjectOneUIDField();
 
+        // WHEN
+        List<DbDataDto.Item> actualItems = genHelper.buildDefaultContentItems(Optional.of(ENTRY_REFERENCE), topicObject);
+
+        // THEN
+        assertThat(actualItems).hasSize(1);
+        assertThat(actualItems).extracting("rawValue").containsExactly(ENTRY_REFERENCE);
+    }
+
+    @Test
+    public void buildDefaultContentItems_whenOneField_andNoReference_shouldCreateOneItemWithDefaultRef() {
+        // GIVEN
+        DbDto topicObject = createTopicObjectOneUIDField();
+
+        // WHEN
+        List<DbDataDto.Item> actualItems = genHelper.buildDefaultContentItems(Optional.empty(), topicObject);
+
+        // THEN
+        assertThat(actualItems).hasSize(1);
+        assertThat(actualItems.get(0).getRawValue()).isNotEmpty();
+    }
+
+    private DbDto createTopicObjectOneUIDField() {
+        return DbDto.builder()
+                .withStructure(DbStructureDto.builder()
+                        .addItem(DbStructureDto.Field.builder()
+                                .fromType(DbStructureDto.FieldType.UID)
+                                .ofRank(1)
+                                .build())
+                        .build())
+                .withData(DbDataDto.builder()
+                        .addEntry(DbDataDto.Entry.builder()
+                                .addItem(DbDataDto.Item.builder()
+                                        .ofFieldRank(1)
+                                        .withRawValue(ENTRY_REFERENCE)
+                                        .build())
+                                .build())
+                        .build())
+                .build();
+    }
+
+    private DbDto createTopicObjectOneIntegerField() {
+        return DbDto.builder()
+                .withStructure(DbStructureDto.builder()
+                        .addItem(DbStructureDto.Field.builder()
+                                .fromType(DbStructureDto.FieldType.INTEGER)
+                                .ofRank(1)
+                                .build())
+                        .build())
+                .build();
     }
 }
