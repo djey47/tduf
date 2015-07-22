0cc041c42fa0495b209cced4b5770b720ac05de7
djey <j.canler@gmail.com>
2015-07-22 19:32:00 +0200

    feature(shell) : add more messages to tell what's going on.

--- cli/src/main/shell/Alpha-DatabaseEditor.cmd
+++ cli/src/main/shell/Alpha-DatabaseEditor.cmd
@@ -2,7 +2,11 @@
 IF [%1]==[] (
 	ECHO Missing parameter: database directory.
 ) ELSE (
+	ECHO Unpacking TDU database from %1, please wait...
 	CALL DatabaseTool unpack-all -d %1 -j %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
+	ECHO Starting Database Editor...
 	CALL DatabaseEditor %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
+	ECHO Database Editor ended, now repacking database to %1, please wait...
 	CALL DatabaseTool repack-all -o %1 -j %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
+	ECHO All done!
 )
