53e838a1dd0585193af9272bafc717d22c6d495a
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-08-06 13:12:04 +0200

    feature(gui/installer) : introduce new module.

---
+++ gui-installer/src/main/java/fr/tduf/gui/installer/Installer.java
@@ -0,0 +1,23 @@
+package fr.tduf.gui.installer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Installer Java FX Application.
+ */
+public class Installer extends Application {
+
+    /**
+     * Single application entry point
+     * @param args  : arguments passed with command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
