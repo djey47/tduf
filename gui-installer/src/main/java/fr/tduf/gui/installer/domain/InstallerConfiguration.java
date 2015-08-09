3cd0d2f11378f941e837de5fcfe22f119ab08942
djey <j.canler@gmail.com>
2015-08-09 15:26:29 +0200

    feature(gui/installer) : add assets directory to configuration.

--- gui-installer/src/main/java/fr/tduf/gui/installer/domain/InstallerConfiguration.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/domain/InstallerConfiguration.java
@@ -9,16 +9,24 @@ public class InstallerConfiguration {
 
     private String testDriveUnlimitedDirectory;
 
+    private String assetsDirectory;
+
     private InstallerConfiguration() {}
 
     public String getTestDriveUnlimitedDirectory() {
         return testDriveUnlimitedDirectory;
     }
 
+    public String getAssetsDirectory() {
+        return assetsDirectory;
+    }
+
     public static InstallerConfigurationBuilder builder() {
         return new InstallerConfigurationBuilder() {
 
             private String testDriveUnlimitedDirectory;
+
+            private String assetsDirectory = ".";
 
             @Override
             public InstallerConfigurationBuilder withTestDriveUnlimitedDirectory(String testDriveUnlimitedDirectory) {
@@ -27,20 +35,29 @@ public class InstallerConfiguration {
             }
 
             @Override
+            public InstallerConfigurationBuilder withAssetsDirectory(String assetsDirectory) {
+                this.assetsDirectory = assetsDirectory;
+                return this;
+            }
+
+            @Override
             public InstallerConfiguration build() {
                 requireNonNull(testDriveUnlimitedDirectory, "TDU directory is required.");
+                requireNonNull(assetsDirectory, "Assets directory is required.");
 
                 InstallerConfiguration installerConfiguration = new InstallerConfiguration();
                 installerConfiguration.testDriveUnlimitedDirectory = testDriveUnlimitedDirectory;
+                installerConfiguration.assetsDirectory = assetsDirectory;
 
                 return installerConfiguration;
-
             }
         };
     }
 
     public interface InstallerConfigurationBuilder {
         InstallerConfigurationBuilder withTestDriveUnlimitedDirectory(String testDriveUnlimitedDirectory);
+
+        InstallerConfigurationBuilder withAssetsDirectory(String testDriveUnlimitedDirectory);
 
         InstallerConfiguration build();
     }
