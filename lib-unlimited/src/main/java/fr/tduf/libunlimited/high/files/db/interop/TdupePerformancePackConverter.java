cb98da69e235e858844de8ba101a7123ba5d11df
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-08-13 18:03:33 +0200

    feature(lib/database) : introduce new TDUPE interoperability component.

---
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverter.java
@@ -0,0 +1,7 @@
+package fr.tduf.libunlimited.high.files.db.interop;
+
+/**
+ * Utility class allowing to convert performance packs between TDUPE and TDUF systems.
+ */
+public class TdupePerformancePackConverter {
+}
---
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverterTest.java
@@ -0,0 +1,7 @@
+package fr.tduf.libunlimited.high.files.db.interop;
+
+import static org.junit.Assert.*;
+
+public class TdupePerformancePackConverterTest {
+
+}
