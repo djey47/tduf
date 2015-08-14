352edb457d033c68c2da1f97b089020edbeb91c6
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-08-14 18:01:31 +0200

    feature(lib/database) : implement tdupk>tduf conversion - WIP.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverter.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverter.java
@@ -5,6 +5,8 @@ import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 
 import java.util.Optional;
 
+import static java.util.Objects.requireNonNull;
+
 /**
  * Utility class allowing to convert performance packs between TDUPE and TDUF systems.
  */
@@ -18,6 +20,9 @@ public class TdupePerformancePackConverter {
      * @return
      */
     public static DbPatchDto tdupkToJson(String carPhysicsDataLine, Optional<String> carPhysicsRef, DbDto carPhysicsTopicObject) {
+        requireNonNull(carPhysicsDataLine, "Line from Performance Pack is required.");
+        requireNonNull(carPhysicsTopicObject, "CarPhysicsData topic object is required.");
+
         return null;
     }
 }
--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverterTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverterTest.java
@@ -1,7 +1,65 @@
 package fr.tduf.libunlimited.high.files.db.interop;
 
-import static org.junit.Assert.*;
+import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Optional;
 
 public class TdupePerformancePackConverterTest {
 
+    private static Class<TdupePerformancePackConverterTest> thisClass = TdupePerformancePackConverterTest.class;
+
+    @Test(expected = NullPointerException.class)
+    public void tdupkToJson_whenNullLine_shouldThrowException() {
+        // GIVEN-WHEN
+        TdupePerformancePackConverter.tdupkToJson(null, Optional.<String>empty(), DbDto.builder().build());
+
+        // THEN: NPE
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void tdupkToJson_whenNullTopicObject_shouldThrowException() {
+        // GIVEN-WHEN
+        TdupePerformancePackConverter.tdupkToJson("", Optional.<String>empty(), null);
+
+        // THEN: NPE
+    }
+
+    @Test
+    public void tdupkToJson_withoutReference_shouldMakePatchObject() throws IOException, URISyntaxException {
+        // GIVEN
+        String carPhysicsDataLine = readLineFromPack();
+        DbDto carPhysicsTopicObject = loadCarPhysicsTopicFromResources();
+
+        // WHEN
+        DbPatchDto actualPatchObject = TdupePerformancePackConverter.tdupkToJson(carPhysicsDataLine, Optional.<String>empty(), carPhysicsTopicObject);
+
+        // THEN
+//        assertThat(actualPatchObject).isNotNull();
+    }
+
+    @Test
+    public void tdupkToJson_withReference_shouldMakePatchObject() {
+        // TODO
+    }
+
+    private static String readLineFromPack() throws IOException, URISyntaxException {
+        URI packFileURI = thisClass.getResource("/db/patch/tdupe/F150.tdupk").toURI();
+        List<String> lines = Files.readAllLines(Paths.get(packFileURI));
+        return lines.get(0);
+    }
+
+    private static DbDto loadCarPhysicsTopicFromResources() throws URISyntaxException, IOException {
+        URI topicFileURI = thisClass.getResource("/db/json/TDU_CarPhysicsData.json").toURI();
+        return new ObjectMapper().readValue(new File(topicFileURI), DbDto.class);
+    }
 }
