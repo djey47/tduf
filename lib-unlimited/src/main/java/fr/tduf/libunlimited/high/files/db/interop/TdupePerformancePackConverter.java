4005ba94f5264e84d28f4a22d735a07bf097200f
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-08-14 16:44:01 +0200

    feature (cli/database) : implement apply-tdupk op - WIP.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverter.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverter.java
@@ -1,7 +1,23 @@
 package fr.tduf.libunlimited.high.files.db.interop;
 
+import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+
+import java.util.Optional;
+
 /**
  * Utility class allowing to convert performance packs between TDUPE and TDUF systems.
  */
 public class TdupePerformancePackConverter {
+
+    /**
+     *
+     * @param carPhysicsDataLine
+     * @param carPhysicsRef
+     * @param carPhysicsTopicObject
+     * @return
+     */
+    public static DbPatchDto tdupkToJson(String carPhysicsDataLine, Optional<String> carPhysicsRef, DbDto carPhysicsTopicObject) {
+        return null;
+    }
 }
