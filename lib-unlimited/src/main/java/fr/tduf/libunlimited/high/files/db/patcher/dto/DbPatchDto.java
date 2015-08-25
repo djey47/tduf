66c69b88a1110106d8dc3615827adcba380ef841
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-08-25 14:24:36 +0200

    feature(lib/database) : implement toString() method on change object.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/patcher/dto/DbPatchDto.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/patcher/dto/DbPatchDto.java
@@ -2,8 +2,6 @@ package fr.tduf.libunlimited.high.files.db.patcher.dto;
 
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.annotate.JsonTypeName;
@@ -14,6 +12,7 @@ import java.util.List;
 
 import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
 import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
 
 /**
  * Represents a patch to be applied to TDU database (TDUF format)
@@ -95,6 +94,11 @@ public class DbPatchDto {
         @Override
         public int hashCode() {
             return reflectionHashCode(this);
+        }
+
+        @Override
+        public String toString() {
+            return reflectionToString(this);
         }
 
         public DbResourceDto.Locale getLocale() {
