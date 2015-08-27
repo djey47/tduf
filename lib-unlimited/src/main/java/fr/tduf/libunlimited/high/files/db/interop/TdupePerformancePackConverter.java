803b825162df266b2a9e3f5981ae8081ee82360a
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-08-27 17:14:30 +0200

    feature(lib/database) : implement tdupk>tduf conversion when non provided slot reference.

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverter.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/interop/TdupePerformancePackConverter.java
@@ -1,16 +1,21 @@
 package fr.tduf.libunlimited.high.files.db.interop;
 
+import fr.tduf.libunlimited.high.files.db.miner.BulkDatabaseMiner;
 import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbDataDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import static fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto.DbChangeDto.ChangeTypeEnum.UPDATE;
 import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.CAR_PHYSICS_DATA;
 import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
 
 /**
  * Utility class allowing to convert performance packs between TDUPE and TDUF systems.
@@ -19,19 +24,20 @@ public class TdupePerformancePackConverter {
 
     private static final String REGEX_SEPARATOR_ITEMS = ";";
 
+    private static final Set<Integer> FIELD_RANKS_NON_PHYSICAL = new HashSet<>(asList(1, 3, 4, 9, 10, 12, 100, 101));
+
     /**
      * Converts a performance pack line (aka. CarPhysics entry)
-     * @param carPhysicsDataLine
-     * @param carPhysicsRef
-     * @param carPhysicsTopicObject
-     * @return
+     * @param carPhysicsDataLine    : performance pack value
+     * @param carPhysicsRef         : vehicle slot reference, not mandatory
+     * @param carPhysicsTopicObject : actual car physics topic in database
+     * @return a TDUF mini patch object.
      */
     public static DbPatchDto tdupkToJson(String carPhysicsDataLine, Optional<String> carPhysicsRef, DbDto carPhysicsTopicObject) {
         requireNonNull(carPhysicsDataLine, "Line from Performance Pack is required.");
         requireNonNull(carPhysicsTopicObject, "CarPhysicsData topic object is required.");
 
-        // TODO keep actual contents into account if available
-        DbPatchDto.DbChangeDto changeObject = getChangeObjectForContentsUpdate(carPhysicsDataLine);
+        DbPatchDto.DbChangeDto changeObject = getChangeObjectForContentsUpdate(carPhysicsDataLine, carPhysicsTopicObject);
 
         return DbPatchDto.builder()
                 .addChanges(singletonList(changeObject))
@@ -39,14 +45,44 @@ public class TdupePerformancePackConverter {
     }
 
     // TODO move to Helper
-    private static DbPatchDto.DbChangeDto getChangeObjectForContentsUpdate(String contentsEntry) {
-        List<String> values = asList(contentsEntry.split(REGEX_SEPARATOR_ITEMS));
+    private static DbPatchDto.DbChangeDto getChangeObjectForContentsUpdate(String contentsEntry, DbDto carPhysicsTopicObject) {
 
+        List<String> packValues = asList(contentsEntry.split(REGEX_SEPARATOR_ITEMS));
+
+        BulkDatabaseMiner databaseMiner = BulkDatabaseMiner.load(singletonList(carPhysicsTopicObject));
+        String slotReference = packValues.get(0);
+        Optional<DbDataDto.Entry> carPhysicsEntry = databaseMiner.getContentEntryFromTopicWithReference(slotReference, CAR_PHYSICS_DATA);
+
+        List<String> itemValues = applyPhysicalChangesToPotentialEntry(packValues, carPhysicsEntry);
         return DbPatchDto.DbChangeDto.builder()
                 .withType(UPDATE)
                 .forTopic(CAR_PHYSICS_DATA)
-                .asReference(values.get(0))
-                .withEntryValues(values)
+                .asReference(slotReference)
+                .withEntryValues(itemValues)
                 .build();
+    }
+
+    private static List<String> applyPhysicalChangesToPotentialEntry(List<String> packValues, Optional<DbDataDto.Entry> carPhysicsEntry) {
+        if (carPhysicsEntry.isPresent()) {
+            return carPhysicsEntry.get().getItems().stream()
+
+                    .map((item) -> getProperRawValue(packValues, item))
+
+                    .collect(toList());
+        }
+
+        return packValues;
+    }
+
+    private static String getProperRawValue(List<String> packValues, DbDataDto.Item item) {
+
+        int fieldRank = item.getFieldRank();
+        String rawValue = packValues.get(fieldRank - 1);
+
+        if (FIELD_RANKS_NON_PHYSICAL.contains(fieldRank)) {
+            rawValue = item.getRawValue();
+        }
+
+        return rawValue;
     }
 }
