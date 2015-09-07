57e427a0e1281fc736f7411e7bb0cc9eae7e931f
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-09-07 13:40:46 +0200

    feature(shell) : test each step result and abort if necessary + change output format.

--- cli/src/main/shell/Alpha-DatabaseEditor.cmd
+++ cli/src/main/shell/Alpha-DatabaseEditor.cmd
@@ -2,11 +2,36 @@
 IF [%1]==[] (
 	ECHO Missing parameter: database directory.
 ) ELSE (
-	ECHO Unpacking TDU database from %1, please wait...
+	ECHO.
+	ECHO ...Unpacking TDU database from %1, please wait...
 	CALL DatabaseTool unpack-all -d %1 -j %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
-	ECHO Starting Database Editor...
+	if ERRORLEVEL 1 GOTO handleUnpackError
+	
+	ECHO ...Starting Database Editor...
 	CALL DatabaseEditor %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
-	ECHO Database Editor ended, now repacking database to %1, please wait...
+	if ERRORLEVEL 1 GOTO handleEditorError
+
+	ECHO ...Database Editor ended, now repacking database to %1, please wait...
 	CALL DatabaseTool repack-all -o %1 -j %1\tduf-json >> Alpha-DatabaseEditor.log 2>>&1
-	ECHO All done!
+	if ERRORLEVEL 1 GOTO handleRepackError
+
+	ECHO.
+	ECHO All done!	
 )
+EXIT /B 0
+
+:handleUnpackError
+ECHO Unpacking failed, can't continue.
+GOTO exitFailure
+
+:handleEditorError
+ECHO Editor failed, can't continue. Changes won't be applied.
+GOTO exitFailure
+
+:handleRepackError
+ECHO Repacking failed. Changes won't be applied.
+GOTO exitFailure
+
+:exitFailure
+ECHO Please check Alpha-DatabaseEditor.log for details.
+EXIT /B 1
