62850480920d83665184a92faa501ea71b6fec51
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-10-14 15:37:06 +0200

    technical(lib) : introduce cache manager component.

---
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/common/cache/CacheManager.java
@@ -0,0 +1,72 @@
+package fr.tduf.libunlimited.common.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+/**
+ * Generic manager to simplify caching of items
+ */
+public enum CacheManager {
+
+    /**
+     * Describes unique instance
+     */
+    it(new CacheManagerInstance(true));
+
+    private final CacheManagerInstance cacheManagerInstance;
+
+    CacheManager(CacheManagerInstance cacheManagerInstance) {
+        this.cacheManagerInstance = cacheManagerInstance;
+    }
+
+    /**
+     * @return unique instance of manager
+     */
+    public CacheManagerInstance self() {
+        return cacheManagerInstance;
+    }
+
+    public static class CacheManagerInstance {
+
+        private final boolean enabled;
+        private final Map<String, Map<String, ?>> stores;
+
+        private CacheManagerInstance(boolean enabled) {
+            this.enabled = enabled;
+            this.stores = new HashMap<>(16);
+        }
+
+        /**
+         * Updates store with the result.
+         * @param storeName : store in which key should be located
+         * @param key       : key to the value to be retrieved
+         * @param supplier  : describes processing to get a brand new value
+         * @param <R>       : type of value to be Teturned
+         * @return value from cache in store if existing, otherwise value returned by call to supplier.
+         */
+        public <R> R getValueFromKey(String storeName, String key, Supplier<R> supplier) {
+            if (enabled) {
+
+                stores.putIfAbsent(storeName, new HashMap<>(16));
+                Map<String, R> storeMap = (Map<String, R>) stores.get(storeName);
+
+                storeMap.putIfAbsent(key, supplier.get());
+                return storeMap.get(key);
+            }
+
+            return supplier.get();
+        }
+
+        public void clearAllStores() {
+            stores.clear();
+        }
+
+        /**
+         * @param storeName : name of store in which all keys will be removed.
+         */
+        public void clearStoreByName(String storeName) {
+            stores.getOrDefault(storeName, new HashMap<>()).clear();
+        }
+    }
+}
