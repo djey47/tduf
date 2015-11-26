5e940fd4941a0134a64aa1f8ae77144442688ed4
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-11-26 14:37:15 +0100

    feature(lib/mapping) : add operation to return magic state on entry

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/banks/mapping/domain/BankMapEntryTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/banks/mapping/domain/BankMapEntryTest.java
@@ -21,4 +21,37 @@ public class BankMapEntryTest {
         assertThat(entry.getSize1()).isEqualTo(0);
         assertThat(entry.getSize2()).isEqualTo(0);
     }
+
+    @Test
+    public void isMagic_whenAllSizes0_shouldReturnTrue() {
+        // GIVEN
+        BankMap bankMap = new BankMap();
+        bankMap.addEntry(1589L, 0L, 0L);
+        BankMap.Entry entry = bankMap.getEntries().stream().findAny().get();
+
+        // WHEN-THEN
+        assertThat(entry.isMagic()).isTrue();
+    }
+
+    @Test
+    public void isMagic_whenAllSizesNot0_shouldReturnFalse() {
+        // GIVEN
+        BankMap bankMap = new BankMap();
+        bankMap.addEntry(1589L, 150L, 150L);
+        BankMap.Entry entry = bankMap.getEntries().stream().findAny().get();
+
+        // WHEN-THEN
+        assertThat(entry.isMagic()).isFalse();
+    }
+
+    @Test
+    public void isMagic_whenOneSizeNot0_shouldReturnFalse() {
+        // GIVEN
+        BankMap bankMap = new BankMap();
+        bankMap.addEntry(1589L, 0L, 150L);
+        BankMap.Entry entry = bankMap.getEntries().stream().findAny().get();
+
+        // WHEN-THEN
+        assertThat(entry.isMagic()).isFalse();
+    }
 }
