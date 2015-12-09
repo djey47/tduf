4b3623eae5bb5a49cf32fb53523101090d3e1408
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-12-09 11:05:23 +0100

    refactor(lib/database/patcher) : extract common test

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/DatabasePatcher_focusOnResourcesTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/DatabasePatcher_focusOnResourcesTest.java
@@ -1,30 +1,24 @@
 package fr.tduf.libunlimited.high.files.db.patcher;
 
-import fr.tduf.libunlimited.high.files.db.common.AbstractDatabaseHolder;
 import fr.tduf.libunlimited.high.files.db.miner.BulkDatabaseMiner;
 import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
-import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
 import java.util.Optional;
 
 import static java.util.Collections.singletonList;
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
-public class DatabasePatcher_focusOnResourcesTest {
-    private static final Class<DatabasePatcher_focusOnResourcesTest> thisClass = DatabasePatcher_focusOnResourcesTest.class;
+public class DatabasePatcher_focusOnResourcesTest extends DatabasePatcher_commonTest {
 
     @Before
     public void setUp() {
-        BulkDatabaseMiner.clearAllCaches();
+        super.setUp();
     }
 
     @Test
@@ -105,15 +99,6 @@ public class DatabasePatcher_focusOnResourcesTest {
         assertResourceEntryMissing(databaseMiner, DbDto.Topic.BOTS, DbResourceDto.Locale.FRANCE, "33333333");
         assertResourceEntryMissing(databaseMiner, DbDto.Topic.BOTS, DbResourceDto.Locale.ITALY, "60367256");
         assertResourceEntryMissing(databaseMiner, DbDto.Topic.BOTS, DbResourceDto.Locale.ITALY, "33333333");
-    }
-
-    private static <T> T readObjectFromResource(Class<T> objectClass, String resource) throws URISyntaxException, IOException {
-        URI resourceURI = thisClass.getResource(resource).toURI();
-        return new ObjectMapper().readValue(new File(resourceURI), objectClass);
-    }
-
-    private static DatabasePatcher createPatcher(List<DbDto> databaseObjects) throws ReflectiveOperationException {
-        return AbstractDatabaseHolder.prepare(DatabasePatcher.class, databaseObjects);
     }
 
     private static void assertResourceEntryPresentAndMatch(BulkDatabaseMiner databaseMiner, DbDto.Topic topic, DbResourceDto.Locale locale, String ref, String value) {
