0b07f34cf59c8140fa7963c9aae046358f6c409f
Jerome CANLER <jerome.canler@ext.pmu.fr>
2015-12-22 14:47:00 +0100

    clean(gui/database) : reset default database directory to empty

--- gui-database/src/main/java/fr/tduf/gui/database/common/SettingsConstants.java
+++ gui-database/src/main/java/fr/tduf/gui/database/common/SettingsConstants.java
@@ -4,7 +4,7 @@ package fr.tduf.gui.database.common;
  * Gives all constants needed to default settings.
  */
 public class SettingsConstants {
-    public static final String DATABASE_DIRECTORY_DEFAULT = "/media/sf_DevStore/GIT/tduf/cli/integ-tests/db-json/";
+    public static final String DATABASE_DIRECTORY_DEFAULT = "";
 
     public static final String PATH_RESOURCE_PROFILES = "/layout/defaultProfiles.json";
 }
