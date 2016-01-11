ad521fa4c50ae72720a1abf734669f79510c6ae0
djey <j.canler@gmail.com>
2016-01-11 21:06:09 +0100

    feature(gui/installer) : add database miner in context

--- gui-installer/src/main/java/fr/tduf/gui/installer/domain/DatabaseContext.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/domain/DatabaseContext.java
@@ -1,8 +1,11 @@
 package fr.tduf.gui.installer.domain;
 
+import fr.tduf.libunlimited.high.files.db.miner.BulkDatabaseMiner;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
 
 import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Stores all information about loaded database.
@@ -12,9 +15,12 @@ public class DatabaseContext {
 
     private final String jsonDatabaseDirectory;
 
+    private final BulkDatabaseMiner miner;
+
     public DatabaseContext(List<DbDto> topicObjects, String jsonDatabaseDirectory) {
-        this.topicObjects = topicObjects;
-        this.jsonDatabaseDirectory = jsonDatabaseDirectory;
+        this.topicObjects = requireNonNull(topicObjects, "A list of database objects is required.");
+        this.jsonDatabaseDirectory = requireNonNull(jsonDatabaseDirectory, "A directory in which JSON database exists is required.");
+        this.miner = BulkDatabaseMiner.load(topicObjects);
     }
 
     public List<DbDto> getTopicObjects() {
@@ -23,5 +29,9 @@ public class DatabaseContext {
 
     public String getJsonDatabaseDirectory() {
         return jsonDatabaseDirectory;
+    }
+
+    public BulkDatabaseMiner getMiner() {
+        return miner;
     }
 }
