7244b055a88f2a065deaca36edcdc2d826e3f91c
Jerome CANLER <jerome.canler@ext.pmu.fr>
2016-01-22 18:22:42 +0100

    feature(lib/database) : init unit tests for placeholders - WIP

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/DatabasePatcher_focusOnPlaceholdersTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/DatabasePatcher_focusOnPlaceholdersTest.java
@@ -1,4 +1,16 @@
 package fr.tduf.libunlimited.high.files.db.patcher;
 
+import org.junit.Test;
+
 public class DatabasePatcher_focusOnPlaceholdersTest {
+
+    @Test
+    public void apply_whenUpdateContents_forRef_withProperty_shouldUsePropertyValue() {
+
+    }
+
+    @Test
+    public void apply_whenUpdateContents_forRef_withoutProperty_shouldUseGeneratedValue() {
+
+    }
 }
