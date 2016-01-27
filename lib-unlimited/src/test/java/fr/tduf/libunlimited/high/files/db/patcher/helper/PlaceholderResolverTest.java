3268d8b1fac2b95dae691b1325fbf1fd2a5621af
djey <j.canler@gmail.com>
2016-01-27 18:48:12 +0100

    test(lib/database) : add unit tests

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/helper/PlaceholderResolverTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/db/patcher/helper/PlaceholderResolverTest.java
@@ -12,8 +12,6 @@ import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.CAR_PHYSICS_DATA
 import static fr.tduf.libunlimited.low.files.db.dto.DbResourceDto.Locale.FRANCE;
 import static fr.tduf.libunlimited.low.files.db.dto.DbStructureDto.FieldType.RESOURCE_CURRENT_LOCALIZED;
 import static fr.tduf.libunlimited.low.files.db.dto.DbStructureDto.FieldType.UID;
-import static java.util.Optional.empty;
-import static java.util.Optional.of;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlaceholderResolverTest {
@@ -40,13 +38,40 @@ public class PlaceholderResolverTest {
     }
 
     @Test
+    public void resolveValuePlaceholder_whenNoPlaceholder_shouldReturnInitialValue() {
+        // GIVEN-WHEN-THEN
+        assertThat(
+                PlaceholderResolver.resolveValuePlaceholder("FOO", new PatchProperties())
+        ).isEqualTo("FOO");
+    }
+
+    @Test
+    public void resolveValuePlaceholder_whenPlaceholder_withProperty_shouldReturnPropertyValue() {
+        // GIVEN
+        final PatchProperties patchProperties = new PatchProperties();
+        patchProperties.register("FOO", "1");
+
+        // WHEN-THEN
+        assertThat(
+                PlaceholderResolver.resolveValuePlaceholder("{FOO}", patchProperties)
+        ).isEqualTo("1");
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void resolveValuePlaceholder_whenPlaceholder_withoutProperty_shouldThrowException() {
+        // GIVEN-WHEN-THEN
+        assertThat(
+                PlaceholderResolver.resolveValuePlaceholder("{FOO}", new PatchProperties())
+        ).isEqualTo("1");
+    }
+
+    @Test
     public void resolveContentsReferencePlaceholder_whenNoPlaceholder_shouldReturnInitialValue() {
         // GIVEN-WHEN-THEN
         assertThat(
                 PlaceholderResolver.resolveContentsReferencePlaceholder("FOO", new PatchProperties(), databaseObject)
         ).isEqualTo("FOO");
     }
-
     @Test
     public void resolveContentsReferencePlaceholder_whenPlaceholder_withProperty_shouldReturnPropertyValue() {
         // GIVEN
@@ -94,6 +119,4 @@ public class PlaceholderResolverTest {
                 PlaceholderResolver.resolveResourceReferencePlaceholder("{FOO}", new PatchProperties(), databaseObject)
         ).isNotNull();
     }
-
-
 }
