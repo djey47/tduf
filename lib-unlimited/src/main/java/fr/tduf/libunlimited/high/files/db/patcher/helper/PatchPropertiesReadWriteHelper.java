d207d38537161d5b2c47c078c815d49aad4a288d
djey <j.canler@gmail.com>
2016-01-28 15:31:13 +0100

    feature(lib/database) : introduce helper class

---
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/patcher/helper/PatchPropertiesReadWriteHelper.java
@@ -0,0 +1,69 @@
+package fr.tduf.libunlimited.high.files.db.patcher.helper;
+
+import com.esotericsoftware.minlog.Log;
+import fr.tduf.libunlimited.high.files.db.patcher.domain.PatchProperties;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+
+/**
+ * Helper class to open or save patch properties with disk.
+ */
+public class PatchPropertiesReadWriteHelper {
+    private static final String THIS_CLASS_NAME = PatchPropertiesReadWriteHelper.class.getSimpleName();
+
+    /**
+     * @param patchFile : JSON mini patch file to be applied
+     * @return loaded properties if a corresponding property file exists
+     * @throws IOException
+     */
+    public static PatchProperties readPatchProperties(File patchFile) throws IOException {
+        String propertyFile = patchFile + ".properties";
+
+        final PatchProperties patchProperties = new PatchProperties();
+        final File propertyFileHandle = new File(propertyFile);
+        if(propertyFileHandle.exists()) {
+
+            Log.info(THIS_CLASS_NAME, "Using patch properties file: " + propertyFile);
+
+            final InputStream inputStream = new FileInputStream(propertyFileHandle);
+            patchProperties.load(inputStream);
+
+        } else {
+
+            Log.info(THIS_CLASS_NAME, "Patch properties file not provided: " + propertyFile);
+
+        }
+
+        return patchProperties;
+    }
+
+    /**
+     * @param patchFile : JSON mini patch file which has just been applied
+     * @return path of effective property file if any, otherwise empty
+     * @throws IOException
+     */
+    public static Optional<String> writePatchProperties(PatchProperties patchProperties, String patchFile) throws IOException {
+        if (patchProperties.isEmpty()) {
+            return empty();
+        }
+
+        final Path patchPath = Paths.get(patchFile);
+        Path patchParentPath = patchPath.getParent();
+        String patchFileName = patchPath.getFileName().toString();
+        final String targetFileName = "effective-" + patchFileName + ".properties";
+        String targetPropertyFile = patchParentPath.resolve(targetFileName).toString();
+
+        Log.info(THIS_CLASS_NAME, "Writing properties file: " + targetPropertyFile);
+
+        final OutputStream outputStream = new FileOutputStream(targetPropertyFile);
+        patchProperties.store(outputStream, null);
+
+        return of(targetPropertyFile);
+    }
+}
