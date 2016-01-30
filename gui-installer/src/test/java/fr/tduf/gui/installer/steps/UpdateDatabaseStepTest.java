d6d8c31f7f2c590059edf86dab53bbb6a1643df0
djey <j.canler@gmail.com>
2016-01-30 13:17:28 +0100

    refactor(gui/installer) : use helper method to get DatabaseContext from a directory with JSON files

--- gui-installer/src/test/java/fr/tduf/gui/installer/steps/UpdateDatabaseStepTest.java
+++ gui-installer/src/test/java/fr/tduf/gui/installer/steps/UpdateDatabaseStepTest.java
@@ -4,8 +4,6 @@ import fr.tduf.gui.installer.common.helper.TestHelper;
 import fr.tduf.gui.installer.domain.DatabaseContext;
 import fr.tduf.gui.installer.domain.InstallerConfiguration;
 import fr.tduf.libunlimited.high.files.banks.BankSupport;
-import fr.tduf.libunlimited.low.files.db.dto.DbDto;
-import fr.tduf.libunlimited.low.files.db.rw.helper.DatabaseReadWriteHelper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,13 +13,9 @@ import org.mockito.runners.MockitoJUnitRunner;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
 
 import static fr.tduf.gui.installer.steps.GenericStep.StepType.UPDATE_DATABASE;
-import static fr.tduf.libunlimited.low.files.db.rw.helper.DatabaseReadWriteHelper.EXTENSION_JSON;
 import static java.util.Optional.empty;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
@@ -36,18 +30,20 @@ public class UpdateDatabaseStepTest {
     @Mock
     private BankSupport bankSupportMock;
 
+    private DatabaseContext databaseContext;
+
     private String tempDirectory;
 
     @Before
     public void setUp() throws IOException {
+        databaseContext = TestHelper.createJsonDatabase();
+
         tempDirectory = TestHelper.createTempDirectory();
     }
 
     @Test
     public void applyPatches_shouldNotCrash() throws URISyntaxException, IOException, ReflectiveOperationException {
         // GIVEN
-        DatabaseContext databaseContext = createJsonDatabase();
-
         String assetsDirectory = new File(thisClass.getResource("/assets-patch-only").toURI()).getAbsolutePath();
         InstallerConfiguration configuration = InstallerConfiguration.builder()
                 .withTestDriveUnlimitedDirectory(tempDirectory)
@@ -68,7 +64,6 @@ public class UpdateDatabaseStepTest {
     @Test
     public void repackJsonDatabase_shouldCallBankSupportComponent() throws IOException, ReflectiveOperationException {
         // GIVEN
-        DatabaseContext databaseContext = createJsonDatabase();
         TestHelper.createFakeDatabase(databaseContext.getJsonDatabaseDirectory(), "original-");
 
         InstallerConfiguration configuration = InstallerConfiguration.builder()
@@ -97,28 +92,5 @@ public class UpdateDatabaseStepTest {
         verify(bankSupportMock).packAll(anyString(), eq(databasePath.resolve("DB_SP.bnk").toString()));
         verify(bankSupportMock).packAll(anyString(), eq(databasePath.resolve("DB_US.bnk").toString()));
         verifyNoMoreInteractions(bankSupportMock);
-    }
-
-    private static DatabaseContext createJsonDatabase() throws IOException {
-        String jsonDatabaseDirectory = TestHelper.createTempDirectory();
-
-        Path jsonDatabasePath = Paths.get(thisClass.getResource("/db-json").getFile());
-        Files.walk(jsonDatabasePath)
-
-                .filter((path) -> Files.isRegularFile(path))
-
-                .filter((path) -> EXTENSION_JSON.equalsIgnoreCase(com.google.common.io.Files.getFileExtension(path.toString())))
-
-                .forEach((path) -> {
-                    try {
-                        Files.copy(path, Paths.get(jsonDatabaseDirectory).resolve(path.getFileName()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                });
-
-        List<DbDto> topicObjects = DatabaseReadWriteHelper.readFullDatabaseFromJson(jsonDatabaseDirectory);
-
-        return new DatabaseContext(topicObjects, jsonDatabaseDirectory);
     }
 }
