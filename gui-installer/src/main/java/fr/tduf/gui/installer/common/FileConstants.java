770bd3cfa31952c384da107c4fc03885bd7be5f5
djey <j.canler@gmail.com>
2016-01-30 14:29:32 +0100

    feature(gui/installer) : introduce new class to host constants

---
+++ gui-installer/src/main/java/fr/tduf/gui/installer/common/FileConstants.java
@@ -0,0 +1,10 @@
+package fr.tduf.gui.installer.common;
+
+/**
+ * Gives all constants to access particular information about TDU files.
+ */
+public class FileConstants {
+    public static final String SUFFIX_INTERIOR_BANK_FILE = "_I";
+
+    public static final String SUFFIX_AUDIO_BANK_FILE = "_audio";
+}
