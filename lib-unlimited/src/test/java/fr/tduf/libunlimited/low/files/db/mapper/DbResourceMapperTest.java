32fafd8cb04fda5790a8198c617e9c44cfb11365
djey <j.canler@gmail.com>
2016-02-17 15:55:09 +0100

    refactor(lib/database) : adapt test to resource model v2

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/db/mapper/DbResourceMapperTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/db/mapper/DbResourceMapperTest.java
@@ -1,7 +1,8 @@
 package fr.tduf.libunlimited.low.files.db.mapper;
 
 import com.esotericsoftware.minlog.Log;
-import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
+import fr.tduf.libunlimited.common.helper.FilesHelper;
+import fr.tduf.libunlimited.low.files.db.dto.DbResourceEnhancedDto;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 import org.json.JSONException;
@@ -10,8 +11,11 @@ import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import static fr.tduf.libunlimited.low.files.db.dto.DbResourceEnhancedDto.Locale.FRANCE;
+import static fr.tduf.libunlimited.low.files.db.dto.DbResourceEnhancedDto.Locale.ITALY;
+import static java.util.Arrays.asList;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 public class DbResourceMapperTest {
@@ -26,39 +30,45 @@ public class DbResourceMapperTest {
     }
 
     @Test
-    public void serialize_shouldWriteProperJson() throws IOException, JSONException {
+    public void serialize_shouldWriteProperJson() throws IOException, JSONException, URISyntaxException {
         //GIVEN
-        DbResourceDto.Entry entry1 = DbResourceDto.Entry.builder()
-                .forReference("53410835")
-                .withValue("VAL1")
-                .build();
-        DbResourceDto.Entry entry2 = DbResourceDto.Entry.builder()
-                .forReference("54410835")
-                .withValue("VAL2")
-                .build();
-        DbResourceDto dbResourceDto = DbResourceDto.builder()
-                .atVersion("1,2")
+        DbResourceEnhancedDto.Item item1 = DbResourceEnhancedDto.Item.builder()
                 .withLocale(FRANCE)
-                .withCategoryCount(6)
-                .addEntry(entry1)
-                .addEntry(entry2)
+                .withValue("VAL1_FR")
                 .build();
-        String expectedJson = "{\n" +
-                "  \"locale\" : \"FRANCE\",\n" +
-                "  \"version\" : \"1,2\",\n" +
-                "  \"categoryCount\" : 6,\n" +
-                "  \"entries\" : [ {\n" +
-                "    \"ref\" : \"53410835\",\n" +
-                "    \"value\" : \"VAL1\"\n" +
-                "  }, {\n" +
-                "    \"ref\" : \"54410835\",\n" +
-                "    \"value\" : \"VAL2\"\n" +
-                "  } ]\n" +
-                "}";
+        DbResourceEnhancedDto.Item item2 = DbResourceEnhancedDto.Item.builder()
+                .withLocale(ITALY)
+                .withValue("VAL1_IT")
+                .build();
+        DbResourceEnhancedDto.Entry entry1 = DbResourceEnhancedDto.Entry.builder()
+                .forReference("53410835")
+                .withItems(asList(item1, item2))
+                .build();
+        DbResourceEnhancedDto.Item item3 = DbResourceEnhancedDto.Item.builder()
+                .withLocale(FRANCE)
+                .withValue("VAL2_FR")
+                .build();
+        DbResourceEnhancedDto.Item item4 = DbResourceEnhancedDto.Item.builder()
+                .withLocale(ITALY)
+                .withValue("VAL2_IT")
+                .build();
+        DbResourceEnhancedDto.Entry entry2 = DbResourceEnhancedDto.Entry.builder()
+                .forReference("54410835")
+                .withItems(asList(item3, item4))
+                .build();
+        DbResourceEnhancedDto dbResourceEnhancedDto = DbResourceEnhancedDto.builder()
+                .withCategoryCount(1)
+                .atVersion("1,0")
+                .containingEntries(asList(entry1, entry2))
+                .build();
+
+        String expectedJson = FilesHelper.readTextFromResourceFile("/db/json/mapper/resourceObject.json");
+
 
         //WHEN
-        String jsonResult = objectWriter.writeValueAsString(dbResourceDto);
+        String jsonResult = objectWriter.writeValueAsString(dbResourceEnhancedDto);
         Log.debug(thisClass.getSimpleName(), "Actual JSON:" + jsonResult);
+
 
         //THEN
         assertEquals(expectedJson, jsonResult, JSONCompareMode.STRICT);
