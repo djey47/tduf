217454aa146243866f2d12cf61b158741fc59717
djey <j.canler@gmail.com>
2016-02-17 15:46:33 +0100

    refactor(lib/database) : adapt test to resource model v2

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/db/mapper/DbMapperTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/db/mapper/DbMapperTest.java
@@ -1,21 +1,23 @@
 package fr.tduf.libunlimited.low.files.db.mapper;
 
 import com.esotericsoftware.minlog.Log;
+import fr.tduf.libunlimited.common.helper.FilesHelper;
 import fr.tduf.libunlimited.low.files.db.dto.DbDataDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
-import fr.tduf.libunlimited.low.files.db.dto.DbResourceDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbResourceEnhancedDto;
 import fr.tduf.libunlimited.low.files.db.dto.DbStructureDto;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 import org.json.JSONException;
 import org.junit.Before;
 import org.junit.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.ACHIEVEMENTS;
+import static java.util.Collections.singletonList;
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
 public class DbMapperTest {
@@ -30,7 +32,7 @@ public class DbMapperTest {
     }
 
     @Test
-    public void serialize_shouldWriteProperJson() throws IOException, JSONException {
+    public void serialize_shouldWriteProperJson() throws IOException, JSONException, URISyntaxException {
         //GIVEN
         DbStructureDto dbStructureDto = DbStructureDto.builder()
                 .forReference("2442784645")
@@ -41,29 +43,20 @@ public class DbMapperTest {
         DbDto dbTopicDto = DbDto.builder()
                 .withStructure(dbStructureDto)
                 .withData(DbDataDto.builder().build())
-                .addResource(DbResourceDto.builder().build())
-                .addResource(DbResourceDto.builder().build())
+                .withResource(DbResourceEnhancedDto.builder()
+                        .atVersion("1,2")
+                        .withCategoryCount(6)
+                        .containingEntries(singletonList(DbResourceEnhancedDto.Entry.builder()
+                                .forReference("REF")
+                                .withItems(singletonList(DbResourceEnhancedDto.Item.builder()
+                                        .withLocale(DbResourceEnhancedDto.Locale.FRANCE)
+                                        .withValue("VAL")
+                                        .build()))
+                                .build()))
+                        .build())
                 .build();
 
-        String expectedJson = "{\n" +
-                "  \"structure\" : {\n" +
-                "    \"ref\" : \"2442784645\",\n" +
-                "    \"topic\" : \"ACHIEVEMENTS\",\n" +
-                "    \"version\" : \"1,0\",\n" +
-                "    \"categoryCount\" :  5,\n" +
-                "    \"fields\" : [ ]\n" +
-                "  },\n" +
-                "  \"data\" : {\n" +
-                "    \"entries\" : [ ]\n" +
-                "  },\n" +
-                "  \"resources\" : [ {\n" +
-                "    \"entries\" : [ ],\n" +
-                "    \"categoryCount\" : 0\n" +
-                "  }, {\n" +
-                "    \"entries\" : [ ],\n" +
-                "    \"categoryCount\" : 0\n" +
-                "  } ]\n" +
-                "}";
+        String expectedJson = FilesHelper.readTextFromResourceFile("/db/json/mapper/topicObject.json");
 
 
         //WHEN
