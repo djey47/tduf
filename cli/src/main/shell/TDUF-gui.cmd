d0288b332d8c748c73f9acf229e1ab4f6b9af154
djey <j.canler@gmail.com>
2016-03-08 16:35:40 +0100

    feature(gui/database) : remove obsolete cli unpack/repack and grant admin rights

--- cli/src/main/shell/TDUF-gui.cmd
+++ cli/src/main/shell/TDUF-gui.cmd
@@ -1,8 +1,35 @@
-@ECHO OFF
+@echo off
+
+SET START_DIR=%~dp0
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (shift & goto gotPrivileges)
+
+REM Invoking UAC for Privilege Escalation
+
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs"
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs"
+"%temp%\OEgetPrivileges.vbs"
+exit /B
+
+:gotPrivileges
+
+REM Running Admin shell
+
+setlocal & pushd .
+
+CD /D %START_DIR%
 
 MKDIR logs 2>NUL
 CD cli 2>NUL
 
 CALL .\CheckJava.cmd
 CALL .\SetVersion.cmd
-CALL .\DatabaseEditor-QuickLauncher.cmd %* > ..\logs\TDUF-gui.log 2>>&1
+CALL .\DatabaseEditor-QuickLauncher.cmd
--- src/main/shell/cli-util/DatabaseEditor-QuickLauncher.cmd
+++ src/main/shell/cli-util/DatabaseEditor-QuickLauncher.cmd
@@ -1,50 +1,14 @@
 @ECHO OFF
-IF [%1]==[] ( GOTO askForParam ) ELSE ( GOTO startGui )
-
-:askForParam
-ECHO Missing parameter: database directory.
-ECHO.
-SETLOCAL DisableDelayedExpansion
-SET "batchPath=%~0"
-SETLOCAL EnableDelayedExpansion
-SET "script=%temp%\OEgetDatabaseDir.vbs"
-ECHO Dim directory > %script%
-ECHO directory = InputBox^("This tool will unpack and convert TDU database for you. When finished, whole database will be repacked." ^& vbCrLf ^& "Please enter TDU Database Directory for BNK files:", "TDUF Database Editor: Quick Launcher", "C:\Programs\Atari\Test Drive Unlimited"^) >> %script%
-ECHO If ^(directory = ""^) Then WScript.Quit >> %script%
-ECHO Set shell = CreateObject ^("WScript.Shell"^) >> %script%
-ECHO shell.run "!batchPath!" ^& " " ^& directory >> %script%
-CALL %script%
-GOTO exitSuccess
-
-:startGui
-ECHO *** TDUF DATABASE EDITOR QUICK LAUNCHER ***
-ECHO.
-ECHO ...Unpacking TDU database from %1, please wait...
-CALL DatabaseTool unpack-all -d %1 -j %1\tduf-json > ..\logs\DatabaseEditor.log 2>>&1
-if ERRORLEVEL 1 GOTO handleUnpackError
-	
 ECHO ...Starting Database Editor...
-CALL DatabaseEditor %1\tduf-json >> ..\logs\DatabaseEditor.log 2>>&1
+CALL DatabaseEditor %1 >> ..\logs\DatabaseEditor.log 2>>&1
 if ERRORLEVEL 1 GOTO handleEditorError
-
-ECHO ...Database Editor ended, now repacking database to %1, please wait...
-CALL DatabaseTool repack-all -o %1 -j %1\tduf-json >> ..\logs\DatabaseEditor.log 2>>&1
-if ERRORLEVEL 1 GOTO handleRepackError
 
 ECHO.
 ECHO All done!
 GOTO exitSuccess
 
-:handleUnpackError
-ECHO Unpacking failed, can't continue.
-GOTO exitFailure
-
 :handleEditorError
 ECHO Editor failed, can't continue. Changes won't be applied.
-GOTO exitFailure
-
-:handleRepackError
-ECHO Repacking failed. Changes won't be applied.
 GOTO exitFailure
 
 :exitSuccess
