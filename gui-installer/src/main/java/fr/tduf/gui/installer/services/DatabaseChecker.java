fd490a7c0a679bb2e7c0a45e4fa031b0bad0316b
djey <j.canler@gmail.com>
2016-03-28 21:38:30 +0200

    refactor(gui/installer) : use component to load database with cache support

--- gui-installer/src/main/java/fr/tduf/gui/installer/services/DatabaseChecker.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/services/DatabaseChecker.java
@@ -1,12 +1,11 @@
 package fr.tduf.gui.installer.services;
 
+import fr.tduf.libunlimited.common.cache.DatabaseBanksCacheHelper;
 import fr.tduf.libunlimited.high.files.banks.BankSupport;
 import fr.tduf.libunlimited.high.files.db.common.AbstractDatabaseHolder;
 import fr.tduf.libunlimited.high.files.db.integrity.DatabaseIntegrityChecker;
 import fr.tduf.libunlimited.low.files.db.domain.IntegrityError;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
-import fr.tduf.libunlimited.low.files.db.rw.JsonGateway;
-import fr.tduf.libunlimited.low.files.db.rw.helper.DatabaseBankHelper;
 import fr.tduf.libunlimited.low.files.db.rw.helper.DatabaseReadWriteHelper;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
@@ -15,8 +14,9 @@ import javafx.beans.property.StringProperty;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
-import java.nio.file.Files;
-import java.util.*;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Background service to load and check TDU database from banks directory.
@@ -31,31 +31,19 @@ public class DatabaseChecker extends Service<Set<IntegrityError>> {
             @Override
             protected Set<IntegrityError> call() throws Exception {
 
-                // TODO externalize steps 1-3 to common module
-                updateMessage("Performing database check 1/4, please wait...");
+                updateMessage("Performing database check 1/3, please wait...");
+                String jsonDirectory = DatabaseBanksCacheHelper.unpackDatabaseToJsonWithCacheSupport(Paths.get(databaseLocation.get()), bankSupport.get());
 
-                String unpackedDirectory = DatabaseBankHelper.unpackDatabaseFromDirectory(databaseLocation.get(), Optional.empty(), bankSupport.get());
-
-                updateMessage("Performing database check 2/4, please wait...");
-
-                final String jsonDirectory = Files.createTempDirectory("guiInstaller-databaseCheck").toString();
-                Set<IntegrityError> integrityErrors = new HashSet<>();
-                JsonGateway.dump(unpackedDirectory, jsonDirectory, new ArrayList<>(), integrityErrors);
-
-                updateMessage("Performing database check 3/4, please wait...");
-
+                updateMessage("Performing database check 2/3, please wait...");
                 final List<DbDto> databaseObjects = DatabaseReadWriteHelper.readFullDatabaseFromJson(jsonDirectory);
 
-                updateMessage("Performing database check 4/4, please wait...");
-
+                updateMessage("Performing database check 3/3, please wait...");
                 final DatabaseIntegrityChecker checkerComponent = AbstractDatabaseHolder.prepare(DatabaseIntegrityChecker.class, databaseObjects);
                 final Set<IntegrityError> integrityErrorsFromExtensiveCheck = checkerComponent.checkAllContentsObjects();
 
-                integrityErrors.addAll(integrityErrorsFromExtensiveCheck);
+                updateMessage("Done checking database, " + integrityErrorsFromExtensiveCheck.size() + " error(s).");
 
-                updateMessage("Done checking database, " + integrityErrors.size() + " error(s).");
-
-                return integrityErrors;
+                return integrityErrorsFromExtensiveCheck;
             }
         };
     }
