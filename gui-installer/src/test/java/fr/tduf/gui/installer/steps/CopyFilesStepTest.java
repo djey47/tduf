810e8ae3e6783648b99484ea04d9daf249ea5d53
djey <j.canler@gmail.com>
2016-03-29 13:33:08 +0200

    test(gui/installer) : add test for bike case

--- gui-installer/src/test/java/fr/tduf/gui/installer/steps/CopyFilesStepTest.java
+++ gui-installer/src/test/java/fr/tduf/gui/installer/steps/CopyFilesStepTest.java
@@ -16,7 +16,6 @@ import java.nio.file.Paths;
 import static fr.tduf.gui.installer.steps.GenericStep.StepType.COPY_FILES;
 import static org.assertj.core.api.StrictAssertions.assertThat;
 
-// TODO enhance test with front and rear rims having different names
 public class CopyFilesStepTest {
 
     private static final Class<CopyFilesStepTest> thisClass = CopyFilesStepTest.class;
@@ -35,14 +34,14 @@ public class CopyFilesStepTest {
         // GIVEN
         System.out.println("Testing TDU directory: " + tempDirectory);
 
-        String assetsDirectory = new File(thisClass.getResource("/assets-all").toURI()).getAbsolutePath();
+        String assetsDirectory = new File(thisClass.getResource("/assets-all/car").toURI()).getAbsolutePath();
         InstallerConfiguration configuration = InstallerConfiguration.builder()
                 .withTestDriveUnlimitedDirectory(tempDirectory)
                 .withAssetsDirectory(assetsDirectory)
                 .build();
         DatabaseContext databaseContext = InstallerTestsHelper.createJsonDatabase();
         PatchProperties patchProperties = new PatchProperties();
-        patchProperties.setVehicleSlotReferenceIfNotExists("606298799");
+        patchProperties.setVehicleSlotReferenceIfNotExists("606298799"); // AC427 (car)
 
 
         // WHEN
@@ -51,11 +50,12 @@ public class CopyFilesStepTest {
 
 
         // THEN
-        Path vehicleBanksPath = Paths.get(tempDirectory, "Euro", "Bnk", "Vehicules");
-        Path soundBanksPath = Paths.get(tempDirectory, "Euro", "Bnk", "Sound", "Vehicules");
-        Path highHudBanksPath = Paths.get(tempDirectory, "Euro", "Bnk", "FrontEnd", "HiRes", "Gauges");
-        Path lowHudBanksPath = Paths.get(tempDirectory, "Euro", "Bnk", "FrontEnd", "LowRes", "Gauges");
-        Path rimBanksPath = Paths.get(tempDirectory, "Euro", "Bnk", "Vehicules", "Rim", "AC");
+        Path banksPath = Paths.get(tempDirectory, "Euro", "Bnk");
+        Path vehicleBanksPath = banksPath.resolve("Vehicules");
+        Path soundBanksPath = banksPath.resolve("Sound").resolve("Vehicules");
+        Path highHudBanksPath = banksPath.resolve("FrontEnd").resolve("HiRes").resolve("Gauges");
+        Path lowHudBanksPath = banksPath.resolve("FrontEnd").resolve("LowRes").resolve("Gauges");
+        Path rimBanksPath = vehicleBanksPath.resolve("Rim").resolve("AC");
 
         Path vehicleModelAssetsPath = Paths.get(assetsDirectory, "3D");
         assertThat(vehicleBanksPath.resolve("AC_427.bnk").toFile())
@@ -82,5 +82,37 @@ public class CopyFilesStepTest {
         assertThat(rimBanksPath.resolve("AC_427_F_01.bnk").toFile())
                 .exists()
                 .hasSameContentAs(rimAssetsPath.resolve("AC_289_F_01.bnk").toFile());
+    }
+
+    @Test
+    public void copyFilesStep_withDifferentRimsFrontRear_shouldCopyThemToCorrectLocation_withRightNames() throws Exception {
+        // GIVEN
+        System.out.println("Testing TDU directory: " + tempDirectory);
+
+        String assetsDirectory = new File(thisClass.getResource("/assets-all/bike").toURI()).getAbsolutePath();
+        InstallerConfiguration configuration = InstallerConfiguration.builder()
+                .withTestDriveUnlimitedDirectory(tempDirectory)
+                .withAssetsDirectory(assetsDirectory)
+                .build();
+        DatabaseContext databaseContext = InstallerTestsHelper.createJsonDatabase();
+        PatchProperties patchProperties = new PatchProperties();
+        patchProperties.setVehicleSlotReferenceIfNotExists("1208897332"); // Triumph Daytona (bike)
+
+
+        // WHEN
+        GenericStep.starterStep(configuration, databaseContext, patchProperties)
+                .nextStep(COPY_FILES).start();
+
+
+        // THEN
+        Path rimBanksPath = Paths.get(tempDirectory, "Euro", "Bnk", "Vehicules", "Rim", "Triumph");
+
+        Path rimAssetsPath = Paths.get(assetsDirectory, "3D", "RIMS");
+        assertThat(rimBanksPath.resolve("DAYTONA_955I_F.bnk").toFile())
+                .exists()
+                .hasSameContentAs(rimAssetsPath.resolve("BIKE_F_01.bnk").toFile());
+        assertThat(rimBanksPath.resolve("DAYTONA_955I_R.bnk").toFile())
+                .exists()
+                .hasSameContentAs(rimAssetsPath.resolve("BIKE_R_01.bnk").toFile());
     }
 }
