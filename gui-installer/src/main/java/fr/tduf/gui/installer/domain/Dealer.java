e91b76c453a89cb25ad9f3a254f1a2e856d93069
djey <j.canler@gmail.com>
2016-04-07 00:15:36 +0200

    feature(gui/installer) : create dealer domain object

---
+++ gui-installer/src/main/java/fr/tduf/gui/installer/domain/Dealer.java
@@ -0,0 +1,31 @@
+package fr.tduf.gui.installer.domain;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Domain object representing dealer information.
+ */
+public class Dealer {
+    private String ref;
+
+    private Dealer(String ref) {
+        this.ref = requireNonNull(ref, "A dealer reference is required.");
+    }
+
+    public static DealerBuilder builder() {
+        return new DealerBuilder();
+    }
+
+    public static class DealerBuilder {
+        private String ref;
+
+        public DealerBuilder withRef(String ref) {
+            this.ref = ref;
+            return this;
+        }
+
+        public Dealer build() {
+            return new Dealer(ref);
+        }
+    }
+}
