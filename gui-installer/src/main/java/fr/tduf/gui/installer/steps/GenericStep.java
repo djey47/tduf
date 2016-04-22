6b19a5d3a9c520cbc98c1f6dc6d1100d1cfd23c9
djey <j.canler@gmail.com>
2016-04-22 11:00:26 +0200

    feature(gui/installer) : create database backup step - WIP

--- gui-installer/src/main/java/fr/tduf/gui/installer/steps/GenericStep.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/steps/GenericStep.java
@@ -14,7 +14,7 @@ import static java.util.Objects.requireNonNull;
  * Parent of all install steps
  */
 public abstract class GenericStep {
-    public enum StepType { UNDEFINED, LOAD_DATABASE, UPDATE_DATABASE, SAVE_DATABASE, BACKUP_DATABASE, UPDATE_MAGIC_MAP, LOAD_PATCH, SELECT_SLOTS, COPY_FILES }
+    public enum StepType { UNDEFINED, LOAD_DATABASE, UPDATE_DATABASE, SAVE_DATABASE, BACKUP_DATABASE, RESTORE_DATABASE, UPDATE_MAGIC_MAP, LOAD_PATCH, SELECT_SLOTS, COPY_FILES }
 
     private StepType type;
 
@@ -61,6 +61,9 @@ public abstract class GenericStep {
         switch (stepType) {
             case BACKUP_DATABASE:
                 currentStep = new BackupDatabaseStep();
+                break;
+            case RESTORE_DATABASE:
+                currentStep = new RestoreDatabaseStep();
                 break;
             case UPDATE_DATABASE:
                 currentStep = new UpdateDatabaseStep();
---
+++ gui-installer/src/main/java/fr/tduf/gui/installer/steps/RestoreDatabaseStep.java
@@ -0,0 +1,13 @@
+package fr.tduf.gui.installer.steps;
+
+import java.io.IOException;
+
+/**
+ * Copy safe database banks files to game directory back.
+ */
+public class RestoreDatabaseStep extends GenericStep {
+    @Override
+    protected void perform() throws IOException, ReflectiveOperationException {
+
+    }
+}
