f149bb3bcbee700c315556966745e07a64f49764
djey <j.canler@gmail.com>
2016-04-26 00:26:02 +0200

    feature(gui/installer) : restore database files from backup location

--- gui-installer/src/main/java/fr/tduf/gui/installer/steps/RestoreDatabaseStep.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/steps/RestoreDatabaseStep.java
@@ -1,14 +1,40 @@
 package fr.tduf.gui.installer.steps;
 
+import com.esotericsoftware.minlog.Log;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Copy safe database banks files to game directory back.
  */
 public class RestoreDatabaseStep extends GenericStep {
+    private static final String THIS_CLASS_NAME = RestoreDatabaseStep.class.getSimpleName();
+
     @Override
     protected void perform() throws IOException, ReflectiveOperationException {
-        // TODO
+        requireNonNull(getInstallerConfiguration(), "Installer configuration is required.");
+        requireNonNull(getDatabaseContext(), "Database context is required.");
 
+        Path databasePath = Paths.get(getInstallerConfiguration().resolveDatabaseDirectory());
+        Path databaseBackupPath = Paths.get(getDatabaseContext().getBackupDatabaseDirectory());
+
+        Log.info(THIS_CLASS_NAME, "->Restoring current database from " + databaseBackupPath + "...");
+
+        Files.walk(databaseBackupPath, 1)
+                .filter(Files::isRegularFile)
+                .forEach((bankFilePath) -> {
+                    final Path originalFilePath = databasePath.resolve(bankFilePath.getFileName());
+                    try {
+                        Files.copy(bankFilePath, originalFilePath, StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException ioe) {
+                        throw new RuntimeException("Unable to restore backup: " + bankFilePath, ioe);
+                    }
+                });
     }
 }
---
+++ gui-installer/src/test/java/fr/tduf/gui/installer/steps/RestoreDatabaseStepTest.java
@@ -0,0 +1,46 @@
+package fr.tduf.gui.installer.steps;
+
+import fr.tduf.gui.installer.domain.DatabaseContext;
+import fr.tduf.gui.installer.domain.InstallerConfiguration;
+import fr.tduf.libtesting.common.helper.FilesHelper;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class RestoreDatabaseStepTest {
+
+    @Test
+    public void perform_shouldRestoreDatabaseFiles() throws Exception {
+        // GIVEN
+        final String tduTempDirectory = FilesHelper.createTempDirectoryForInstaller();
+        final Path tduDatabasePath = FilesHelper.getTduDatabasePath(tduTempDirectory);
+        Files.createDirectories(tduDatabasePath);
+
+        Path backupDatabasePath = Paths.get(tduTempDirectory, "backup", "database");
+        Path databaseBackupFile = backupDatabasePath.resolve("DB.BNK");
+        Files.createDirectories(backupDatabasePath);
+        Files.createFile(databaseBackupFile);
+
+        InstallerConfiguration installerConfiguration = InstallerConfiguration.builder()
+                .withTestDriveUnlimitedDirectory(tduTempDirectory)
+                .build();
+        DatabaseContext databaseContext = new DatabaseContext(new ArrayList<>(), "");
+        databaseContext.setBackupDatabaseDirectory(backupDatabasePath.toString());
+        GenericStep restoreDatabaseStep = GenericStep.starterStep(installerConfiguration, databaseContext)
+                .nextStep(GenericStep.StepType.RESTORE_DATABASE);
+
+
+        // WHEN
+        restoreDatabaseStep.perform();
+
+
+        // THEN
+        assertThat(tduDatabasePath.resolve("DB.BNK")).exists();
+    }
+}
