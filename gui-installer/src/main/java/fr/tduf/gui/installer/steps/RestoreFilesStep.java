967d09cd88ccbb28cc314be1b5013ad56db97626
djey <j.canler@gmail.com>
2016-04-26 01:20:47 +0200

    feature(gui/installer) : restore banks files from backup location

--- gui-installer/src/main/java/fr/tduf/gui/installer/steps/RestoreFilesStep.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/steps/RestoreFilesStep.java
@@ -1,14 +1,40 @@
 package fr.tduf.gui.installer.steps;
 
+import com.esotericsoftware.minlog.Log;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Copy safe banks files to game directory back.
  */
 public class RestoreFilesStep extends GenericStep {
+    private static final String THIS_CLASS_NAME = RestoreFilesStep.class.getSimpleName();
+
     @Override
     protected void perform() throws IOException, ReflectiveOperationException {
-        // TODO
+        requireNonNull(getInstallerConfiguration(), "Installer configuration is required.");
 
+        Path backupPath = Paths.get(getInstallerConfiguration().resolveFilesBackupDirectory());
+        Path banksPath = Paths.get(getInstallerConfiguration().resolveBanksDirectory());
+
+        Log.info(THIS_CLASS_NAME, "->Restoring files from " + backupPath + "...");
+
+        Files.walk(backupPath)
+                .filter(Files::isRegularFile)
+                .forEach((bankFilePath) -> {
+                    final Path subTree = backupPath.relativize(bankFilePath);
+                    final Path originalFilePath = banksPath.resolve(subTree);
+                    try {
+                        Files.copy(bankFilePath, originalFilePath, StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException ioe) {
+                        throw new RuntimeException("Unable to restore backup: " + bankFilePath, ioe);
+                    }
+                });
     }
 }
---
+++ gui-installer/src/test/java/fr/tduf/gui/installer/steps/RestoreFilesStepTest.java
@@ -0,0 +1,45 @@
+package fr.tduf.gui.installer.steps;
+
+import fr.tduf.gui.installer.domain.DatabaseContext;
+import fr.tduf.gui.installer.domain.InstallerConfiguration;
+import fr.tduf.libtesting.common.helper.FilesHelper;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RestoreFilesStepTest {
+
+    @Test
+    public void perform_shouldRestoreBackup() throws Exception {
+        // GIVEN
+        final String tduTempDirectory = FilesHelper.createTempDirectoryForInstaller();
+        Path bankFilesPath = Paths.get(tduTempDirectory).resolve("Euro").resolve("Bnk").resolve("Vehicules");
+        Files.createDirectories(bankFilesPath);
+
+        Path backupFilesPath = Paths.get(tduTempDirectory, "backup", "files");
+        Path backupFile = backupFilesPath.resolve("Vehicules").resolve("AC_427.BNK");
+        Files.createDirectories(backupFile.getParent());
+        Files.createFile(backupFile);
+
+        InstallerConfiguration installerConfiguration = InstallerConfiguration.builder()
+                .withTestDriveUnlimitedDirectory(tduTempDirectory)
+                .build();
+        installerConfiguration.setBackupDirectory(backupFilesPath.getParent().toString());
+        DatabaseContext databaseContext = new DatabaseContext(new ArrayList<>(), "");
+        GenericStep restoreFilesStep = GenericStep.starterStep(installerConfiguration, databaseContext)
+                .nextStep(GenericStep.StepType.RESTORE_FILES);
+
+
+        // WHEN
+        restoreFilesStep.perform();
+
+
+        // THEN
+        assertThat(bankFilesPath.resolve("AC_427.BNK")).exists();
+    }
+}
