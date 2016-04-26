df133c8cf5bb339d6c872c8a417973a7e8fce830
djey <j.canler@gmail.com>
2016-04-26 12:26:32 +0200

    buildfix(gui/installer) : adapt tests to new error handling

--- gui-installer/src/test/java/fr/tduf/gui/installer/services/StepsCoordinatorTest.java
+++ gui-installer/src/test/java/fr/tduf/gui/installer/services/StepsCoordinatorTest.java
@@ -4,12 +4,17 @@ import fr.tduf.gui.installer.domain.DatabaseContext;
 import fr.tduf.gui.installer.domain.InstallerConfiguration;
 import fr.tduf.gui.installer.domain.exceptions.StepException;
 import fr.tduf.gui.installer.steps.GenericStep;
+import fr.tduf.libtesting.common.helper.FilesHelper;
 import fr.tduf.libtesting.common.helper.javafx.JavaFXThreadingRule;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,13 +24,19 @@ public class StepsCoordinatorTest {
     @Rule
     public JavaFXThreadingRule javaFXRule = new JavaFXThreadingRule();
 
+    private String tempDirectory;
+
+    @Before
+    public void setUp() throws IOException {
+        tempDirectory = FilesHelper.createTempDirectoryForInstaller();
+        Files.createDirectories(Paths.get(tempDirectory, "files"));
+    }
+
     @Test(expected=StepException.class)
     public void handleStepExceptionInInstallTask_whenNonCriticalError_shouldRethrowException() throws StepException {
         // GIVEN
-        ObjectProperty<InstallerConfiguration> configuration = new SimpleObjectProperty<>(InstallerConfiguration.builder()
-                .withTestDriveUnlimitedDirectory("")
-                .build());
-        ObjectProperty<DatabaseContext> context = new SimpleObjectProperty<>(new DatabaseContext(new ArrayList<>(), ""));
+        ObjectProperty<InstallerConfiguration> configuration = createInstallerConfiguration();
+        ObjectProperty<DatabaseContext> context = createDatabaseContext();
         StepsCoordinator.InstallTask installTask = new StepsCoordinator.InstallTask(configuration, context);
         StepException stepException = new StepException(GenericStep.StepType.UPDATE_MAGIC_MAP, "Non critical error for database", null);
 
@@ -41,10 +52,8 @@ public class StepsCoordinatorTest {
     @Test(expected=StepException.class)
     public void handleStepExceptionInInstallTask_whenDatabaseCriticalError_shouldRethrowException() throws StepException {
         // GIVEN
-        ObjectProperty<InstallerConfiguration> configuration = new SimpleObjectProperty<>(InstallerConfiguration.builder()
-                .withTestDriveUnlimitedDirectory("")
-                .build());
-        ObjectProperty<DatabaseContext> context = new SimpleObjectProperty<>(new DatabaseContext(new ArrayList<>(), ""));
+        ObjectProperty<InstallerConfiguration> configuration = createInstallerConfiguration();
+        ObjectProperty<DatabaseContext> context = createDatabaseContext();
         StepsCoordinator.InstallTask installTask = new StepsCoordinator.InstallTask(configuration, context);
         StepException stepException = new StepException(GenericStep.StepType.UPDATE_DATABASE, "Critical error for database", null);
 
@@ -55,5 +64,19 @@ public class StepsCoordinatorTest {
             assertThat(se).isEqualTo(stepException);
             throw se;
         }
+    }
+
+    private ObjectProperty<DatabaseContext> createDatabaseContext() {
+        final SimpleObjectProperty<DatabaseContext> context = new SimpleObjectProperty<>(new DatabaseContext(new ArrayList<>(), ""));
+        context.getValue().setBackupDatabaseDirectory(tempDirectory);
+        return context;
+    }
+
+    private ObjectProperty<InstallerConfiguration> createInstallerConfiguration() {
+        final SimpleObjectProperty<InstallerConfiguration> installerConfiguration = new SimpleObjectProperty<>(InstallerConfiguration.builder()
+                .withTestDriveUnlimitedDirectory(tempDirectory)
+                .build());
+        installerConfiguration.getValue().setBackupDirectory(tempDirectory);
+        return installerConfiguration;
     }
 }
