63fa008c6e6f3ab71a10f57d7025a588f9af3c6e
djey <j.canler@gmail.com>
2016-04-28 15:30:14 +0200

    feature(interop/cams) : add tdumtcli's in/out object

---
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/bin/cameras/interop/dto/GenuineCamViewsDto.java
@@ -0,0 +1,65 @@
+package fr.tduf.libunlimited.high.files.bin.cameras.interop.dto;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonTypeName;
+
+import java.util.List;
+
+/**
+ * In/out object from .net cli, CAM-I and CAM-C commands.
+ */
+@JsonTypeName("genuineCamViews")
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class GenuineCamViewsDto {
+
+    @JsonProperty("views")
+    private List<GenuineCamViewDto> views;
+
+    public List<GenuineCamViewDto> getViews() {
+        return views;
+    }
+
+    @JsonTypeName("genuineCamView")
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class  GenuineCamViewDto {
+        /** All handled view kinds **/
+        public enum Type {
+            Bumper, Bumper_Back,
+            Cockpit, Cockpit_Back,
+            Follow_Far, Follow_Far_Back,
+            Follow_Large, Follow_Large_Back,
+            Follow_Near, Follow_Near_Back,
+            Hood, Hood_Back,
+            Unknown
+        }
+
+        @JsonProperty("type")
+        private Type viewType;
+
+        @JsonProperty("cameraId")
+        private int cameraId;
+
+        @JsonProperty("viewId")
+        private int viewId;
+
+        @JsonProperty("customized")
+        private boolean customized;
+
+        public Type getViewType() {
+            return viewType;
+        }
+
+        public int getCameraId() {
+            return cameraId;
+        }
+
+        public int getViewId() {
+            return viewId;
+        }
+
+        public boolean isCustomized() {
+            return customized;
+        }
+    }
+}
