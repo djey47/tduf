500312a04d70ca9b24de7577149cad1a0fb5f54e
djey <j.canler@gmail.com>
2016-04-28 16:33:52 +0200

    feature(lib/interop/cameras) : add support for CAM-C op

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/bin/cameras/interop/GenuineCamGateway.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/bin/cameras/interop/GenuineCamGateway.java
@@ -1,12 +1,15 @@
 package fr.tduf.libunlimited.high.files.bin.cameras.interop;
 
+import com.esotericsoftware.minlog.Log;
 import fr.tduf.libunlimited.common.domain.ProcessResult;
 import fr.tduf.libunlimited.common.helper.CommandLineHelper;
 import fr.tduf.libunlimited.high.files.bin.cameras.interop.dto.GenuineCamViewsDto;
 import fr.tduf.libunlimited.low.files.bin.cameras.domain.CameraInfo;
 import org.codehaus.jackson.map.ObjectMapper;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import static fr.tduf.libunlimited.common.helper.CommandLineHelper.EXIT_CODE_SUCCESS;
@@ -16,10 +19,12 @@ import static fr.tduf.libunlimited.common.helper.CommandLineHelper.EXIT_CODE_SUC
  */
 // TODO create common gateway component
 public class GenuineCamGateway {
+    private static final String THIS_CLASS_NAME = GenuineCamGateway.class.getSimpleName();
 
     static final String EXE_TDUMT_CLI = Paths.get(".", "tools", "tdumt-cli", "tdumt-cli.exe").toString();
     // TODO replace with enum
     static final String CLI_COMMAND_CAM_LIST = "CAM-L";
+    static final String CLI_COMMAND_CAM_CUSTOMIZE = "CAM-C";
 
     private CommandLineHelper commandLineHelper;
 
@@ -31,13 +36,21 @@ public class GenuineCamGateway {
      * tdumt-cli syntax: CAM-L <camFileName> <camId>
      */
     public CameraInfo getCameraInfo(String camFileName, int camId) throws IOException {
-
         ProcessResult processResult = commandLineHelper.runCliCommand(EXE_TDUMT_CLI, CLI_COMMAND_CAM_LIST, camFileName, Integer.valueOf(camId).toString());
         handleCommandLineErrors(processResult);
 
         GenuineCamViewsDto outputObject = new ObjectMapper().readValue(processResult.getOut(), GenuineCamViewsDto.class);
 
         return mapGenuineCamViewsToCameraInfo(outputObject);
+    }
+
+    /**
+     * tdumt-cli syntax: CAM-C <camFileName> <camId> <customizeInputFileName>
+     */
+    public void customizeCamera(String camFileName, int camId, GenuineCamViewsDto customizeInput) throws IOException {
+        String customizeInputFileName = createCamCustomizeInputFile(customizeInput);
+        ProcessResult processResult = commandLineHelper.runCliCommand(EXE_TDUMT_CLI, CLI_COMMAND_CAM_CUSTOMIZE, camFileName, Integer.valueOf(camId).toString(), customizeInputFileName);
+        handleCommandLineErrors(processResult);
     }
 
     private static void handleCommandLineErrors(ProcessResult processResult) throws IOException {
@@ -50,5 +63,14 @@ public class GenuineCamGateway {
     private static CameraInfo mapGenuineCamViewsToCameraInfo(GenuineCamViewsDto outputObject) {
         // TODO mapping
         return new CameraInfo();
+    }
+
+    private static String createCamCustomizeInputFile(GenuineCamViewsDto customizeInput) throws IOException {
+        File inputFile = Files.createTempDirectory("libUnlimited-cameras").resolve("CustomizeInput.json").toFile();
+        new ObjectMapper().writerWithDefaultPrettyPrinter().writeValue(inputFile, customizeInput);
+
+        String inputFileName = inputFile.getAbsolutePath();
+        Log.debug(THIS_CLASS_NAME, "inputFileName: " + inputFileName);
+        return inputFileName;
     }
 }
--- lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/bin/cameras/interop/GenuineCamGatewayTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/high/files/bin/cameras/interop/GenuineCamGatewayTest.java
@@ -1,20 +1,30 @@
 package fr.tduf.libunlimited.high.files.bin.cameras.interop;
 
+import com.esotericsoftware.minlog.Log;
 import fr.tduf.libunlimited.common.domain.ProcessResult;
 import fr.tduf.libunlimited.common.helper.CommandLineHelper;
 import fr.tduf.libunlimited.common.helper.FilesHelper;
+import fr.tduf.libunlimited.high.files.bin.cameras.interop.dto.GenuineCamViewsDto;
 import fr.tduf.libunlimited.low.files.bin.cameras.domain.CameraInfo;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import static fr.tduf.libunlimited.high.files.bin.cameras.interop.GenuineCamGateway.CLI_COMMAND_CAM_LIST;
+import static fr.tduf.libunlimited.high.files.bin.cameras.interop.GenuineCamGateway.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,6 +35,14 @@ public class GenuineCamGatewayTest {
 
     @InjectMocks
     private GenuineCamGateway genuineCamGateway;
+
+    @Captor
+    private ArgumentCaptor<String> commandArgumentsCaptor;
+
+    @Before
+    public void setUp() throws IOException {
+        Log.set(Log.LEVEL_DEBUG);
+    }
 
     @Test
     public void getCameraInfo_whenSuccess_shouldInvokeCommandLineCorrectly_andReturnObject() throws IOException, URISyntaxException {
@@ -41,10 +59,34 @@ public class GenuineCamGatewayTest {
         // TODO enhance test
     }
 
+    @Test
+    public void customizeCamera_whenSuccess_shouldInvokeCommandLineCorrectly() throws IOException, URISyntaxException {
+        // GIVEN
+        String camFileName = "cameras.bin";
+        int camId = 326;
+        GenuineCamViewsDto customizeInput = new GenuineCamViewsDto();
+        mockCommandLineHelperToReturnCameraCustomizeSuccess(camFileName, camId);
+
+        // WHEN
+        genuineCamGateway.customizeCamera(camFileName, camId, customizeInput);
+
+        // THEN
+        verify(commandLineHelperMock).runCliCommand(eq(EXE_TDUMT_CLI), eq(CLI_COMMAND_CAM_CUSTOMIZE), eq(camFileName), eq(Integer.valueOf(camId).toString()), commandArgumentsCaptor.capture());
+
+        String expectedInputContents = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(customizeInput);
+        assertThat(new File(commandArgumentsCaptor.getValue()))
+                .exists()
+                .hasContent(expectedInputContents);
+    }
+
     private void mockCommandLineHelperToReturnCameraViewsSuccess(String bankFileName, int camId) throws IOException, URISyntaxException {
         String jsonOutput = FilesHelper.readTextFromResourceFile("/files/interop/tdumt-cli/CAM-L.output.json");
         ProcessResult processResult = new ProcessResult(CLI_COMMAND_CAM_LIST, 0, jsonOutput, "");
-        when(commandLineHelperMock.runCliCommand(GenuineCamGateway.EXE_TDUMT_CLI, CLI_COMMAND_CAM_LIST, bankFileName, Integer.valueOf(camId).toString())).thenReturn(processResult);
+        when(commandLineHelperMock.runCliCommand(EXE_TDUMT_CLI, CLI_COMMAND_CAM_LIST, bankFileName, Integer.valueOf(camId).toString())).thenReturn(processResult);
     }
 
+    private void mockCommandLineHelperToReturnCameraCustomizeSuccess(String bankFileName, int camId) throws IOException, URISyntaxException {
+        ProcessResult processResult = new ProcessResult(CLI_COMMAND_CAM_CUSTOMIZE, 0, "{}", "");
+        when(commandLineHelperMock.runCliCommand(eq(EXE_TDUMT_CLI), eq(CLI_COMMAND_CAM_CUSTOMIZE), eq(bankFileName), eq(Integer.valueOf(camId).toString()), anyString())).thenReturn(processResult);
+    }
 }
