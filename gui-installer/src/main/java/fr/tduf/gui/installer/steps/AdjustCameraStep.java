7c96113f8b37b7ea24f78d9d1954874fc6deb6dc
djey <j.canler@gmail.com>
2016-05-04 18:32:19 +0200

    feature(gui/installer): implement custom cam feature - WIP

--- gui-installer/src/main/java/fr/tduf/gui/installer/steps/AdjustCameraStep.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/steps/AdjustCameraStep.java
@@ -1,13 +1,70 @@
 package fr.tduf.gui.installer.steps;
 
+import com.esotericsoftware.minlog.Log;
+import fr.tduf.libunlimited.high.files.bin.cameras.interop.dto.GenuineCamViewsDto;
+import fr.tduf.libunlimited.high.files.db.patcher.domain.PatchProperties;
+
 import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toCollection;
 
 /**
  * Customizes views for installed vehicle.
  */
 class AdjustCameraStep extends GenericStep {
+    private static final String THIS_CLASS_NAME = AdjustCameraStep.class.getSimpleName();
+
     @Override
     protected void perform() throws IOException, ReflectiveOperationException {
-        throw new UnsupportedOperationException();
+        requireNonNull(getInstallerConfiguration(), "Installer configuration is required.");
+        requireNonNull(getDatabaseContext(), "Database context is required.");
+
+        int cameraId = getDatabaseContext().getPatchProperties().getCameraIdentifier().orElseGet(this::getCameraIdentifierFromDatabase);
+        String cameraFileName = Paths.get(getInstallerConfiguration().resolveDatabaseDirectory(), "Cameras.bin").toString();
+        GenuineCamViewsDto customViewsObject = buildCustomViewsFromProperties();
+
+        Log.info(THIS_CLASS_NAME, "->Adjusting camera id " + cameraId + ": " + cameraFileName);
+        getInstallerConfiguration().getCameraSupport().customizeCamera(cameraFileName, cameraId, customViewsObject);
+    }
+
+    private GenuineCamViewsDto buildCustomViewsFromProperties() {
+        final GenuineCamViewsDto genuineCamViewsDto = new GenuineCamViewsDto();
+
+        Stream.of(PatchProperties.CustomizableCameraView.values())
+
+                .map(this::buildCustomViewFromProperties)
+
+                .filter(Optional::isPresent)
+
+                .map(Optional::get)
+
+                .collect(toCollection(genuineCamViewsDto::getViews));
+
+        return genuineCamViewsDto;
+    }
+
+    private Optional<GenuineCamViewsDto.GenuineCamViewDto> buildCustomViewFromProperties(PatchProperties.CustomizableCameraView cameraView) {
+
+        return getDatabaseContext().getPatchProperties().getCustomizedCameraView(cameraView)
+
+                .map(prop -> {
+                    final GenuineCamViewsDto.GenuineCamViewDto genuineCamViewDto = new GenuineCamViewsDto.GenuineCamViewDto();
+
+                    // TODO
+//                    genuineCamViewDto.setViewType(cameraView.);
+//                    genuineCamViewDto.setCameraId(cameraId);
+//                    genuineCamViewDto.setViewId();
+
+                    return genuineCamViewDto;
+                });
+    }
+
+    private int getCameraIdentifierFromDatabase() {
+        // TODO
+        return 0;
     }
 }
