03df145ccf7af58fef9adb999e6f72b8fce7c642
djey <j.canler@gmail.com>
2016-05-27 14:33:56 +0200

    refactor(cli/database): rename start script and remove obsolete one

--- src/main/shell/TDUF-database-gui.cmd
+++ src/main/shell/TDUF-database-gui.cmd
@@ -32,4 +32,25 @@ CD tools\cli
 
 CALL .\CheckJava.cmd
 CALL .\SetVersion.cmd
-CALL .\DatabaseEditor-QuickLauncher.cmd
+
+ECHO ...Starting Database Editor...
+CALL DatabaseEditor %1 >> ..\..\logs\DatabaseEditor.log 2>>&1
+if ERRORLEVEL 1 GOTO handleEditorError
+
+ECHO.
+ECHO All done!
+GOTO exitSuccess
+
+:handleEditorError
+ECHO Editor failed, can't continue. Changes won't be applied.
+GOTO exitFailure
+
+:exitSuccess
+PAUSE
+EXIT /B 0
+
+:exitFailure
+ECHO Please check DatabaseEditor.log in logs directory for details.
+ECHO.
+PAUSE
+EXIT /B 1
