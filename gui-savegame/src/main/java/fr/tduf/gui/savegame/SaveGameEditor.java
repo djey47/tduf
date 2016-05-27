6a78eaa67b93bab35830d1fb5ff2ba85700bda32
djey <j.canler@gmail.com>
2016-05-27 14:02:01 +0200

    conf(gui): add save game editor GUI module

---
+++ gui-savegame/src/main/java/fr/tduf/gui/savegame/SaveGameEditor.java
@@ -0,0 +1,24 @@
+package fr.tduf.gui.savegame;
+
+import fr.tduf.gui.common.javafx.application.AbstractGuiApp;
+import javafx.stage.Stage;
+
+/**
+ * SaveGameEditor Java FX Application.
+ */
+public class SaveGameEditor extends AbstractGuiApp {
+
+    @Override
+    public void startApp(Stage primaryStage) throws Exception {
+//        MainStageDesigner.init(primaryStage);
+        primaryStage.show();
+    }
+
+    /**
+     * Single application entry point
+     * @param args  : arguments passed with command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
