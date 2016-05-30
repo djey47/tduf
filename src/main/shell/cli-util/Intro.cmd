76ea59419715ec217d9a5b6d88b38627b8f78eb0
djey <j.canler@gmail.com>
2016-05-30 13:05:36 +0200

    refactor(scripts): simplify layout to only display useful scripts

--- src/main/shell/cli-util/Intro.cmd
+++ src/main/shell/cli-util/Intro.cmd
@@ -1,12 +1,17 @@
 @ECHO OFF
 
+CLS
+
 CALL .\CheckJava.cmd
 CALL .\SetVersion.cmd
 
 ECHO *** Welcome to TDUF: Command Line Interface v%TDUF_VERSION% ***
 ECHO.
-ECHO Available modules :
-DIR *.cmd /B
+DIR Intro.cmd /B
+ECHO --- Visual tools ---
+DIR *Editor.cmd /B
+ECHO --- Advanced tools ---
+DIR *Tool.cmd /B
 ECHO.
 ECHO Just type and run any of these scripts to get usage details. Enjoy!
 ECHO.
