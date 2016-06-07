1ccb9b0bb394595e487aefccc9196eb55437be83
djey <j.canler@gmail.com>
2016-06-07 13:44:51 +0200

    feature(gui/installer) : add constant value

--- gui-installer/src/main/java/fr/tduf/gui/installer/common/DatabaseConstants.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/common/DatabaseConstants.java
@@ -4,15 +4,15 @@ package fr.tduf.gui.installer.common;
  * Gives all constants to access particular information in TDU database.
  */
 public class DatabaseConstants {
-    public static final int FIELD_RANK_CAR_REF = 1;            // CAR PHYSICS, CAR RIMS, CAR COLORS
+    public static final int FIELD_RANK_CAR_REF = 1;                                 // CAR PHYSICS, CAR RIMS, CAR COLORS
 
-    public static final int FIELD_RANK_MANUFACTURER_NAME = 3;  // BRANDS
+    public static final int FIELD_RANK_MANUFACTURER_NAME = 3;                       // BRANDS
 
-    public static final int FIELD_RANK_COLOR_NAME = 3;          // CAR COLORS
+    public static final int FIELD_RANK_COLOR_NAME = 3;                              // CAR COLORS
     public static final int FIELD_RANK_INTERIOR_1 = 8;
     public static final int FIELD_RANK_INTERIOR_15 = 22;
 
-    public static final int FIELD_RANK_CAR_BRAND = 2;          // CAR PHYSICS
+    public static final int FIELD_RANK_CAR_BRAND = 2;                               // CAR PHYSICS
     public static final int FIELD_RANK_GROUP = 5;
     public static final int FIELD_RANK_CAR_FILE_NAME = 9;
     public static final int FIELD_RANK_DEFAULT_RIMS = 10;
@@ -24,15 +24,15 @@ public class DatabaseConstants {
     public static final int FIELD_RANK_SECU2 = 101;
     public static final int FIELD_RANK_ID_CAR = 102;
 
-    public static final int FIELD_RANK_DEALER_REF = 1;            // CAR SHOPS
+    public static final int FIELD_RANK_DEALER_REF = 1;                              // CAR SHOPS
     public static final int FIELD_RANK_DEALER_NAME = 2;
     public static final int FIELD_RANK_DEALER_LIBELLE = 3;
     public static final int FIELD_RANK_DEALER_SLOT_1 = 4;
     public static final int FIELD_RANK_DEALER_SLOT_15 = 18;
 
-    public static final int FIELD_RANK_INTERIOR_NAME = 3;       // INTERIOR
+    public static final int FIELD_RANK_INTERIOR_NAME = 3;                           // INTERIOR
 
-    public static final int FIELD_RANK_RIM_REF = 1;               // RIMS
+    public static final int FIELD_RANK_RIM_REF = 1;                                 // RIMS
     public static final int FIELD_RANK_RSC_PATH = 13;
     public static final int FIELD_RANK_RSC_FILE_NAME_FRONT = 14;
     public static final int FIELD_RANK_RSC_FILE_NAME_REAR = 15;
@@ -53,7 +53,7 @@ public class DatabaseConstants {
     public static final String RESOURCE_REF_GROUP_MB = "77800264";
     public static final String RESOURCE_REF_GROUP_Z = "92900264";
 
-    public static final String REF_FREE_DEALER_SLOT = "61085282";               // CAR SHOPS
+    public static final String REF_FREE_DEALER_SLOT = "61085282";                   // CAR SHOPS
     public static final String RESOURCE_VALUE_PREFIX_FILE_NAME_CAR_DEALER = "ECD_";
     public static final String RESOURCE_VALUE_PREFIX_FILE_NAME_BIKE_DEALER = "EBD_";
     public static final String RESOURCE_VALUE_PREFIX_FILE_NAME_CAR_RENTAL = "ECR_";
@@ -62,6 +62,7 @@ public class DatabaseConstants {
     public static final String RESOURCE_REF_NONE_INTERIOR_NAME = "53365512";
 
     public static final String RESOURCE_REF_DEFAULT_RIM_BRAND = "55765512";         // RIMS
+    public static final String RESOURCE_REF_NO_RIM_NAME = "54276512";
 
     // GENERIC
     public static final String RESOURCE_VALUE_DEFAULT = "Default";
