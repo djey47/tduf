ae39de425c73be98cd34cb053ce04dce24e48c9b
djey <j.canler@gmail.com>
2016-06-11 16:04:40 +0200

    refactor(lib/database) : convert files to shorter attribute names

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/db/mapper/DbDataMapperTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/db/mapper/DbDataMapperTest.java
@@ -1,6 +1,7 @@
 package fr.tduf.libunlimited.low.files.db.mapper;
 
 import com.esotericsoftware.minlog.Log;
+import fr.tduf.libunlimited.common.helper.FilesHelper;
 import fr.tduf.libunlimited.low.files.db.dto.DbDataDto;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
@@ -10,6 +11,7 @@ import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 
@@ -26,7 +28,7 @@ public class DbDataMapperTest {
     }
 
     @Test
-    public void serialize_shouldWriteProperJson() throws IOException, JSONException {
+    public void serialize_shouldWriteProperJson() throws IOException, JSONException, URISyntaxException {
         //GIVEN
         DbDataDto.Item item11 = DbDataDto.Item.builder()
                 .ofFieldRank(1)
@@ -74,45 +76,13 @@ public class DbDataMapperTest {
                 .addEntry(entry1)
                 .addEntry(entry2)
                 .build();
-        String expectedJson = "{\n" +
-                "  \"entries\" : [ {\n" +
-                "    \"id\" : 1,\n" +
-                "    \"items\" : [ {\n" +
-                "      \"name\" : \"ID\",\n" +
-                "      \"rawValue\" : \"111111\",\n" +
-                "      \"fieldRank\" : 1\n" +
-                "    }, {\n" +
-                "      \"name\" : \"Name\",\n" +
-                "      \"rawValue\" : \"111\",\n" +
-                "      \"fieldRank\" : 2\n" +
-                "    }, {\n" +
-                "      \"name\" : \"Switchs\",\n" +
-                "      \"rawValue\" : \"1\",\n" +
-                "      \"fieldRank\" : 3\n" +
-                "    } ]\n" +
-                "  }, {\n" +
-                "    \"id\" : 2,\n" +
-                "    \"items\" : [ {\n" +
-                "      \"name\" : \"ID\",\n" +
-                "      \"rawValue\" : \"222222\",\n" +
-                "      \"fieldRank\" : 1\n" +
-                "    }, {\n" +
-                "      \"name\" : \"Name\",\n" +
-                "      \"rawValue\" : \"222\",\n" +
-                "      \"fieldRank\" : 2\n" +
-                "    }, {\n" +
-                "      \"name\" : \"Switchs\",\n" +
-                "      \"rawValue\" : \"0\",\n" +
-                "      \"fieldRank\" : 3\n" +
-                "    } ]\n" +
-                "  } ]\n" +
-                "}";
 
         //WHEN
         String jsonResult = objectWriter.writeValueAsString(dbDataDto);
         Log.debug(thisClass.getSimpleName(), "Actual JSON:" + jsonResult);
 
         //THEN
+        String expectedJson = FilesHelper.readTextFromResourceFile("/db/json/mapper/topicObject.data.json");
         assertEquals(expectedJson, jsonResult, JSONCompareMode.STRICT);
     }
 }
