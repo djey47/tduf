f289d491f54bbd6a085ea9138de38af10530dabd
djey <j.canler@gmail.com>
2016-06-14 15:14:53 +0200

    refactor(gui/installer) : use patch enhancer

--- gui-installer/src/main/java/fr/tduf/gui/installer/steps/UpdateDatabaseStep.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/steps/UpdateDatabaseStep.java
@@ -1,44 +1,26 @@
 package fr.tduf.gui.installer.steps;
 
 import com.esotericsoftware.minlog.Log;
-import fr.tduf.gui.installer.common.DatabaseConstants;
 import fr.tduf.gui.installer.common.FileConstants;
 import fr.tduf.gui.installer.common.InstallerConstants;
-import fr.tduf.gui.installer.domain.PaintJob;
-import fr.tduf.gui.installer.domain.RimSlot;
-import fr.tduf.gui.installer.domain.SecurityOptions;
-import fr.tduf.gui.installer.domain.VehicleSlot;
+import fr.tduf.gui.installer.steps.helper.PatchEnhancer;
 import fr.tduf.libunlimited.common.helper.FilesHelper;
 import fr.tduf.libunlimited.high.files.db.common.AbstractDatabaseHolder;
-import fr.tduf.libunlimited.high.files.db.dto.DbFieldValueDto;
 import fr.tduf.libunlimited.high.files.db.interop.tdupe.TdupeGateway;
 import fr.tduf.libunlimited.high.files.db.patcher.DatabasePatcher;
 import fr.tduf.libunlimited.high.files.db.patcher.domain.PatchProperties;
 import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
-import fr.tduf.libunlimited.high.files.db.patcher.helper.PlaceholderConstants;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.math.RoundingMode;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto.DbChangeDto.ChangeTypeEnum.UPDATE;
-import static fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto.DbChangeDto.ChangeTypeEnum.UPDATE_RES;
-import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.*;
-import static java.util.Arrays.asList;
-import static java.util.Collections.singletonList;
 import static java.util.Objects.requireNonNull;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.IntStream.rangeClosed;
 
 /**
  * Applies available patch onto loaded database then convert it to TDU format back
@@ -51,235 +33,11 @@ class UpdateDatabaseStep extends GenericStep {
         requireNonNull(getInstallerConfiguration(), "Installer configuration is required.");
         requireNonNull(getDatabaseContext(), "Database context is required.");
 
-        enhancePatchObject();
+        new PatchEnhancer(getDatabaseContext()).enhancePatchObject();
 
         applyMiniPatch();
 
         applyPerformancePackage(getDatabaseContext().getPatchProperties().getVehicleSlotReference().get());
-    }
-
-    void enhancePatchObjectWithPaintJobs(VehicleSlot vehicleSlot) {
-        Log.info(THIS_CLASS_NAME, "->Adding paint jobs changes to initial patch");
-
-        enhancePatchObjectWithExteriors(vehicleSlot);
-
-        if (!vehicleSlot.getPaintJobs().isEmpty()) {
-            enhancePatchObjectWithInteriors(vehicleSlot.getPaintJobs().get(0).getInteriorPatternRefs());
-        }
-    }
-
-    // TODO see if bikes support more than 1 rim set
-    void enhancePatchObjectWithRims(VehicleSlot vehicleSlot) {
-        Log.info(THIS_CLASS_NAME, "->Adding rim changes to initial patch");
-
-        AtomicInteger rimIndex = new AtomicInteger(1);
-        List<DbPatchDto.DbChangeDto> changeObjectsForRims = vehicleSlot.getRims().stream()
-                .flatMap(rimSlot -> createChangeObjectsForRims(vehicleSlot, rimSlot, rimIndex))
-                .collect(toList());
-
-        getDatabaseContext().getPatchObject().getChanges().addAll(changeObjectsForRims);
-    }
-
-    private void enhancePatchObject() {
-        PatchProperties patchProperties = getDatabaseContext().getPatchProperties() ;
-
-        if(patchProperties.getDealerReference().isPresent()
-                && patchProperties.getDealerSlot().isPresent()) {
-            enhancePatchObjectWithLocationChange();
-        }
-
-        enhancePatchObjectWithInstallFlag();
-
-        getDatabaseContext().getUserSelection().getVehicleSlot()
-                .ifPresent(vehicleSlot -> {
-                    enhancePatchObjectWithPaintJobs(vehicleSlot);
-                    enhancePatchObjectWithRims(vehicleSlot);
-                });
-    }
-
-    private void enhancePatchObjectWithLocationChange() {
-        Log.info(THIS_CLASS_NAME, "->Adding dealer slot change to initial patch");
-
-        PatchProperties patchProperties = getDatabaseContext().getPatchProperties();
-        int effectiveFieldRank = patchProperties.getDealerSlot().get() + 3;
-
-        DbPatchDto.DbChangeDto changeObject = DbPatchDto.DbChangeDto.builder()
-                .forTopic(CAR_SHOPS)
-                .withType(UPDATE)
-                .asReference(patchProperties.getDealerReference().get())
-                .withPartialEntryValues(singletonList(DbFieldValueDto.fromCouple(effectiveFieldRank, patchProperties.getVehicleSlotReference().get())))
-                .build();
-
-        getDatabaseContext().getPatchObject().getChanges().add(changeObject);
-    }
-
-    private void enhancePatchObjectWithInstallFlag() {
-        Log.info(THIS_CLASS_NAME, "->Adding install flag change to initial patch");
-
-        final String secuOneRawValue = SecurityOptions.INSTALLED.setScale(0, RoundingMode.UNNECESSARY).toString();
-        DbPatchDto.DbChangeDto changeObject = DbPatchDto.DbChangeDto.builder()
-                .forTopic(CAR_PHYSICS_DATA)
-                .withType(UPDATE)
-                .asReference(getDatabaseContext().getPatchProperties().getVehicleSlotReference().get())
-                .withPartialEntryValues(singletonList(DbFieldValueDto.fromCouple(DatabaseConstants.FIELD_RANK_SECU1, secuOneRawValue)))
-                .build();
-
-        getDatabaseContext().getPatchObject().getChanges().add(changeObject);
-    }
-
-    private void enhancePatchObjectWithExteriors(VehicleSlot vehicleSlot) {
-        AtomicInteger exteriorIndex = new AtomicInteger(1);
-        List<DbPatchDto.DbChangeDto> changeObjectsForPaintJobs = vehicleSlot.getPaintJobs().stream()
-
-                .flatMap(paintJob -> createChangeObjectsForExterior(vehicleSlot, paintJob, exteriorIndex))
-
-                .collect(toList());
-
-        getDatabaseContext().getPatchObject().getChanges().addAll(changeObjectsForPaintJobs);
-    }
-
-    private void enhancePatchObjectWithInteriors(List<String> interiorPatternRefs) {
-        AtomicInteger interiorIndex = new AtomicInteger(1);
-        List<DbPatchDto.DbChangeDto> changeObjectsForInteriors = interiorPatternRefs.stream()
-
-                .filter(intRef -> !DatabaseConstants.REF_NO_INTERIOR.equals(intRef))
-
-                .map(intRef -> createChangeObjectForInterior(intRef, interiorIndex))
-
-                .collect(toList());
-
-        getDatabaseContext().getPatchObject().getChanges().addAll(changeObjectsForInteriors);
-    }
-
-    private Stream<DbPatchDto.DbChangeDto> createChangeObjectsForExterior(VehicleSlot vehicleSlot, PaintJob paintJob, AtomicInteger exteriorIndex) {
-        if (!getDatabaseContext().getPatchProperties().getExteriorMainColorId(paintJob.getRank()).isPresent()) {
-            return Stream.empty();
-        }
-
-        List<String> interiorRefs = new ArrayList<>(paintJob.getInteriorPatternRefs());
-        try (IntStream stream = rangeClosed(interiorRefs.size(), 15)) {
-            stream.forEach(index -> interiorRefs.add(DatabaseConstants.REF_NO_INTERIOR));
-        }
-
-        int index = exteriorIndex.getAndIncrement();
-        DbPatchDto.DbChangeDto entryUpdateChange = DbPatchDto.DbChangeDto.builder()
-                .withType(UPDATE)
-                .forTopic(CAR_COLORS)
-                .withEntryValues(asList(
-                        vehicleSlot.getRef(),
-                        PlaceholderConstants.getPlaceHolderForExteriorMainColor(index),
-                        PlaceholderConstants.getPlaceHolderForExteriorNameResource(index),
-                        PlaceholderConstants.getPlaceHolderForExteriorSecondaryColor(index),
-                        PlaceholderConstants.getPlaceHolderForExteriorCalipersColor(index),
-                        "0",
-                        "0",
-                        interiorRefs.get(0),
-                        interiorRefs.get(1),
-                        interiorRefs.get(2),
-                        interiorRefs.get(3),
-                        interiorRefs.get(4),
-                        interiorRefs.get(5),
-                        interiorRefs.get(6),
-                        interiorRefs.get(7),
-                        interiorRefs.get(8),
-                        interiorRefs.get(9),
-                        interiorRefs.get(10),
-                        interiorRefs.get(11),
-                        interiorRefs.get(12),
-                        interiorRefs.get(13),
-                        interiorRefs.get(14)
-                ))
-                .build();
-
-        DbPatchDto.DbChangeDto resourceUpdateChange = DbPatchDto.DbChangeDto.builder()
-                .withType(UPDATE_RES)
-                .forTopic(CAR_COLORS)
-                .asReference(paintJob.getName().getRef())
-                .withValue(paintJob.getName().getValue())
-                .build();
-
-        return Stream.of(entryUpdateChange, resourceUpdateChange);
-    }
-
-    private DbPatchDto.DbChangeDto createChangeObjectForInterior(String intRef, AtomicInteger interiorIndex) {
-        int index = interiorIndex.getAndIncrement();
-        return DbPatchDto.DbChangeDto.builder()
-                .withType(UPDATE)
-                .forTopic(INTERIOR)
-                .asReference(intRef)
-                .withEntryValues(asList(
-                        intRef,
-                        DatabaseConstants.REF_DEFAULT_BRAND,
-                        DatabaseConstants.RESOURCE_REF_NONE_INTERIOR_NAME,
-                        PlaceholderConstants.getPlaceHolderForInteriorMainColor(index),
-                        PlaceholderConstants.getPlaceHolderForInteriorSecondaryColor(index),
-                        PlaceholderConstants.getPlaceHolderForInteriorMaterial(index),
-                        "0"
-                ))
-                .build();
-    }
-
-    private Stream<DbPatchDto.DbChangeDto> createChangeObjectsForRims(VehicleSlot vehicleSlot, RimSlot rimSlot, AtomicInteger rimIndex) {
-        int index = rimIndex.getAndIncrement();
-        if (!getDatabaseContext().getPatchProperties().getRimSlotReference(index).isPresent()) {
-            return Stream.empty();
-        }
-
-        DbPatchDto.DbChangeDto associationEntryUpdate = DbPatchDto.DbChangeDto.builder()
-                .withType(UPDATE)
-                .forTopic(CAR_RIMS)
-                .withEntryValues( asList(
-                        vehicleSlot.getRef(),
-                        getDatabaseContext().getPatchProperties().getRimSlotReference(index).get()
-                ))
-                .build();
-
-        DbPatchDto.DbChangeDto slotEntryUpdate = DbPatchDto.DbChangeDto.builder()
-                .withType(UPDATE)
-                .forTopic(RIMS)
-                .asReference(rimSlot.getRef())
-                .withEntryValues( asList(
-                        rimSlot.getRef(),
-                        PlaceholderConstants.getPlaceHolderForRimBrand(index),
-                        DatabaseConstants.RESOURCE_REF_NO_RIM_NAME,
-                        PlaceholderConstants.getPlaceHolderForRimNameResource(index),
-                        PlaceholderConstants.getPlaceHolderForFrontRimWidth(index),
-                        PlaceholderConstants.getPlaceHolderForFrontRimHeight(index),
-                        PlaceholderConstants.getPlaceHolderForFrontRimDiameter(index),
-                        PlaceholderConstants.getPlaceHolderForRearRimWidth(index),
-                        PlaceholderConstants.getPlaceHolderForRearRimHeight(index),
-                        PlaceholderConstants.getPlaceHolderForRearRimDiameter(index),
-                        "0",
-                        "0",
-                        PlaceholderConstants.getPlaceHolderForRimBrand(index),
-                        PlaceholderConstants.getPlaceHolderForFrontRimFileNameResource(index),
-                        PlaceholderConstants.getPlaceHolderForRearRimFileNameResource(index),
-                        "0"
-                ))
-                .build();
-
-        DbPatchDto.DbChangeDto slotNameResourceUpdate = DbPatchDto.DbChangeDto.builder()
-                .withType(UPDATE_RES)
-                .forTopic(RIMS)
-                .asReference(PlaceholderConstants.getPlaceHolderForRimNameResource(index))
-                .withValue(PlaceholderConstants.getPlaceHolderForRimName(index))
-                .build();
-
-        DbPatchDto.DbChangeDto slotFrontFileNameResourceUpdate = DbPatchDto.DbChangeDto.builder()
-                .withType(UPDATE_RES)
-                .forTopic(RIMS)
-                .asReference(PlaceholderConstants.getPlaceHolderForFrontRimFileNameResource(index))
-                .withValue(PlaceholderConstants.getPlaceHolderForFrontRimFileName(index))
-                .build();
-
-        DbPatchDto.DbChangeDto slotRearNameResourceUpdate = DbPatchDto.DbChangeDto.builder()
-                .withType(UPDATE_RES)
-                .forTopic(RIMS)
-                .asReference(PlaceholderConstants.getPlaceHolderForRearRimFileNameResource(index))
-                .withValue(PlaceholderConstants.getPlaceHolderForRearRimFileName(index))
-                .build();
-
-        return Stream.of(associationEntryUpdate, slotEntryUpdate, slotNameResourceUpdate, slotFrontFileNameResourceUpdate, slotRearNameResourceUpdate);
     }
 
     private void applyMiniPatch() throws ReflectiveOperationException, IOException {
--- gui-installer/src/test/java/fr/tduf/gui/installer/steps/helper/PatchEnhancerTest.java
+++ gui-installer/src/test/java/fr/tduf/gui/installer/steps/helper/PatchEnhancerTest.java
@@ -1,5 +1,200 @@
 package fr.tduf.gui.installer.steps.helper;
 
+import com.esotericsoftware.minlog.Log;
+import fr.tduf.gui.installer.domain.*;
+import fr.tduf.gui.installer.domain.exceptions.StepException;
+import fr.tduf.libunlimited.high.files.db.patcher.domain.PatchProperties;
+import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import static fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto.DbChangeDto.ChangeTypeEnum.UPDATE;
+import static fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto.DbChangeDto.ChangeTypeEnum.UPDATE_RES;
+import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.*;
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PatchEnhancerTest {
 
+    private static final String SLOT_REFERENCE = "30000000";
+
+    private DatabaseContext databaseContext;
+    private PatchProperties patchProperties;
+
+    @Before
+    public void setUp() throws IOException, URISyntaxException {
+        Log.set(Log.LEVEL_DEBUG);
+
+        patchProperties = new PatchProperties();
+        patchProperties.setVehicleSlotReferenceIfNotExists(SLOT_REFERENCE);
+
+        databaseContext = new DatabaseContext(new ArrayList<>(0), "");
+        databaseContext.setPatch(DbPatchDto.builder().build(), patchProperties);
+    }
+
+    @Test
+    public void enhancePatchObjectWithPaintJobs_withPaintJobProperties_shouldAddCarColors_andInterior_updateInstructions() throws URISyntaxException, IOException, ReflectiveOperationException, StepException {
+        // GIVEN
+        String mainColorId = "1111";
+        String secColorId = "2222";
+        String calColorId = "3333";
+        String nameId = "4444";
+        String name = "PJ name";
+        String intId = "5555";
+        String intManufacturerId = "62938337";
+        String intNameId = "53365512";
+
+        patchProperties.setExteriorMainColorIdIfNotExists(mainColorId, 1);
+
+        final VehicleSlot vehicleSlot = VehicleSlot.builder()
+                .withRef(SLOT_REFERENCE)
+                .addPaintJob(PaintJob.builder()
+                        .withName(Resource.from(nameId, name))
+                        .withColors(Resource.from(mainColorId, ""), Resource.from(secColorId, ""), Resource.from(calColorId, ""))
+                        .addInteriorPattern(intId)
+                        .build())
+                .build();
+
+
+        // WHEN
+        new PatchEnhancer(databaseContext).enhancePatchObjectWithPaintJobs(vehicleSlot);
+
+
+        // THEN
+        DbPatchDto patchObject = databaseContext.getPatchObject();
+        assertThat(patchObject.getChanges()).hasSize(3);
+        assertThat(patchObject.getChanges()).extracting("type").containsOnly(UPDATE, UPDATE_RES);
+        assertThat(patchObject.getChanges()).extracting("topic").containsOnly(CAR_COLORS, INTERIOR);
+        assertThat(patchObject.getChanges()).extracting("ref").containsOnly(null, nameId, intId);
+        assertThat(patchObject.getChanges()).extracting("values").containsOnly(
+                asList(
+                        SLOT_REFERENCE,
+                        "{COLORID.M.1}",
+                        "{RES_COLORNAME.1}",
+                        "{COLORID.S.1}",
+                        "{CALLIPERSID.1}",
+                        "0",
+                        "0",
+                        intId,
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636",
+                        "11319636"
+                ),
+                null,
+                asList(
+                        intId,
+                        intManufacturerId,
+                        intNameId,
+                        "{INTCOLORID.M.1}",
+                        "{INTCOLORID.S.1}",
+                        "{INTMATERIALID.1}",
+                        "0"
+                ));
+        assertThat(patchObject.getChanges()).extracting("value").containsOnly(null, name);
+    }
+
+    @Test
+    public void enhancePatchObjectWithRims_withRimProperties_shouldAddCarRims_andRims_updateInstructions() throws URISyntaxException, IOException, ReflectiveOperationException, StepException {
+        // GIVEN
+        String rimId1 = "1111";
+        String rimId2 = "2222";
+
+        patchProperties.setRimsSlotReferenceIfNotExists(rimId1, 1);
+        patchProperties.setRimsSlotReferenceIfNotExists(rimId2, 2);
+
+        final RimSlot rimSlot1 = RimSlot.builder()
+                .withRef(rimId1)
+                .build();
+        final RimSlot rimSlot2 = RimSlot.builder()
+                .withRef(rimId2)
+                .build();
+        final VehicleSlot vehicleSlot = VehicleSlot.builder()
+                .withRef(SLOT_REFERENCE)
+                .withDefaultRims(rimSlot1)
+                .addRim(rimSlot2)
+                .build();
+
+
+        // WHEN
+        new PatchEnhancer(databaseContext).enhancePatchObjectWithRims(vehicleSlot);
+
+
+        // THEN
+        DbPatchDto patchObject = databaseContext.getPatchObject();
+        assertThat(patchObject.getChanges()).hasSize(10); // 5 per rim set
+        assertThat(patchObject.getChanges()).extracting("type").containsOnly(UPDATE, UPDATE_RES);
+        assertThat(patchObject.getChanges()).extracting("topic").containsOnly(CAR_RIMS, RIMS);
+        assertThat(patchObject.getChanges()).extracting("ref").containsOnly(
+                null,
+                rimId1,
+                "{RES_RIMNAME.1}",
+                "{RES_BANKNAME.FR.1}",
+                "{RES_BANKNAME.RR.1}",
+                rimId2,
+                "{RES_RIMNAME.2}",
+                "{RES_BANKNAME.FR.2}",
+                "{RES_BANKNAME.RR.2}"
+        );
+        assertThat(patchObject.getChanges()).extracting("values").containsOnly(
+                asList(SLOT_REFERENCE, rimId1),
+                asList(
+                        rimId1,
+                        "{RIMBRANDREF.1}",
+                        "54276512",
+                        "{RES_RIMNAME.1}",
+                        "{RIMWIDTH.FR.1}",
+                        "{RIMHEIGHT.FR.1}",
+                        "{RIMDIAM.FR.1}",
+                        "{RIMWIDTH.RR.1}",
+                        "{RIMHEIGHT.RR.1}",
+                        "{RIMDIAM.RR.1}",
+                        "0",
+                        "0",
+                        "{RIMBRANDREF.1}",
+                        "{RES_BANKNAME.FR.1}",
+                        "{RES_BANKNAME.RR.1}",
+                        "0"),
+                null,
+                asList(SLOT_REFERENCE, rimId2),
+                asList(
+                        rimId2,
+                        "{RIMBRANDREF.2}",
+                        "54276512",
+                        "{RES_RIMNAME.2}",
+                        "{RIMWIDTH.FR.2}",
+                        "{RIMHEIGHT.FR.2}",
+                        "{RIMDIAM.FR.2}",
+                        "{RIMWIDTH.RR.2}",
+                        "{RIMHEIGHT.RR.2}",
+                        "{RIMDIAM.RR.2}",
+                        "0",
+                        "0",
+                        "{RIMBRANDREF.2}",
+                        "{RES_BANKNAME.FR.2}",
+                        "{RES_BANKNAME.RR.2}",
+                        "0"));
+        assertThat(patchObject.getChanges()).extracting("value").containsOnly(
+                null,
+                "{RIMNAME.1}",
+                "{BANKNAME.FR.1}",
+                "{BANKNAME.RR.1}",
+                "{RIMNAME.2}",
+                "{BANKNAME.FR.2}",
+                "{BANKNAME.RR.2}");
+    }
 }
