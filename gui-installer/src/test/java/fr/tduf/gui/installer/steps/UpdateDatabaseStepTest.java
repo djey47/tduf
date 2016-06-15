91c6f5dcec741f483e6d0e2fe87e64b39d15c7b2
djey <j.canler@gmail.com>
2016-06-15 12:03:09 +0200

    refactor(gui/installer) : remove obsolete tests

--- gui-installer/src/test/java/fr/tduf/gui/installer/steps/UpdateDatabaseStepTest.java
+++ gui-installer/src/test/java/fr/tduf/gui/installer/steps/UpdateDatabaseStepTest.java
@@ -2,11 +2,9 @@ package fr.tduf.gui.installer.steps;
 
 import com.esotericsoftware.minlog.Log;
 import fr.tduf.gui.installer.common.helper.InstallerTestsHelper;
-import fr.tduf.gui.installer.domain.DatabaseContext;
-import fr.tduf.gui.installer.domain.InstallerConfiguration;
+import fr.tduf.gui.installer.domain.*;
 import fr.tduf.gui.installer.domain.exceptions.StepException;
 import fr.tduf.libunlimited.high.files.banks.BankSupport;
-import fr.tduf.libunlimited.high.files.db.dto.DbFieldValueDto;
 import fr.tduf.libunlimited.high.files.db.patcher.domain.PatchProperties;
 import fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto;
 import org.junit.Before;
@@ -21,10 +19,6 @@ import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
 import static fr.tduf.gui.installer.steps.GenericStep.StepType.UPDATE_DATABASE;
-import static fr.tduf.libunlimited.high.files.db.patcher.dto.DbPatchDto.DbChangeDto.ChangeTypeEnum.UPDATE;
-import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.CAR_PHYSICS_DATA;
-import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.CAR_SHOPS;
-import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -32,6 +26,21 @@ public class UpdateDatabaseStepTest {
     private static final Class<UpdateDatabaseStepTest> thisClass = UpdateDatabaseStepTest.class;
 
     private static final String SLOT_REFERENCE = "30000000";
+    private static final String CARID = "3000";
+    private static final String BANKNAME = "TDUCP_3000";
+    private static final String RES_BANKNAME = "30000567";
+    private static final String BANKNAME_FR_1 = "TDUCP_3000_F_01";
+    private static final String BANKNAME_RR_1 = "TDUCP_3000_R_01";
+    private static final String RES_BANKNAME_FR_1 = "3000000010";
+    private static final String RES_BANKNAME_RR_1 = "3000000011";
+    private static final String RIMREF_1 = "3000000001";
+    private static final String RES_RIMBRAND_1 = "654857";
+    private static final String RIMBRAND_1 = "Default";
+    private static final String RES_COLORNAME_1 = "4607167";
+    private static final String COLORNAME_1 = "Nero";
+    private static final String RES_COLORNAME_2 = "4607267";
+    private static final String COLORNAME_2 = "Blau";
+    private static final String INTREF_1 = "5000000001";
 
     @Mock
     private BankSupport bankSupportMock;
@@ -40,18 +49,28 @@ public class UpdateDatabaseStepTest {
 
     private String assetsDirectory;
 
-    private PatchProperties patchProperties;
     private InstallerConfiguration installerConfiguration;
 
     @Before
     public void setUp() throws IOException, URISyntaxException {
         Log.set(Log.LEVEL_DEBUG);
 
-        patchProperties = new PatchProperties();
+        PatchProperties patchProperties = new PatchProperties();
         patchProperties.setVehicleSlotReferenceIfNotExists(SLOT_REFERENCE);
+        patchProperties.setInteriorMainColorIdIfNotExists("", 1);
+        patchProperties.setInteriorSecondaryColorIdIfNotExists("", 1);
+        patchProperties.setInteriorMaterialIdIfNotExists("", 1);
+        patchProperties.register("RIMWIDTH.FR.1", "0");
+        patchProperties.register("RIMWIDTH.RR.1", "0");
+        patchProperties.register("RIMHEIGHT.FR.1", "0");
+        patchProperties.register("RIMHEIGHT.RR.1", "0");
+        patchProperties.register("RIMDIAM.FR.1", "0");
+        patchProperties.register("RIMDIAM.RR.1", "0");
+        patchProperties.register("RIMNAME.1", "");
 
         databaseContext = InstallerTestsHelper.createJsonDatabase();
         databaseContext.setPatch(DbPatchDto.builder().build(), patchProperties);
+        databaseContext.getUserSelection().selectVehicleSlot(createVehicleSlot());
 
         String tempDirectory = InstallerTestsHelper.createTempDirectory();
         assetsDirectory = new File(thisClass.getResource("/assets-patch-only").toURI()).getAbsolutePath();
@@ -90,48 +109,6 @@ public class UpdateDatabaseStepTest {
     }
 
     @Test
-    public void perform_withDealerProperties_shouldAddCarShops_andCarPhysics_updateInstructions() throws URISyntaxException, IOException, ReflectiveOperationException, StepException {
-        // GIVEN
-        String dealerRef = "0000";
-        patchProperties.setDealerReferenceIfNotExists(dealerRef);
-        patchProperties.setDealerSlotIfNotExists(10);
-
-
-        // WHEN
-        final UpdateDatabaseStep updateDatabaseStep = (UpdateDatabaseStep) (
-                GenericStep.starterStep(installerConfiguration, databaseContext)
-                        .nextStep(UPDATE_DATABASE));
-        updateDatabaseStep.perform();
-
-
-        // THEN
-        DbPatchDto patchObject = databaseContext.getPatchObject();
-        assertThat(patchObject.getChanges()).extracting("type").containsOnly(UPDATE, UPDATE);
-        assertThat(patchObject.getChanges()).extracting("topic").containsOnly(CAR_SHOPS, CAR_PHYSICS_DATA);
-        assertThat(patchObject.getChanges()).extracting("ref").containsOnly(dealerRef, SLOT_REFERENCE);
-        assertThat(patchObject.getChanges()).extracting("partialValues").containsOnly(
-                singletonList(DbFieldValueDto.fromCouple(13, SLOT_REFERENCE)),
-                singletonList(DbFieldValueDto.fromCouple(100, "100")));
-    }
-
-    @Test
-    public void perform_withoutDealerProperties_shouldAddCarPhysicsUpdateInstruction() throws URISyntaxException, IOException, ReflectiveOperationException, StepException {
-        // GIVEN-WHEN
-        final UpdateDatabaseStep updateDatabaseStep = (UpdateDatabaseStep) (
-                GenericStep.starterStep(installerConfiguration, databaseContext)
-                        .nextStep(UPDATE_DATABASE));
-        updateDatabaseStep.perform();
-
-
-        // THEN
-        DbPatchDto patchObject = databaseContext.getPatchObject();
-        assertThat(patchObject.getChanges()).extracting("type").containsOnly(UPDATE);
-        assertThat(patchObject.getChanges()).extracting("topic").containsOnly(CAR_PHYSICS_DATA);
-        assertThat(patchObject.getChanges()).extracting("ref").containsOnly(SLOT_REFERENCE);
-        assertThat(patchObject.getChanges()).extracting("partialValues").containsOnly(singletonList(DbFieldValueDto.fromCouple(100, "100")));
-    }
-
-    @Test
     public void perform_withPerformancePack_shouldNotCrash() throws URISyntaxException, IOException, ReflectiveOperationException, StepException {
         // GIVEN
         assetsDirectory = new File(thisClass.getResource("/assets-patch-tdupk-only").toURI()).getAbsolutePath();
@@ -143,5 +120,33 @@ public class UpdateDatabaseStepTest {
         updateDatabaseStep.perform();
 
         // THEN
+    }
+
+    private static VehicleSlot createVehicleSlot() {
+        return VehicleSlot.builder()
+                .withRef(SLOT_REFERENCE)
+                .withCarIdentifier(Integer.valueOf(CARID))
+                .withFileName(Resource.from(RES_BANKNAME, BANKNAME))
+                .withDefaultRims(RimSlot.builder()
+                        .withRef(RIMREF_1)
+                        .withParentDirectoryName(Resource.from(RES_RIMBRAND_1, RIMBRAND_1))
+                        .withRimsInformation(RimSlot.RimInfo.builder()
+                                        .withFileName(Resource.from(RES_BANKNAME_FR_1, BANKNAME_FR_1))
+                                        .build(),
+                                RimSlot.RimInfo.builder()
+                                        .withFileName(Resource.from(RES_BANKNAME_RR_1, BANKNAME_RR_1))
+                                        .build())
+                        .build())
+                .addPaintJob(PaintJob.builder()
+                        .atRank(1)
+                        .withName(Resource.from(RES_COLORNAME_1, COLORNAME_1))
+                        .addInteriorPattern(INTREF_1)
+                        .build())
+                .addPaintJob(PaintJob.builder()
+                        .atRank(2)
+                        .withName(Resource.from(RES_COLORNAME_2, COLORNAME_2))
+                        .addInteriorPattern(INTREF_1)
+                        .build())
+                .build();
     }
 }
