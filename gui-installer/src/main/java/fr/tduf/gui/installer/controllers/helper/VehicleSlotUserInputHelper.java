96e82200482cc3c5fce57787a22330d284561b36
djey <j.canler@gmail.com>
2016-06-16 15:37:36 +0200

    refactor(gui/installer) : use selected vehicle slot instance instead of reloading it

--- gui-installer/src/main/java/fr/tduf/gui/installer/controllers/helper/VehicleSlotUserInputHelper.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/controllers/helper/VehicleSlotUserInputHelper.java
@@ -1,11 +1,8 @@
 package fr.tduf.gui.installer.controllers.helper;
 
 import com.esotericsoftware.minlog.Log;
-import fr.tduf.gui.installer.common.DisplayConstants;
-import fr.tduf.gui.installer.common.helper.VehicleSlotsHelper;
 import fr.tduf.gui.installer.controllers.VehicleSlotsStageController;
 import fr.tduf.gui.installer.domain.DatabaseContext;
-import fr.tduf.gui.installer.domain.VehicleSlot;
 import fr.tduf.gui.installer.domain.javafx.VehicleSlotDataItem;
 import fr.tduf.gui.installer.stages.VehicleSlotsStageDesigner;
 import javafx.stage.Stage;
@@ -46,11 +43,7 @@ public class VehicleSlotUserInputHelper {
 
         Log.info(THIS_CLASS_NAME, "->Using vehicle slot: " + selectedItem);
 
-        String slotRef = selectedItem.referenceProperty().get();
-        VehicleSlot vehicleSlot = VehicleSlotsHelper.load(context.getMiner()).getVehicleSlotFromReference(slotRef)
-                .orElseThrow(() -> new IllegalArgumentException(String.format(DisplayConstants.MESSAGE_FMT_INVALID_SLOT_INFO, slotRef)));
-
-        context.getUserSelection().selectVehicleSlot(vehicleSlot);
+        context.getUserSelection().selectVehicleSlot(selectedItem.vehicleSlotPoperty().getValue());
     }
 
     private static VehicleSlotsStageController initSlotsBrowserController(Window mainWindow) throws IOException {
--- gui-installer/src/test/java/fr/tduf/gui/installer/controllers/helper/VehicleSlotUserInputHelperTest.java
+++ gui-installer/src/test/java/fr/tduf/gui/installer/controllers/helper/VehicleSlotUserInputHelperTest.java
@@ -25,5 +25,4 @@ public class VehicleSlotUserInputHelperTest {
 
         // THEN: no FX calls
     }
-
 }
