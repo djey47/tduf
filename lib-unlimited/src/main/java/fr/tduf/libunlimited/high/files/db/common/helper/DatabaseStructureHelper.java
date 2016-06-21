4cd9b3e2b572fe5d92a33097138561eab6cb46c6
djey <j.canler@gmail.com>
2016-06-21 15:37:36 +0200

    feature(lib/database) : add helper class for structure metadata

---
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/high/files/db/common/helper/DatabaseStructureHelper.java
@@ -0,0 +1,20 @@
+package fr.tduf.libunlimited.high.files.db.common.helper;
+
+import fr.tduf.libunlimited.high.files.db.dto.DbMetadataDto;
+import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+
+/**
+ * Helper class to access structure information.
+ */
+public class DatabaseStructureHelper extends MetaDataHelper {
+    /**
+     * @return true if provided topic supports REF, false otherwise
+     */
+    public boolean isRefSupportForTopic(DbDto.Topic topic) {
+        return databaseMetadataObject.getTopics().stream()
+                .filter(topicMetaData -> topicMetaData.getTopic() == topic)
+                .findAny()
+                .map(DbMetadataDto.TopicMetadataDto::isRefSupport)
+                .orElseThrow(() -> new IllegalStateException("No metadata found for topic: " + topic));
+    }
+}
