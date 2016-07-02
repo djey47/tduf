6b6a4ee68962ad5af5380d3c8da95ad14e8cb01c
djey <j.canler@gmail.com>
2016-07-02 16:59:23 +0200

    feature(gui/installer) : prepare orchestration

--- gui-installer/src/main/java/fr/tduf/gui/installer/services/StepsCoordinator.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/services/StepsCoordinator.java
@@ -106,7 +106,7 @@ public class StepsCoordinator extends Service<Void> {
 
             updateMessage(DisplayConstants.STATUS_UNINSTALL_IN_PROGRESS);
 
-            callStepChain(RETRIEVE_BACKUP);
+            callStepChain(RETRIEVE_BACKUP/*, REMOVE_BACKUP*/);
 
             updateMessage(DisplayConstants.STATUS_UNINSTALL_DONE);
 
