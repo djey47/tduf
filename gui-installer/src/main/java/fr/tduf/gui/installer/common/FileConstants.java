3cbe92176aeb632dd0775fd91ce12dc3c9e19e84
djey <j.canler@gmail.com>
2016-07-09 14:58:13 +0200

    bugfix(gui/installer) : fix resource location

--- gui-installer/src/main/java/fr/tduf/gui/installer/common/FileConstants.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/common/FileConstants.java
@@ -20,9 +20,9 @@ public class FileConstants {
     public static final String DIRECTORY_NAME_FRONT_END = "FrontEnd";
     public static final String DIRECTORY_NAME_HUDS = "Gauges";
 
-    public static final String RESOURCE_NAME_CLEAN_PATCH = "clean-slot.mini.json";
-    public static final String RESOURCE_NAME_TDUCP_CAR_PATCH = "tducp-car-slot.mini.json";
-    public static final String RESOURCE_NAME_TDUCP_BIKE_PATCH = "tducp-bike-slot.mini.json";
+    public static final String RESOURCE_NAME_CLEAN_PATCH = "/gui-installer/templates/clean-slot.mini.json";
+    public static final String RESOURCE_NAME_TDUCP_CAR_PATCH = "/gui-installer/templates/tducp-car-slot.mini.json";
+    public static final String RESOURCE_NAME_TDUCP_BIKE_PATCH = "/gui-installer/templates/tducp-bike-slot.mini.json";
 
     private FileConstants() {}
 }
