7c640a59570d635684acf358202f7b75a304e327
djey <j.canler@gmail.com>
2016-07-11 14:43:08 +0200

    refactor(gui/installer) : remove exception

--- gui-installer/src/main/java/fr/tduf/gui/installer/steps/GenericStep.java
+++ gui-installer/src/main/java/fr/tduf/gui/installer/steps/GenericStep.java
@@ -100,8 +100,7 @@ public abstract class GenericStep {
 
         if (StepType.LOAD_DATABASE == stepType
                 || StepType.LOAD_PATCH == stepType
-                || StepType.SELECT_SLOTS == stepType
-                || StepType.RESTORE_SLOT == stepType) {
+                || StepType.SELECT_SLOTS == stepType) {
             throw new IllegalArgumentException("Step type requires interactive processing and as such can't be dealt with orchestrator: " + stepType);
         }
 
