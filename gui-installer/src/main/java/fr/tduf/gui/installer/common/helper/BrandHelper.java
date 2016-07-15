8023f0d060283012d8426a5fea35e1250fde2c90
djey <j.canler@gmail.com>
2016-07-15 15:01:12 +0200

    feature(gui/installer) : introduce new helper for brands

---
+++ gui-installer/src/main/java/fr/tduf/gui/installer/common/helper/BrandHelper.java
@@ -0,0 +1,66 @@
+package fr.tduf.gui.installer.common.helper;
+
+import fr.tduf.gui.installer.common.DatabaseConstants;
+import fr.tduf.gui.installer.domain.Brand;
+import fr.tduf.gui.installer.domain.Resource;
+import fr.tduf.libunlimited.high.files.db.miner.BulkDatabaseMiner;
+import fr.tduf.libunlimited.low.files.db.dto.content.ContentEntryDto;
+
+import java.util.List;
+import java.util.Optional;
+
+import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.BRANDS;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Component to get advanced information on vehicle brands.
+ */
+public class BrandHelper extends CommonHelper {
+    private BrandHelper(BulkDatabaseMiner miner) {
+        super(miner);
+    }
+
+    /**
+     * @param miner : component to parse database
+     * @return a new helper instance.
+     */
+    public static BrandHelper load(BulkDatabaseMiner miner) {
+        return new BrandHelper(miner);
+    }
+
+    /**
+     * @param brandReference    : entry reference (UID)
+     * @return brand information about provided reference, or empty if none has been found.
+     */
+    public Optional<Brand> getBrandFromReference(String brandReference) {
+        return miner.getContentEntryFromTopicWithReference(brandReference, BRANDS)
+                .map(this::brandEntryToDomainObject);
+    }
+
+    /**
+     * @return all available brands in database
+     */
+    public List<Brand> getAllBrands() {
+        return miner.getDatabaseTopic(BRANDS)
+                .orElseThrow(() -> new IllegalStateException("No brands information was found in database"))
+                .getData().getEntries().stream()
+                .map(this::brandEntryToDomainObject)
+                .collect(toList());
+    }
+
+    private Brand brandEntryToDomainObject(ContentEntryDto brandEntry) {
+        String brandRef = brandEntry.getItemAtRank(DatabaseConstants.FIELD_RANK_BRAND_REF)
+                .orElseThrow(() -> new IllegalStateException("No item at rank 1 in brands topic"))
+                .getRawValue();
+        Resource idResource = getResourceFromDatabaseEntry(brandEntry, BRANDS, DatabaseConstants.FIELD_RANK_MANUFACTURER_ID)
+                .orElseThrow(() -> new IllegalStateException("No manufacturer identifier"));
+        Resource nameResource = getResourceFromDatabaseEntry(brandEntry, BRANDS, DatabaseConstants.FIELD_RANK_MANUFACTURER_NAME)
+                .orElseThrow(() -> new IllegalStateException("No manufacturerbrand name"));
+
+        return Brand.builder()
+                .withReference(brandRef)
+                .withIdentifier(idResource)
+                .withDisplayedName(nameResource)
+                .build();
+    }
+}
---
+++ gui-installer/src/test/java/fr/tduf/gui/installer/common/helper/BrandHelperTest.java
@@ -0,0 +1,150 @@
+package fr.tduf.gui.installer.common.helper;
+
+import fr.tduf.gui.installer.domain.Brand;
+import fr.tduf.gui.installer.domain.Resource;
+import fr.tduf.libunlimited.high.files.db.miner.BulkDatabaseMiner;
+import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import fr.tduf.libunlimited.low.files.db.dto.content.ContentEntryDto;
+import fr.tduf.libunlimited.low.files.db.dto.content.ContentItemDto;
+import fr.tduf.libunlimited.low.files.db.dto.content.DbDataDto;
+import fr.tduf.libunlimited.low.files.db.dto.resource.DbResourceDto;
+import fr.tduf.libunlimited.low.files.db.dto.resource.ResourceEntryDto;
+import fr.tduf.libunlimited.low.files.db.dto.resource.ResourceItemDto;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static fr.tduf.gui.installer.domain.Resource.from;
+import static fr.tduf.libunlimited.common.game.domain.Locale.UNITED_STATES;
+import static fr.tduf.libunlimited.low.files.db.dto.DbDto.Topic.BRANDS;
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BrandHelperTest {
+    private static final String BRAND_REF_1 = "REF1";
+    private static final String BRAND_REF_2 = "REF2";
+    private static final String BRAND_NOREF = "REF3";
+    private static final Resource BRAND_ID_RESOURCE_1 = from("ID1", "ALFA");
+    private static final Resource BRAND_NAME_RESOURCE_1 = from("NAME1", "Alfa Romeo");
+    private static final Resource BRAND_ID_RESOURCE_2 = from("ID2", "HONDA");
+    private static final Resource BRAND_NAME_RESOURCE_2 = from("NAME2", "Honda");
+
+    @Mock
+    private BulkDatabaseMiner miner;
+
+    @InjectMocks
+    private BrandHelper brandHelper;
+
+    @Before
+    public void setUp() {
+        ContentEntryDto brandsEntry1 = ContentEntryDto.builder()
+                .forId(0)
+                .addItem(ContentItemDto.builder().ofFieldRank(1).withRawValue(BRAND_REF_1).build())
+                .addItem(ContentItemDto.builder().ofFieldRank(2).withRawValue(BRAND_ID_RESOURCE_1.getRef()).build())
+                .addItem(ContentItemDto.builder().ofFieldRank(3).withRawValue(BRAND_NAME_RESOURCE_1.getRef()).build())
+                .build();
+        ContentEntryDto brandsEntry2 = ContentEntryDto.builder()
+                .forId(1)
+                .addItem(ContentItemDto.builder().ofFieldRank(1).withRawValue(BRAND_REF_2).build())
+                .addItem(ContentItemDto.builder().ofFieldRank(2).withRawValue(BRAND_ID_RESOURCE_2.getRef()).build())
+                .addItem(ContentItemDto.builder().ofFieldRank(3).withRawValue(BRAND_NAME_RESOURCE_2.getRef()).build())
+                .build();
+        DbDataDto brandsData = DbDataDto.builder()
+                .forTopic(BRANDS)
+                .addEntry(brandsEntry1)
+                .addEntry(brandsEntry2)
+                .build();
+        DbResourceDto brandsResource = DbResourceDto.builder()
+                .atVersion("1.0")
+                .withCategoryCount(0)
+                .containingEntries(asList(
+                        ResourceEntryDto.builder()
+                                .forReference(BRAND_ID_RESOURCE_1.getRef())
+                                .withItems(singletonList(ResourceItemDto.builder()
+                                        .withLocale(UNITED_STATES)
+                                        .withValue(BRAND_ID_RESOURCE_1.getValue())
+                                        .build()))
+                                .build(),
+                        ResourceEntryDto.builder()
+                                .forReference(BRAND_ID_RESOURCE_2.getRef())
+                                .withItems(singletonList(ResourceItemDto.builder()
+                                        .withLocale(UNITED_STATES)
+                                        .withValue(BRAND_ID_RESOURCE_2.getValue())
+                                        .build()))
+                                .build(),
+                        ResourceEntryDto.builder()
+                                .forReference(BRAND_NAME_RESOURCE_1.getRef())
+                                .withItems(singletonList(ResourceItemDto.builder()
+                                        .withLocale(UNITED_STATES)
+                                        .withValue(BRAND_NAME_RESOURCE_1.getValue())
+                                        .build()))
+                                .build(),
+                        ResourceEntryDto.builder()
+                                .forReference(BRAND_NAME_RESOURCE_2.getRef())
+                                .withItems(singletonList(ResourceItemDto.builder()
+                                        .withLocale(UNITED_STATES)
+                                        .withValue(BRAND_NAME_RESOURCE_2.getValue())
+                                        .build()))
+                                .build()))
+                .build();
+        DbDto brandsTopicObject = DbDto.builder()
+                .withData(brandsData)
+                .withResource(brandsResource)
+                .build();
+
+        when(miner.getContentEntryFromTopicWithReference(BRAND_REF_1, BRANDS)).thenReturn(of(brandsEntry1));
+        when(miner.getContentEntryFromTopicWithReference(BRAND_REF_2, BRANDS)).thenReturn(of(brandsEntry2));
+        when(miner.getContentEntryFromTopicWithReference(BRAND_NOREF, BRANDS)).thenReturn(empty());
+        when(miner.getDatabaseTopic(BRANDS)).thenReturn(of(brandsTopicObject));
+        when(miner.getLocalizedResourceValueFromTopicAndReference(BRAND_ID_RESOURCE_1.getRef(), BRANDS, UNITED_STATES)).thenReturn(of(BRAND_ID_RESOURCE_1.getValue()));
+        when(miner.getLocalizedResourceValueFromTopicAndReference(BRAND_ID_RESOURCE_2.getRef(), BRANDS, UNITED_STATES)).thenReturn(of(BRAND_ID_RESOURCE_2.getValue()));
+        when(miner.getLocalizedResourceValueFromTopicAndReference(BRAND_NAME_RESOURCE_1.getRef(), BRANDS, UNITED_STATES)).thenReturn(of(BRAND_NAME_RESOURCE_1.getValue()));
+        when(miner.getLocalizedResourceValueFromTopicAndReference(BRAND_NAME_RESOURCE_2.getRef(), BRANDS, UNITED_STATES)).thenReturn(of(BRAND_NAME_RESOURCE_2.getValue()));
+    }
+
+    @Test
+    public void getBrandFromReference_whenNonExistingRef_shouldReturnEmpty() throws Exception {
+        // GIVEN-WHEN-THEN
+        assertThat(brandHelper.getBrandFromReference(BRAND_NOREF)).isEmpty();
+    }
+
+    @Test
+    public void getBrandFromReference_whenExistingRef_shouldReturnBrand() throws Exception {
+        // GIVEN-WHEN
+        Optional<Brand> actualBrand = brandHelper.getBrandFromReference(BRAND_REF_1);
+
+        // THEN
+        Brand expectedBrand = Brand.builder()
+                .withReference(BRAND_REF_1)
+                .withIdentifier(BRAND_ID_RESOURCE_1)
+                .withDisplayedName(BRAND_NAME_RESOURCE_1)
+                .build();
+        assertThat(actualBrand).contains(expectedBrand);
+
+    }
+
+    @Test
+    public void getAllBrands_shouldReturnAllEntries() throws Exception {
+        // GIVEN
+
+        // WHEN
+        List<Brand> actualBrands = brandHelper.getAllBrands();
+
+        // THEN
+        assertThat(actualBrands).hasSize(2);
+        assertThat(actualBrands).extracting("ref").containsOnly(BRAND_REF_1, BRAND_REF_2);
+        assertThat(actualBrands).extracting("identifier").containsOnly(BRAND_ID_RESOURCE_1, BRAND_ID_RESOURCE_2);
+        assertThat(actualBrands).extracting("displayedName").containsOnly(BRAND_NAME_RESOURCE_1, BRAND_NAME_RESOURCE_2);
+    }
+}
