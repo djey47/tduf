a4860364e2ed13c64a9bf847adc4656f9dfe6f71
Jerome CANLER <jerome.canler@ext.pmu.fr>
2016-08-31 14:01:14 +0200

    refactor(gui/common) : add TODO

--- gui-common/src/main/java/fr/tduf/gui/common/AppConstants.java
+++ gui-common/src/main/java/fr/tduf/gui/common/AppConstants.java
@@ -1,5 +1,6 @@
 package fr.tduf.gui.common;
 
+// TODO apply code rules
 public class AppConstants {
     public static final String SWITCH_PREFIX = "-";
     public static final String SWITCH_VERBOSE = "-v";
