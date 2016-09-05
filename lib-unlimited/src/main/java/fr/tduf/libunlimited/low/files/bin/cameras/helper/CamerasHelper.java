c13462f471aee344cae7f624551ca5e1e0925a35
djey <j.canler@gmail.com>
2016-09-05 13:32:54 +0200

    refactor(lib/cameras) : apply code rules

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/bin/cameras/helper/CamerasHelper.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/bin/cameras/helper/CamerasHelper.java
@@ -9,10 +9,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static java.util.Comparator.naturalOrder;
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Static methods tho access or modify camera information in datastore.
+ */
 public class CamerasHelper {
 
     private static final int MIN_CAMERA_SET_ID = 1;
     private static final int MAX_GENUINE_CAMERA_SET_ID = 10000;
+
+    private CamerasHelper(){}
 
     /**
      * Creates or replace a camera set at targetCameraId with all views from set at sourceCameraId.
@@ -42,14 +47,14 @@ public class CamerasHelper {
                 .sorted(naturalOrder())
                 .filter(cameraId -> cameraId >= MIN_CAMERA_SET_ID
                         && cameraId <= MAX_GENUINE_CAMERA_SET_ID)
-                .forEach((cameraId) -> duplicateCameraSet(cameraId, cameraId + targetCameraId, parser));
+                .forEach(cameraId -> duplicateCameraSet(cameraId, cameraId + targetCameraId, parser));
     }
 
     private static void updateViewsInDatastore(DataStore dataStore, long sourceCameraId, long targetCameraId, CamerasParser parser) {
         AtomicInteger viewIndex = new AtomicInteger(parser.getTotalViewCount());
         parser.getCameraViews().get(sourceCameraId).stream()
-                .map((originalViewStore) -> cloneViewStoreForNewCamera(originalViewStore, targetCameraId))
-                .forEach((clonedViewStore) -> dataStore.mergeRepeatedValues("views", viewIndex.getAndIncrement(), clonedViewStore));
+                .map(originalViewStore -> cloneViewStoreForNewCamera(originalViewStore, targetCameraId))
+                .forEach(clonedViewStore -> dataStore.mergeRepeatedValues("views", viewIndex.getAndIncrement(), clonedViewStore));
     }
 
     private static void updateIndexInDatastore(DataStore dataStore, long sourceCameraId, long targetCameraId, Map<Long, Short> cameraIndex) {
@@ -58,7 +63,7 @@ public class CamerasHelper {
         dataStore.addRepeatedIntegerValue("index", "cameraId", currentIndexEntryCount, targetCameraId);
         dataStore.addRepeatedIntegerValue("index", "viewCount", currentIndexEntryCount, viewCount);
 
-        dataStore.addInteger("indexSize", currentIndexEntryCount + 1);
+        dataStore.addInteger("indexSize", currentIndexEntryCount + 1L);
     }
 
     private static DataStore cloneViewStoreForNewCamera(DataStore viewStore, long targetCameraId) {
