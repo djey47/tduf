ee0f96b17ce86016cf71efeb7710d65782a94744
djey <j.canler@gmail.com>
2016-09-06 13:04:22 +0200

    feature(gui/database) : add default locale

--- gui-database/src/main/java/fr/tduf/gui/database/common/SettingsConstants.java
+++ gui-database/src/main/java/fr/tduf/gui/database/common/SettingsConstants.java
@@ -1,5 +1,9 @@
 package fr.tduf.gui.database.common;
 
+import fr.tduf.libunlimited.common.game.domain.Locale;
+
+import static fr.tduf.libunlimited.common.game.domain.Locale.UNITED_STATES;
+
 /**
  * Gives all constants needed to default settings.
  */
@@ -7,4 +11,8 @@ public class SettingsConstants {
     public static final String DATABASE_DIRECTORY_DEFAULT = "";
 
     public static final String PATH_RESOURCE_PROFILES = "/gui-database/layout/defaultProfiles.json";
+
+    public static  final Locale DEFAULT_LOCALE = UNITED_STATES;
+
+    private SettingsConstants(){}
 }
