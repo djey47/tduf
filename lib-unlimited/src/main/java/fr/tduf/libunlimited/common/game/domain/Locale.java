1bd3418743e348918c636bf8e53b5fb5b5784c68
djey <j.canler@gmail.com>
2016-12-03 16:23:39 +0100

    feature(lib/database) : introduce locale ordering

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/common/game/domain/Locale.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/common/game/domain/Locale.java
@@ -6,20 +6,22 @@ import java.util.stream.Stream;
  * All culture variants for i18n
  */
 public enum Locale {
-    ANY("*"),
-    FRANCE("fr"),
-    GERMANY("ge"),
-    UNITED_STATES("us"),
-    KOREA("ko"),
-    CHINA("ch"),
-    JAPAN("ja"),
-    ITALY("it"),
-    SPAIN("sp");
+    ANY("*", -1),
+    FRANCE("fr", 1),
+    GERMANY("ge", 2),
+    UNITED_STATES("us", 3),
+    KOREA("ko", 4),
+    CHINA("ch", 5),
+    JAPAN("ja", 6),
+    ITALY("it", 7),
+    SPAIN("sp", 8);
 
     private final String code;
+    private final int order;
 
-    Locale(String code) {
+    Locale(String code, int order) {
         this.code = code;
+        this.order = order;
     }
 
     /**
@@ -32,6 +34,16 @@ public enum Locale {
             }
         }
         throw new IllegalArgumentException("Unknown Locale code: " + code);
+    }
+
+    /**
+     * Retrieves a valid locale value from its order.
+     */
+    public static Locale fromOrder(int order) {
+        return valuesAsStream()
+                .filter(v -> v.order == order)
+                .findAny()
+                .orElseThrow(() ->new IllegalArgumentException("Unknown Locale order: " + order));
     }
 
     public String getCode() {
