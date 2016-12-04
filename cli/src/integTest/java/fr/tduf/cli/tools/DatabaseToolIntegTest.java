f9d1f44336db716db22e074c0005921aabb8cff8
djey <j.canler@gmail.com>
2016-12-04 17:59:45 +0100

    test(cli/database) : adapt to new naming

--- cli/src/integTest/java/fr/tduf/cli/tools/DatabaseToolIntegTest.java
+++ cli/src/integTest/java/fr/tduf/cli/tools/DatabaseToolIntegTest.java
@@ -25,7 +25,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.File;
 import java.io.FileInputStream;
