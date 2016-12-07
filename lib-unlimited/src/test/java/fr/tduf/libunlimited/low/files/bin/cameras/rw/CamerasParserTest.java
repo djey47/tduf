9cc3111659ace8726d1029d599f0833e48aaa730
djey <j.canler@gmail.com>
2016-12-07 18:37:54 +0100

    refactor (lib/cameras) : migrate to Junit5

--- lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/bin/cameras/rw/CamerasParserTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/bin/cameras/rw/CamerasParserTest.java
@@ -1,7 +1,8 @@
 package fr.tduf.libunlimited.low.files.bin.cameras.rw;
 
 import fr.tduf.libunlimited.common.helper.FilesHelper;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -9,12 +10,20 @@ import java.net.URISyntaxException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class CamerasParserTest {
+
+class CamerasParserTest {
+
+    private static byte[] camContents;
+
+    @BeforeAll
+    static void setUp() throws IOException, URISyntaxException {
+        camContents = FilesHelper.readBytesFromResourceFile("/bin/Cameras.bin");
+    }
 
     @Test
-    public void parse_whenRealFiles_shouldLoadCamerasContents_andFillCaches() throws URISyntaxException, IOException {
+    void parse_whenRealFiles_shouldLoadCamerasContents_andFillCaches() throws URISyntaxException, IOException {
         // GIVEN
-        ByteArrayInputStream camInputStream = getCamerasInputStreamFromFile();
+        ByteArrayInputStream camInputStream = new ByteArrayInputStream(camContents);
 
 
         // WHEN
@@ -34,9 +43,10 @@ public class CamerasParserTest {
     }
 
     @Test
-    public void flushCaches_shouldNullifyAllCaches() throws IOException, URISyntaxException {
+    void flushCaches_shouldNullifyAllCaches() throws IOException, URISyntaxException {
         // GIVEN
-        CamerasParser camerasParser = CamerasParser.load(getCamerasInputStreamFromFile());
+        ByteArrayInputStream camInputStream = new ByteArrayInputStream(camContents);
+        CamerasParser camerasParser = CamerasParser.load(camInputStream);
         camerasParser.parse();
         camerasParser.getCameraViews();
         camerasParser.getCameraIndex();
@@ -48,10 +58,5 @@ public class CamerasParserTest {
         assertThat(camerasParser.getCachedCameraViews()).isNull();
         assertThat(camerasParser.getCachedCameraIndex()).isNull();
         assertThat(camerasParser.getCachedTotalViewCount()).isNull();
-    }
-
-    private static ByteArrayInputStream getCamerasInputStreamFromFile() throws URISyntaxException, IOException {
-        byte[] camContents = FilesHelper.readBytesFromResourceFile("/bin/Cameras.bin");
-        return new ByteArrayInputStream(camContents);
     }
 }
