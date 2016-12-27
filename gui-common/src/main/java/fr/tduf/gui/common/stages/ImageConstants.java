bce7ed7aedaf619c255320a443e18d76c309393d
djey <j.canler@gmail.com>
2016-12-27 21:53:39 +0100

    feature(gui/common) : import free icons

---
+++ gui-common/src/main/java/fr/tduf/gui/common/stages/ImageConstants.java
@@ -0,0 +1,8 @@
+package fr.tduf.gui.common.stages;
+
+public class ImageConstants {
+    /**
+     * ORANGE WARNING SIGN
+     */
+    public static final String RESOURCE_WARN = "/gui-common/img/fwdw/001_11.png";
+}
