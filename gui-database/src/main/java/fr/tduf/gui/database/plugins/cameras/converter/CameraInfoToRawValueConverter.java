ee069000f928580b879c550d363b18f3521145de
djey <j.canler@gmail.com>
2017-01-11 17:32:58 +0100

    feature(gui/database/camerasPlugin) : handle raw value format errors

--- gui-database/src/main/java/fr/tduf/gui/database/plugins/cameras/converter/CameraInfoToRawValueConverter.java
+++ gui-database/src/main/java/fr/tduf/gui/database/plugins/cameras/converter/CameraInfoToRawValueConverter.java
@@ -1,11 +1,14 @@
 package fr.tduf.gui.database.plugins.cameras.converter;
 
+import com.esotericsoftware.minlog.Log;
 import fr.tduf.gui.database.plugins.cameras.common.DisplayConstants;
 import fr.tduf.libunlimited.low.files.bin.cameras.domain.CameraInfo;
 import javafx.collections.ObservableList;
 import javafx.util.StringConverter;
 
 public class CameraInfoToRawValueConverter extends StringConverter<CameraInfo> {
+    private static final String THIS_CLASS_NAME = CameraInfoToRawValueConverter.class.getSimpleName();
+
     private final ObservableList<CameraInfo> allCameras;
 
     public CameraInfoToRawValueConverter(ObservableList<CameraInfo> allCameras) {
@@ -22,11 +25,15 @@ public class CameraInfoToRawValueConverter extends StringConverter<CameraInfo> {
 
     @Override
     public CameraInfo fromString(String cameraIdentifierAsString) {
-        // TODO handle illegal format
-        long cameraId = Long.valueOf(cameraIdentifierAsString);
-        return allCameras.stream()
-                .filter(camera -> camera.getCameraIdentifier() == cameraId)
-                .findAny()
-                .orElse(null);
+        try {
+            long cameraId = Long.valueOf(cameraIdentifierAsString);
+            return allCameras.stream()
+                    .filter(camera -> camera.getCameraIdentifier() == cameraId)
+                    .findAny()
+                    .orElse(null);
+        } catch (NumberFormatException nfe) {
+            Log.error(THIS_CLASS_NAME, "Unable to resolve camera info from raw value: " + cameraIdentifierAsString, nfe);
+            return null;
+        }
     }
 }
