61988715a480ceb8eea21a25457b85443e1a8d66
djey <j.canler@gmail.com>
2017-01-23 23:20:10 +0100

    bugfix(gui/database/iksPlugin) : fix incorrect uppercase

--- gui-database/src/main/java/fr/tduf/gui/database/plugins/iks/common/DisplayConstants.java
+++ gui-database/src/main/java/fr/tduf/gui/database/plugins/iks/common/DisplayConstants.java
@@ -4,7 +4,7 @@ public class DisplayConstants {
     public static final String LABEL_AVAILABLE_IKS = "Available sets:";
     public static final String LABEL_IK_RAW_VALUE_DEFAULT = "";
 
-    public static final String FORMAT_IK_ITEM_LABEL = "%S (%d)";
+    public static final String FORMAT_IK_ITEM_LABEL = "%s (%d)";
 
     private DisplayConstants() {}
 }
