e512ad53d6cf4b8a7156b29862059e42076a7404
djey <j.canler@gmail.com>
2017-01-25 15:43:13 +0100

    feature(gui/database/camerasPlugin) : add error properties to context

--- gui-database/src/main/java/fr/tduf/gui/database/plugins/cameras/CamerasContext.java
+++ gui-database/src/main/java/fr/tduf/gui/database/plugins/cameras/CamerasContext.java
@@ -1,6 +1,8 @@
 package fr.tduf.gui.database.plugins.cameras;
 
 import fr.tduf.gui.database.plugins.common.PluginContext;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.StringProperty;
 
 /**
  * Piece of information required by Cameras plugin.
@@ -9,10 +11,16 @@ public class CamerasContext implements PluginContext {
     private String binaryFileLocation = null;
     private boolean pluginLoaded = false;
 
+    private StringProperty errorMessageProperty = null;
+    private BooleanProperty errorProperty = null;
+
     @Override
     public void reset() {
         binaryFileLocation = null;
         pluginLoaded = false;
+
+        errorMessageProperty = null;
+        errorProperty = null;
     }
 
     void setBinaryFileLocation(String binaryFileLocation) {
@@ -29,5 +37,21 @@ public class CamerasContext implements PluginContext {
 
     boolean isPluginLoaded() {
         return pluginLoaded;
+    }
+
+    StringProperty getErrorMessageProperty() {
+        return errorMessageProperty;
+    }
+
+    void setErrorMessageProperty(StringProperty errorMessageProperty) {
+        this.errorMessageProperty = errorMessageProperty;
+    }
+
+    BooleanProperty getErrorProperty() {
+        return errorProperty;
+    }
+
+    void setErrorProperty(BooleanProperty errorProperty) {
+        this.errorProperty = errorProperty;
     }
 }
