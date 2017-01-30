3b596759038073023abfdeae5d4b5b53c10464d3
djey <j.canler@gmail.com>
2017-01-30 20:39:46 +0100

    feature(gui/database/camerasPlugin) : implement import - WIP

--- gui-database/src/main/java/fr/tduf/gui/database/plugins/cameras/helper/CamerasImExHelper.java
+++ gui-database/src/main/java/fr/tduf/gui/database/plugins/cameras/helper/CamerasImExHelper.java
@@ -3,7 +3,6 @@ package fr.tduf.gui.database.plugins.cameras.helper;
 import fr.tduf.libunlimited.high.files.bin.cameras.patcher.CamPatcher;
 import fr.tduf.libunlimited.high.files.bin.cameras.patcher.dto.CamPatchDto;
 import fr.tduf.libunlimited.high.files.common.patcher.domain.PatchProperties;
-import fr.tduf.libunlimited.high.files.db.patcher.domain.DatabasePatchProperties;
 import fr.tduf.libunlimited.high.files.db.patcher.helper.PatchPropertiesReadWriteHelper;
 import fr.tduf.libunlimited.low.files.bin.cameras.rw.CamerasParser;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -11,8 +10,6 @@ import org.codehaus.jackson.map.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
-
-import static java.util.Optional.empty;
 
 /**
  * Performs import/export operations
@@ -29,11 +26,10 @@ public class CamerasImExHelper {
     public Optional<String> importPatch(File patchFile, CamerasParser camerasParser) throws IOException {
         CamPatchDto patchObject = objectMapper.readValue(patchFile, CamPatchDto.class);
         CamPatcher patcher = new CamPatcher(camerasParser);
-        DatabasePatchProperties patchProperties = PatchPropertiesReadWriteHelper.readDatabasePatchProperties(patchFile);
+        PatchProperties patchProperties = PatchPropertiesReadWriteHelper.readPatchProperties(patchFile);
 
         final PatchProperties effectiveProperties = patcher.applyWithProperties(patchObject, patchProperties);
 
-//        return PatchPropertiesReadWriteHelper.writeEffectivePatchProperties(effectiveProperties, patchFile.getAbsolutePath());
-        return empty();
+        return PatchPropertiesReadWriteHelper.writeEffectivePatchProperties(effectiveProperties, patchFile.getAbsolutePath());
     }
 }
