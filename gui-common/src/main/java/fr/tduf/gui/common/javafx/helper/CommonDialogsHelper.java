7ed58615c8c7e76fe4ee496e3688b68e347e13f6
Jérôme Canler <jerome.canler@soat.fr>
2017-02-13 19:08:16 +0100

    refactor(gui/common) : use JavaFX's input value dialog

--- gui-common/src/main/java/fr/tduf/gui/common/javafx/helper/CommonDialogsHelper.java
+++ gui-common/src/main/java/fr/tduf/gui/common/javafx/helper/CommonDialogsHelper.java
@@ -3,9 +3,7 @@ package fr.tduf.gui.common.javafx.helper;
 import fr.tduf.gui.common.javafx.helper.options.FileBrowsingOptions;
 import fr.tduf.gui.common.javafx.helper.options.SimpleDialogOptions;
 import javafx.application.Platform;
-import javafx.geometry.Insets;
 import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
@@ -78,43 +76,34 @@ public class CommonDialogsHelper {
     }
 
     /**
-     * Display a dialog box requesting user for a value.
+     * Displays a dialog box requesting user for a value.
      * @param title     : text in upper dialog bar
      * @param label     : label for value text field
      * @param parent    : owner dialog, or null to be a top-level one
      * @return resulting value, or absent if dialog was dismissed.
      */
     public static Optional<String> showInputValueDialog(String title, String label, Window parent) {
-        // TODO use FX's TextInputDialog
-        Dialog<String> inputValueDialog = new Dialog<>();
-        inputValueDialog.initOwner(parent);
-        inputValueDialog.setTitle(title);
-        inputValueDialog.setResizable(true);
+        TextInputDialog textInputDialog = new TextInputDialog();
+        TextField valueTextField = textInputDialog.getEditor();
 
-        GridPane grid = new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        // TODO Normalize padding
-        grid.setPadding(new Insets(20, 150, 10, 10));
-
-        TextField valueTextField = new TextField();
-        Platform.runLater(valueTextField::requestFocus);
-
-        grid.add(new Label(label), 0, 0);
-        grid.add(valueTextField, 1, 0);
-        inputValueDialog.getDialogPane().setContent(grid);
+        textInputDialog.setTitle(title);
+        textInputDialog.initOwner(parent);
+        textInputDialog.setHeaderText(label);
 
         ButtonType okButtonType = new ButtonType(LABEL_BUTTON_OK, OK_DONE);
-        ButtonType cancelButtonType = new ButtonType(LABEL_BUTTON_CANCEL, CANCEL_CLOSE);
-        inputValueDialog.getDialogPane().getButtonTypes().setAll(okButtonType, cancelButtonType);
+        textInputDialog.getDialogPane().getButtonTypes().setAll(
+                okButtonType,
+                new ButtonType(LABEL_BUTTON_CANCEL, CANCEL_CLOSE));
 
-        inputValueDialog.setResultConverter(dialogButton -> {
-            if (dialogButton == okButtonType) {
+        textInputDialog.setResultConverter(dialogButton -> {
+            if (okButtonType == dialogButton) {
                 return valueTextField.getText();
             }
             return null;
         });
 
-        return inputValueDialog.showAndWait();
+        Platform.runLater(valueTextField::requestFocus);
+
+        return textInputDialog.showAndWait();
     }
 }
