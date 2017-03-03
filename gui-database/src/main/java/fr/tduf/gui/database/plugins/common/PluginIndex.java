b4d666b8142a15939353dc3351132b0f0ce3812c
djey <j.canler@gmail.com>
2017-03-03 13:25:48 +0100

    feature(gui/database) : add mapping plugin - WIP

--- gui-database/src/main/java/fr/tduf/gui/database/plugins/common/PluginIndex.java
+++ gui-database/src/main/java/fr/tduf/gui/database/plugins/common/PluginIndex.java
@@ -4,6 +4,7 @@ import fr.tduf.gui.database.plugins.bitfield.BitfieldPlugin;
 import fr.tduf.gui.database.plugins.cameras.CamerasPlugin;
 import fr.tduf.gui.database.plugins.contentRef.ContentRefPlugin;
 import fr.tduf.gui.database.plugins.iks.IKsPlugin;
+import fr.tduf.gui.database.plugins.mapping.MappingPlugin;
 import fr.tduf.gui.database.plugins.nope.NopePlugin;
 import fr.tduf.gui.database.plugins.percent.PercentPlugin;
 
@@ -18,7 +19,8 @@ enum PluginIndex {
     CAMERAS("Allows to select over available cameras and view/modify properties", new CamerasPlugin()),
     IKS("Allows to select over available Inverse Kinematics sets", new IKsPlugin()),
     PERCENT("Allows to make percent value changes easier", new PercentPlugin()),
-    CONTENT_REF("Displays warning image and label", new ContentRefPlugin());
+    CONTENT_REF("Displays warning image and label", new ContentRefPlugin()),
+    MAPPING("Shows files required by content entries and registration status in Bnk1.map", new MappingPlugin());
 
     private final String description;
     private final DatabasePlugin pluginInstance;
---
+++ gui-database/src/main/java/fr/tduf/gui/database/plugins/mapping/MappingPlugin.java
@@ -0,0 +1,77 @@
+package fr.tduf.gui.database.plugins.mapping;
+
+import com.esotericsoftware.minlog.Log;
+import fr.tduf.gui.database.plugins.common.DatabasePlugin;
+import fr.tduf.gui.database.plugins.common.EditorContext;
+import fr.tduf.libunlimited.high.files.db.common.helper.CameraAndIKHelper;
+import fr.tduf.libunlimited.low.files.banks.mapping.domain.BankMap;
+import fr.tduf.libunlimited.low.files.banks.mapping.helper.MapHelper;
+import fr.tduf.libunlimited.low.files.banks.mapping.rw.MapParser;
+import fr.tduf.libunlimited.low.files.bin.cameras.domain.CamerasDatabase;
+import fr.tduf.libunlimited.low.files.bin.cameras.helper.CamerasHelper;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.Node;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+
+/**
+ * File mapping status plugin
+ */
+public class MappingPlugin implements DatabasePlugin {
+    private static final Class<MappingPlugin> thisClass = MappingPlugin.class;
+    private static final String THIS_CLASS_NAME = thisClass.getSimpleName();
+
+    private final Property<BankMap> bankMapProperty = new SimpleObjectProperty<>();
+
+    /**
+     * Required contextual information:
+     * @param context : all required information about Database Editor
+     * @throws IOException when cameras file can't be parsed for some reason
+     */
+    @Override
+    public void onInit(EditorContext context) throws IOException {
+        MappingContext mappingContext = context.getMappingContext();
+        mappingContext.reset();
+        
+        bankMapProperty.setValue(null);
+
+        String gameLocation = context.getGameLocation();
+        Path mappingFile = resolveMappingFilePath(gameLocation);
+        if (!Files.exists(mappingFile)) {
+            Log.warn(THIS_CLASS_NAME, "No bnk1.map file was found under game directory: " + gameLocation);
+            return;
+        }
+
+        Log.info(THIS_CLASS_NAME, "Loading mapping info from " + mappingFile);
+
+        BankMap bankMap = MapParser.load(mappingFile.toString()).parse();
+        bankMapProperty.setValue(bankMap);
+
+        Log.info(THIS_CLASS_NAME, "Mapping info loaded");        
+        mappingContext.setPluginLoaded(true);        
+    }
+
+    @Override
+    public void onSave(EditorContext context) throws IOException {
+
+    }
+
+    @Override
+    public Node renderControls(EditorContext context) {
+        return null;
+    }
+
+    @Override
+    public Set<String> getCss() {
+        return null;
+    }
+
+    private Path resolveMappingFilePath(String gameLocation) {
+        return Paths.get(gameLocation, "euro", "bnk", MapHelper.MAPPING_FILE_NAME);
+    }    
+}
