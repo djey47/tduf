11e7eaca529e9987ca4e36b1dbeb6a4429f9df81
djey <j.canler@gmail.com>
2017-03-15 14:55:43 +0100

    refactor(gui/database/mappingPlugin) : add properties

--- gui-database/src/main/java/fr/tduf/gui/database/plugins/mapping/domain/MappingEntry.java
+++ gui-database/src/main/java/fr/tduf/gui/database/plugins/mapping/domain/MappingEntry.java
@@ -1,35 +1,52 @@
 package fr.tduf.gui.database.plugins.mapping.domain;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 /**
  * Model for mapping table row
  */
 public class MappingEntry {
-    private String kind;
-    private String path;
+    private final StringProperty kind = new SimpleStringProperty("");
+    private final StringProperty path = new SimpleStringProperty("");
 
-    private boolean exists;
-    private boolean registered;
+    private final BooleanProperty exists = new SimpleBooleanProperty(false);
+    private final BooleanProperty registered = new SimpleBooleanProperty(false);
 
     public MappingEntry(String kind, String path, boolean exists, boolean registered) {
-        this.kind = kind;
-        this.path = path;
-        this.exists = exists;
-        this.registered = registered;
+        this.kind.setValue(kind);
+        this.path.setValue(path);
+        this.exists.set(exists);
+        this.registered.set(registered);
     }
     
     public String getPath() {
-        return path;
+        return path.getValue();
     }
 
     public boolean isExists() {
-        return exists;
+        return exists.get();
     }
 
-    public boolean isRegistered() {
+    public BooleanProperty existingProperty() {
+        return exists;
+    }    
+    
+    public BooleanProperty registeredProperty() {
         return registered;
     }
 
+    public boolean isRegistered() {
+        return registered.get();
+    }
+    
+    public void registered() {
+        registered.set(true);
+    }
+
     public String getKind() {
-        return kind;
+        return kind.getValue();
     }
 }
