09e239bf8b32fdc995551307706bfc62a82464c1
djey <j.canler@gmail.com>
2017-04-26 11:36:10 +0200

    feature(gui/launcher): enhance PP's switches support

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/common/game/domain/bin/LaunchSwitch.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/common/game/domain/bin/LaunchSwitch.java
@@ -4,26 +4,42 @@ package fr.tduf.libunlimited.common.game.domain.bin;
  * All switches to be used at launch
  */
 public enum LaunchSwitch {
-    WINDOWED("w", "Start windowed"),
-    FRAMERATE("fps", "Show FPS in upper-right corner"),
-    POSITION("pos", "Show coordinates, cameras and sectors in upper-left corner"),
-    TRAFFIC("???", "Show traffic position on GPS map"), //TODO find code
-    FUEL_PP("fuel", "Enable FUEL mode"),
-    HD_PP("hd", "Enable High Definition overrides");
+    // TODO add PP's switches
+    WINDOWED("w", "Start windowed", true),
+    FRAMERATE("fps", "Show FPS in upper-right corner", true),
+    POSITION("pos", "Show coordinates, cameras and sectors in upper-left corner", true),
+    TRAFFIC("???", "Show traffic position on GPS map", true), //TODO find code
+    FUEL_PP("fuel", "Enable FUEL mode", false),
+    HD_PP("hd", "Enable High Definition support", false);
 
+    private static final String SUFFIX_PROJECT_PARADISE = "_PP";
+    
     private final String code;
     private final String label;
+    private final boolean genuine;
 
-    LaunchSwitch(String code, String label) {
+    LaunchSwitch(String code, String label, boolean genuine) {
         this.code = code;
         this.label = label;
+        this.genuine = genuine;
     }
 
+    /**
+     * @return true if this is a switch brought by Project Paradise mod, false otherwise
+     */
+    public boolean isFromProjectParadise() {
+        return name().endsWith(SUFFIX_PROJECT_PARADISE);
+    }
+    
     public String getCode() {
         return code;
     }
 
     public String getLabel() {
         return label;
+    }
+
+    public boolean isGenuine() {
+        return genuine;
     }
 }
