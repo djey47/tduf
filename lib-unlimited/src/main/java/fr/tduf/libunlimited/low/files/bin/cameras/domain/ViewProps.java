8be43ec4b149a40555eff29e4a0b903f5f7bec63
djey47 <djey47@github.com>
2020-04-12 16:34:41 +0200

    feature(lib): enhance description for view props

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/bin/cameras/domain/ViewProps.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/bin/cameras/domain/ViewProps.java
@@ -12,21 +12,21 @@ import java.util.stream.Stream;
  * All handled view properties
  */
 public enum ViewProps implements DataStoreProps {
-    COMPLEMENTARY_VIEW_KIND("complementaryViewKind", "Front/back switch", GenericParser::getNumeric),
-    STEERING_WHEEL_TURN("steeringWheelTurn", "", GenericParser::getNumeric),
+    COMPLEMENTARY_VIEW_KIND("complementaryViewKind", "Front/back switch (use existing view number)", GenericParser::getNumeric),
+    STEERING_WHEEL_TURN("steeringWheelTurn", "? To be tested", GenericParser::getNumeric),
     STEERING_WHEEL_TILT("steeringWheelTilt", "Wheel vertical adjustment (0..255)", GenericParser::getNumeric),
-    CAMERA_POSITION_X("cameraPositionX", "Left/Right", GenericParser::getNumeric),
-    CAMERA_POSITION_Y("cameraPositionY", "Up/Down", GenericParser::getNumeric),
-    CAMERA_POSITION_Z("cameraPositionZ", "Forward/Backward", GenericParser::getNumeric),
-    VIEW_POSITION_X("viewPositionX", "Left/Right", GenericParser::getNumeric),
-    VIEW_POSITION_Y("viewPositionY", "Up/Down", GenericParser::getNumeric),
-    VIEW_POSITION_Z("viewPositionZ", "Forward/Backward", GenericParser::getNumeric),
-    BINOCULARS("binoculars", "", GenericParser::getNumeric),
-    UNKNOWN_1("unk1", "", GenericParser::getNumeric),
-    UNKNOWN_2("unk2", "", GenericParser::getNumeric),
-    UNKNOWN_3("unk3", "", GenericParser::getNumeric),
-    UNKNOWN_4("unk4", "", GenericParser::getNumeric),
-    UNKNOWN_5("unk5", "", GenericParser::getNumeric);
+    CAMERA_POSITION_X("cameraPositionX", "Left/Right (camera)", GenericParser::getNumeric),
+    CAMERA_POSITION_Y("cameraPositionY", "Up/Down (camera)", GenericParser::getNumeric),
+    CAMERA_POSITION_Z("cameraPositionZ", "Forward/Backward (camera)", GenericParser::getNumeric),
+    VIEW_POSITION_X("viewPositionX", "Left/Right (view)", GenericParser::getNumeric),
+    VIEW_POSITION_Y("viewPositionY", "Up/Down (view)", GenericParser::getNumeric),
+    VIEW_POSITION_Z("viewPositionZ", "Forward/Backward (view)", GenericParser::getNumeric),
+    BINOCULARS("binoculars", "Field Of View", GenericParser::getNumeric),
+    UNKNOWN_1("unk1", "? To be tested", GenericParser::getNumeric),
+    UNKNOWN_2("unk2", "? To be tested", GenericParser::getNumeric),
+    UNKNOWN_3("unk3", "? To be tested", GenericParser::getNumeric),
+    UNKNOWN_4("unk4", "? To be tested", GenericParser::getNumeric),
+    UNKNOWN_5("unk5", "? To be tested", GenericParser::getNumeric);
 
     private String storeFieldName;
     private String description;
