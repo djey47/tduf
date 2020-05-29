3c96aa75d50cb400371ba302ccf3cb9f84ed3fa8
djey47 <djey47@github.com>
2020-05-29 14:25:38 +0200

    refactor(cli): enhance logs code

--- src/main/shell/linux/Logs.sh
+++ src/main/shell/linux/Logs.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
-xdg-open ./logs/DatabaseEditor.log || less ./logs/DatabaseEditor.log
-xdg-open ./logs/Launcher.log || less ./logs/Launcher.log
+DATABASE_EDITOR_LOGS=./logs/DatabaseEditor.log
+LAUNCHER_LOGS=./logs/Launcher.log
+
+view_logs () {
+  if [ -f "$1" ]; then
+    xdg-open "$1" || less "$1"
+  else
+    echo "No logs: $1"
+  fi
+}
+
+view_logs $DATABASE_EDITOR_LOGS
+view_logs $LAUNCHER_LOGS
--- src/main/shell/windows/Logs.cmd
+++ src/main/shell/windows/Logs.cmd
@@ -1,4 +1,17 @@
 @ECHO OFF
 
-.\logs\DatabaseEditor.log
-.\logs\Launcher.log
+SET DATABASE_EDITOR_LOGS=./logs/DatabaseEditor.log
+SET LAUNCHER_LOGS=./logs/Launcher.log
+
+CALL :view_logs %DATABASE_EDITOR_LOGS%
+CALL :view_logs %LAUNCHER_LOGS%
+
+EXIT /B 0
+
+:view_logs
+IF EXIST "%1" (
+  %1
+) ELSE (
+  ECHO No logs: %1
+)
+EXIT /B 0
