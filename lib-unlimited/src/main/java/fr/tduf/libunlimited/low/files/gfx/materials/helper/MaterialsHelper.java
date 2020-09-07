bfde341ddbdf6ce5be7bd731fdf7772bf735da4d
djey47 <djey47@github.com>
2020-09-07 18:07:04 +0200

    feature(lib/files): add helper lookup methods

--- lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/gfx/materials/helper/MaterialsHelper.java
+++ lib-unlimited/src/main/java/fr/tduf/libunlimited/low/files/gfx/materials/helper/MaterialsHelper.java
@@ -2,6 +2,7 @@ package fr.tduf.libunlimited.low.files.gfx.materials.helper;
 
 import fr.tduf.libunlimited.high.files.db.miner.BulkDatabaseMiner;
 import fr.tduf.libunlimited.low.files.db.dto.DbDto;
+import fr.tduf.libunlimited.low.files.db.dto.resource.ResourceEntryDto;
 import fr.tduf.libunlimited.low.files.gfx.materials.domain.MaterialDefs;
 import fr.tduf.libunlimited.low.files.gfx.materials.rw.MaterialsWriter;
 
@@ -48,10 +49,8 @@ public class MaterialsHelper {
      */
     public static void updateNormalizedDictionary(Map<String, String> dicToUpdate, BulkDatabaseMiner miner) {
         MATERIAL_TOPICS.forEach(topic -> {
-            Map<String, String> topicDic = miner.getResourcesFromTopic(topic)
-                    .orElseThrow(() -> new IllegalStateException("No resources for topic " + topic))
-                    .getEntries().parallelStream()
-                    .map(resourceEntryDto -> resourceEntryDto.pickValue().orElseThrow(() -> new IllegalStateException("")))
+            Map<String, String> topicDic = getAllResourcesAsParallelStream(topic, miner)
+                    .map(MaterialsHelper::pickResourceValue)
                     .collect(toMap(
                             value -> normalizeString(value.toUpperCase()),
                             value -> value,
@@ -82,5 +81,42 @@ public class MaterialsHelper {
                     String currentSuffix = FIELD_SUFFIXES_COLORS[itemIndex];
                     return String.format(FORMAT_COLOR_SETTINGS_KEY, groupKeyName, settingKeyName, currentSuffix);
                 });
+    }
+
+    /**
+     * @param materialName  : material name to provide a REF for
+     * @param topic         : enclosing database topic
+     * @param miner         : database miner, to perform lookup ops
+     * @return existing resource reference for given material name
+     */
+    public static String getResourceRefForMaterialName(String materialName, DbDto.Topic topic, BulkDatabaseMiner miner) {
+        return getAllResourcesAsParallelStream(topic, miner)
+                .filter(resourceEntry -> pickResourceValue(resourceEntry).equalsIgnoreCase(materialName))
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("No resource entry for material name " + materialName))
+                .getReference();
+    }
+
+    /**
+     * @param materialName  : material name to be searched
+     * @param topic         : enclosing database topic
+     * @param miner         : database miner, to perform lookup ops
+     * @return true if such a material name exists in resources, false otherwise
+     */
+    public static boolean isExistingMaterialNameInResources(String materialName, DbDto.Topic topic, BulkDatabaseMiner miner) {
+        return getAllResourcesAsParallelStream(topic, miner)
+                .map(MaterialsHelper::pickResourceValue)
+                .anyMatch(value -> value.equalsIgnoreCase(materialName));
+    }
+
+    private static String pickResourceValue(ResourceEntryDto resourceEntry) {
+        return resourceEntry.pickValue()
+                .orElseThrow(() -> new IllegalStateException("No resource value available"));
+    }
+
+    private static Stream<ResourceEntryDto> getAllResourcesAsParallelStream(DbDto.Topic topic, BulkDatabaseMiner miner) {
+        return miner.getResourcesFromTopic(topic)
+                .orElseThrow(() -> new IllegalStateException("No resources for topic: " + topic.name()))
+                .getEntries().parallelStream();
     }
 }
--- lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/gfx/materials/helper/MaterialsHelperTest.java
+++ lib-unlimited/src/test/java/fr/tduf/libunlimited/low/files/gfx/materials/helper/MaterialsHelperTest.java
@@ -16,6 +16,7 @@ import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static java.util.Optional.of;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -27,14 +28,12 @@ class MaterialsHelperTest {
     @BeforeEach
     void setUp() {
         initMocks(this);
+        initResourceMocks();
     }
 
     @Test
     void buildNormalizedDictionary_shouldQueryResources() {
-        // given
-        initResourceMocks();
-
-        // when
+        // given-when
         Map<String, String> actualDic = MaterialsHelper.buildNormalizedDictionary(minerMock);
 
         // then
@@ -46,7 +45,6 @@ class MaterialsHelperTest {
     @Test
     void updateNormalizedDictionary_shouldQueryResources() {
         // given
-        initResourceMocks();
         Map<String, String> dicToUpdate = new HashMap<>();
 
         // when
@@ -56,6 +54,43 @@ class MaterialsHelperTest {
         assertThat(dicToUpdate).hasSize(2);
         assertThat(dicToUpdate).containsEntry("\u0087ALUE___", "Value___1");
         assertThat(dicToUpdate).containsEntry("\u0088ALUE___", "Value___2");
+    }
+
+    @Test
+    void isExistingMaterialNameInResources_whenNonExistingResource_shouldReturnFalse() {
+        // given-when-then
+        assertThat(MaterialsHelper.isExistingMaterialNameInResources("Value___1", INTERIOR, minerMock)).isFalse();
+    }
+
+    @Test
+    void isExistingMaterialNameInResources_whenExistingResource_shouldReturnTrue() {
+        // given-when-then
+        assertThat(MaterialsHelper.isExistingMaterialNameInResources("Value___1", CAR_COLORS, minerMock)).isTrue();
+    }
+
+    @Test
+    void getResourceRefForMaterialName_whenNonExistingResource_shouldThrowException() {
+        // given-when-then
+        assertThrows(IllegalStateException.class,
+                () -> MaterialsHelper.getResourceRefForMaterialName("Value___1", INTERIOR, minerMock));
+    }
+
+    @Test
+    void getResourceRefForMaterialName_shouldReturnExistingRef() {
+        // given-when
+        String actualRef = MaterialsHelper.getResourceRefForMaterialName("Value___1", CAR_COLORS, minerMock);
+
+        // then
+        assertThat(actualRef).isEqualTo("REF1");
+    }
+
+    @Test
+    void getResourceRefForMaterialName_shouldReturnExistingRef_caseUnsensitive() {
+        // given-when
+        String actualRef = MaterialsHelper.getResourceRefForMaterialName("VALUE___1", CAR_COLORS, minerMock);
+
+        // then
+        assertThat(actualRef).isEqualTo("REF1");
     }
 
     private void initResourceMocks() {
