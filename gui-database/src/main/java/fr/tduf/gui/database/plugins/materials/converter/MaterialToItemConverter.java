f7c2de8fccea913b52554aa6d7417629e0da10f5
djey47 <djey47@github.com>
2020-10-10 23:09:04 +0200

    refactor(gui/database): apply defensive programming

--- gui-database/src/main/java/fr/tduf/gui/database/plugins/materials/converter/MaterialToItemConverter.java
+++ gui-database/src/main/java/fr/tduf/gui/database/plugins/materials/converter/MaterialToItemConverter.java
@@ -7,16 +7,21 @@ import java.util.function.Function;
 
 import static fr.tduf.gui.database.plugins.materials.common.DisplayConstants.FORMAT_MATERIAL_LABEL;
 import static fr.tduf.gui.database.plugins.materials.common.DisplayConstants.LABEL_NO_NAME;
+import static java.util.Objects.requireNonNull;
 
 public class MaterialToItemConverter extends StringConverter<Material> {
     private final Function<String, String> fullNameProvider;
 
     public MaterialToItemConverter(Function<String, String> fullNameProvider) {
-        this.fullNameProvider = fullNameProvider;
+        this.fullNameProvider = requireNonNull(fullNameProvider, "Material full name provider function is required");
     }
 
     @Override
     public String toString(Material material) {
+        if (material == null) {
+            return "";
+        }
+
         String materialRawName = material.getName();
         String materialClearName = fullNameProvider.apply(materialRawName);
         return String.format(FORMAT_MATERIAL_LABEL, materialRawName, materialClearName == null ? LABEL_NO_NAME : materialClearName);
